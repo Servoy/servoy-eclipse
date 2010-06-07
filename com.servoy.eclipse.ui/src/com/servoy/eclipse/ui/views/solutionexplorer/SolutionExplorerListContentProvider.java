50b634059df131c64a1257d3b3fb34f61e0fc2d8
Laurian Vostinar <lvostinar@servoy.com>
2010-06-07 08:33:24 +0000

    296436: In Serclipse the tooltip of application.getValieListArray returns the type NativeArray, should be String[], i think Case: 296436

--- com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/views/solutionexplorer/SolutionExplorerListContentProvider.java
+++ com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/views/solutionexplorer/SolutionExplorerListContentProvider.java
@@ -58,9 +58,9 @@ import com.servoy.eclipse.ui.node.UserNode;
 import com.servoy.eclipse.ui.node.UserNodeType;
 import com.servoy.eclipse.ui.scripting.CalculationModeHandler;
 import com.servoy.j2db.FlattenedSolution;
+import com.servoy.j2db.IApplication;
 import com.servoy.j2db.FormController.JSForm;
 import com.servoy.j2db.FormManager.HistoryProvider;
-import com.servoy.j2db.IApplication;
 import com.servoy.j2db.dataprocessing.FoundSet;
 import com.servoy.j2db.dataprocessing.JSDatabaseManager;
 import com.servoy.j2db.dataprocessing.Record;
@@ -157,6 +157,7 @@ public class SolutionExplorerListContentProvider implements IStructuredContentPr
 		TYPES.put("IFoundSetInternal", FoundSet.JS_FOUNDSET);
 		TYPES.put("Foundset", FoundSet.JS_FOUNDSET);
 		TYPES.put(FormScope.class.getName(), "Form");
+		TYPES.put("org.mozilla.javascript.NativeArray", "Array");
 		Method[] methods = Object.class.getMethods();
 		for (Method method : methods)
 		{
