fbc042ea2999cc2bbf9fd1c2f26ac0bd0cb55212
Gabi Boros <gboros@servoy.com>
2010-06-16 07:37:39 +0000

    moved loginSolutionName setting logic into Solution

--- com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/quickfix/CreateLoginSolutionQuickFix.java
+++ com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/quickfix/CreateLoginSolutionQuickFix.java
@@ -17,28 +17,21 @@
 
 package com.servoy.eclipse.ui.quickfix;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.dltk.javascript.core.JavaScriptNature;
 import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.window.Window;
 import org.eclipse.ui.IMarkerResolution;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.views.properties.IPropertySource;
 
 import com.servoy.eclipse.core.ServoyLog;
 import com.servoy.eclipse.core.ServoyModel;
 import com.servoy.eclipse.core.ServoyModelManager;
 import com.servoy.eclipse.core.ServoyProject;
 import com.servoy.eclipse.core.repository.EclipseRepository;
-import com.servoy.eclipse.core.util.CoreUtils;
 import com.servoy.eclipse.ui.views.solutionexplorer.actions.MovePersistAction;
 import com.servoy.j2db.persistence.Form;
 import com.servoy.j2db.persistence.IPersist;
@@ -46,6 +39,7 @@ import com.servoy.j2db.persistence.IRepository;
 import com.servoy.j2db.persistence.RepositoryException;
 import com.servoy.j2db.persistence.Solution;
 import com.servoy.j2db.persistence.SolutionMetaData;
+import com.servoy.j2db.persistence.ContentSpec.Element;
 
 public class CreateLoginSolutionQuickFix implements IMarkerResolution
 {
@@ -95,40 +89,31 @@ public class CreateLoginSolutionQuickFix implements IMarkerResolution
 
 								EclipseRepository repository = (EclipseRepository)ServoyModel.getDeveloperRepository();
 								Solution loginSolution = (Solution)repository.createNewRootObject(loginSolutionName, IRepository.SOLUTIONS);
-								IProject resourceProject = servoyProject.getResourcesProject().getProject();
-								newProject.create(null);
-								newProject.open(null);
-
 								if (loginSolution != null)
 								{
+									IProject resourceProject = servoyProject.getResourcesProject().getProject();
+									newProject.create(null);
+									newProject.open(null);
+
 									loginSolution.setSolutionType(SolutionMetaData.LOGIN_SOLUTION);
 									repository.updateRootObject(loginSolution);
-								}
-								IProjectDescription description = newProject.getDescription();
-								description.setNatureIds(new String[] { ServoyProject.NATURE_ID, JavaScriptNature.NATURE_ID });
 
-								description.setReferencedProjects(new IProject[] { resourceProject });
-								newProject.setDescription(description, null);
+									IProjectDescription description = newProject.getDescription();
+									description.setNatureIds(new String[] { ServoyProject.NATURE_ID, JavaScriptNature.NATURE_ID });
 
-								// update current solution's module list with the login solution
-								String[] modules = CoreUtils.getTokenElements(solution.getModulesNames(), ",", true); //$NON-NLS-1$
-								List<String> modulesList = new ArrayList<String>(Arrays.asList(modules));
-								modulesList.add(loginSolution.getName());
+									description.setReferencedProjects(new IProject[] { resourceProject });
+									newProject.setDescription(description, null);
 
-								String modulesTokenized = CoreUtils.getTokenValue(modulesList.toArray(new String[] { }), ","); //$NON-NLS-1$
-								solution.setModulesNames(modulesTokenized);
+									// update the affected properties in the current solution
+									solution.setLoginSolutionName(loginSolutionName);
+									Element loginFormEl = repository.getContentSpec().getPropertyForObjectTypeByName(solution.getTypeID(), "loginFormID"); //$NON-NLS-1$
+									Object loginFormElDefValue = loginFormEl.getDefaultClassValue();
+									solution.setLoginFormID(loginFormElDefValue instanceof Integer ? ((Integer)loginFormElDefValue).intValue() : 0);
+									servoyProject.saveEditingSolutionNodes(new IPersist[] { solution }, false);
 
-								servoyProject.saveEditingSolutionNodes(new IPersist[] { solution }, false);
-
-								// move the login form to the login solution
-								new MoveForm().move(loginForm, ServoyModelManager.getServoyModelManager().getServoyModel().getServoyProject(loginSolutionName));
-
-								// update the affected properties in the current solution
-								IPropertySource propertySource = (IPropertySource)Platform.getAdapterManager().getAdapter(solution, IPropertySource.class);
-								if (propertySource != null)
-								{
-									propertySource.resetPropertyValue("loginFormID"); //$NON-NLS-1$
-									propertySource.setPropertyValue("loginSolutionName", loginSolutionName); //$NON-NLS-1$
+									// move the login form to the login solution
+									new MoveForm().move(loginForm, ServoyModelManager.getServoyModelManager().getServoyModel().getServoyProject(
+										loginSolutionName));
 								}
 							}
 							catch (Exception ex)
