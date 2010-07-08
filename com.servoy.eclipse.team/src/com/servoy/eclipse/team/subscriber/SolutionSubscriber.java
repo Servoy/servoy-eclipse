e371ab1d710b7c72db0446a1cdccc7ab6793ba73
Laurian Vostinar <lvostinar@servoy.com>
2010-07-08 09:00:41 +0000

    290061: When deleting forms in developer, errors in Eclipse log appear Case: 290061

--- com.servoy.eclipse.team/src/com/servoy/eclipse/team/subscriber/SolutionSubscriber.java
+++ com.servoy.eclipse.team/src/com/servoy/eclipse/team/subscriber/SolutionSubscriber.java
@@ -13,7 +13,7 @@
  You should have received a copy of the GNU Affero General Public License along
  with this program; if not, see http://www.gnu.org/licenses or write to the Free
  Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
-*/
+ */
 package com.servoy.eclipse.team.subscriber;
 
 import java.util.ArrayList;
@@ -281,7 +281,7 @@ public class SolutionSubscriber extends ThreeWaySubscriber
 		makeInSync(resource);
 		try
 		{
-			resource.touch(monitor);
+			if (resource.exists()) resource.touch(monitor);
 		}
 		catch (CoreException e)
 		{
