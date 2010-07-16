b196968546e678afc1c4beebc13b5b15aa017918
Johan Compagner <jcompagner@servoy.com>
2010-07-16 09:21:38 +0000

    Writing javadoc in 5.2+
    author tag change
    Case: 302822

--- com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/views/solutionexplorer/actions/NewRelationAction.java
+++ com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/views/solutionexplorer/actions/NewRelationAction.java
@@ -13,7 +13,7 @@
  You should have received a copy of the GNU Affero General Public License along
  with this program; if not, see http://www.gnu.org/licenses or write to the Free
  Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
-*/
+ */
 package com.servoy.eclipse.ui.views.solutionexplorer.actions;
 
 
@@ -41,7 +41,7 @@ import com.servoy.j2db.persistence.Solution;
 /**
  * Action to create a new form/global method depending on the selection of a solution view.
  * 
- * @author Johan Compagner
+ * @author jcompagner
  */
 public class NewRelationAction extends Action implements ISelectionChangedListener
 {
--- com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/views/solutionexplorer/actions/NewValueListAction.java
+++ com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/views/solutionexplorer/actions/NewValueListAction.java
@@ -13,7 +13,7 @@
  You should have received a copy of the GNU Affero General Public License along
  with this program; if not, see http://www.gnu.org/licenses or write to the Free
  Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
-*/
+ */
 package com.servoy.eclipse.ui.views.solutionexplorer.actions;
 
 
@@ -46,7 +46,7 @@ import com.servoy.j2db.util.IdentDocumentValidator;
 /**
  * Action to create a new valuelist depending on the selection of a solution view.
  * 
- * @author Johan Compagner
+ * @author jcompagner
  */
 public class NewValueListAction extends Action implements ISelectionChangedListener
 {
