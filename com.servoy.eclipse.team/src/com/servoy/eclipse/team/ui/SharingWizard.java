02cca9ed277683586bec9434185114051a4f63c1
Gabi Boros <gboros@servoy.com>
2010-09-13 08:32:16 +0000

    do sharing in display thread, as on finish it must have team nature in order to the multiple sharing window from eclipse  to work
    Case: 313397

--- com.servoy.eclipse.team/src/com/servoy/eclipse/team/ui/SharingWizard.java
+++ com.servoy.eclipse.team/src/com/servoy/eclipse/team/ui/SharingWizard.java
@@ -13,16 +13,10 @@
  You should have received a copy of the GNU Affero General Public License along
  with this program; if not, see http://www.gnu.org/licenses or write to the Free
  Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
-*/
+ */
 package com.servoy.eclipse.team.ui;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.WorkspaceJob;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.ISchedulingRule;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.swt.widgets.Display;
@@ -35,7 +29,6 @@ import com.servoy.eclipse.core.ServoyModelManager;
 import com.servoy.eclipse.core.ServoyProject;
 import com.servoy.eclipse.core.ServoyResourcesProject;
 import com.servoy.eclipse.core.repository.RepositoryAccessPoint;
-import com.servoy.eclipse.core.util.SerialRule;
 import com.servoy.eclipse.team.ServoyTeamProvider;
 import com.servoy.j2db.persistence.IRepository;
 import com.servoy.j2db.persistence.RootObjectMetaData;
@@ -54,9 +47,6 @@ public class SharingWizard extends Wizard implements IConfigurationWizard
 		addPage(repositoryPage);
 	}
 
-	private WorkspaceJob sharingJob;
-	private boolean shareWithExistingName;
-
 	@Override
 	public boolean performFinish()
 	{
@@ -65,87 +55,52 @@ public class SharingWizard extends Wizard implements IConfigurationWizard
 		final String user = repositoryPage.getUser();
 		final String passHash = Utils.calculateMD5HashBase64(repositoryPage.getPassword());
 
-		if (sharingJob != null)
-		{
-			sharingJob.cancel();
-		}
 
-		sharingJob = new WorkspaceJob("Sharing '" + servoyProject.getName() + "'")
+		try
 		{
-			@Override
-			public IStatus runInWorkspace(IProgressMonitor monitor) throws CoreException
+			RepositoryAccessPoint repositoryAP = RepositoryAccessPoint.getInstance(serverAddress, user, passHash);
+
+			if (servoyProject.hasNature(ServoyProject.NATURE_ID)) // servoy project
 			{
-				try
+				RootObjectMetaData solutionMetaData = repositoryAP.getRepository().getRootObjectMetaData(servoyProject.getName(), IRepository.SOLUTIONS);
+				if (solutionMetaData == null)
 				{
-					RepositoryAccessPoint repositoryAP = RepositoryAccessPoint.getInstance(serverAddress, user, passHash);
 
-					if (servoyProject.hasNature(ServoyProject.NATURE_ID)) // servoy project
-					{
-						RootObjectMetaData solutionMetaData = repositoryAP.getRepository().getRootObjectMetaData(servoyProject.getName(), IRepository.SOLUTIONS);
-						if (solutionMetaData == null)
-						{
-
-							String resourceProjectName = ServoyModelManager.getServoyModelManager().getServoyModel().getActiveResourcesProject().getProject().getName();
-
-							ServoyTeamProvider.createResourcesProject(resourceProjectName, repositoryAP, serverAddress, user, passHash);
-							ServoyTeamProvider.createSolutionProject(repositoryAP, serverAddress, user, passHash, servoyProject.getName(), 1, null, null, false);
-						}
-						else
-						{
-
-							Display.getDefault().syncExec(new Runnable()
-							{
-								public void run()
-								{
-									shareWithExistingName = MessageDialog.openQuestion(getShell(), "Servoy share", "Solution with name '" +
-										servoyProject.getName() + "' already exists in the repository. Share anyway ?");
-								}
-							});
-							if (shareWithExistingName)
-							{
-								ServoyTeamProvider.createSolutionProject(repositoryAP, serverAddress, user, passHash, servoyProject.getName(), 1, null, null,
-									false);
-							}
-						}
-					}
-					else if (servoyProject.hasNature(ServoyResourcesProject.NATURE_ID)) // style project
-					{
-						ServoyTeamProvider.createResourcesProject(servoyProject.getName(), repositoryAP, serverAddress, user, passHash);
-					}
+					String resourceProjectName = ServoyModelManager.getServoyModelManager().getServoyModel().getActiveResourcesProject().getProject().getName();
 					
-					Display.getDefault().asyncExec(new Runnable()
-					{
-						public void run()
-						{
-							ResourceDecorator rd = (ResourceDecorator)PlatformUI.getWorkbench().getDecoratorManager().getBaseLabelProvider(
-								ResourceDecorator.ID);
-							rd.fireChanged(null);
-						}
-					});
+					ServoyTeamProvider.createResourcesProject(resourceProjectName, repositoryAP, serverAddress, user, passHash);
+					ServoyTeamProvider.createSolutionProject(repositoryAP, serverAddress, user, passHash, servoyProject.getName(), 1, null, null, false);
 				}
-				catch (final Exception ex)
+				else
 				{
-					ServoyLog.logError("Cannot share '" + servoyProject.getName() + "'", ex);
-					if (monitor.isCanceled()) return Status.CANCEL_STATUS;
 
-					Display.getDefault().syncExec(new Runnable()
+					if (MessageDialog.openQuestion(getShell(), "Servoy share", "Solution with name '" + servoyProject.getName() +
+						"' already exists in the repository. Share anyway ?"))
 					{
-						public void run()
-						{
-							MessageDialog.openError(getShell(), "Error", "Cannot share '" + servoyProject.getName() + "'.\n" + ex.getMessage());
-						}
-					});
+						ServoyTeamProvider.createSolutionProject(repositoryAP, serverAddress, user, passHash, servoyProject.getName(), 1, null, null, false);
+					}
 				}
-
-				return Status.OK_STATUS;
+			}
+			else if (servoyProject.hasNature(ServoyResourcesProject.NATURE_ID)) // style project
+			{
+				ServoyTeamProvider.createResourcesProject(servoyProject.getName(), repositoryAP, serverAddress, user, passHash);
 			}
 
-		};
-		sharingJob.setUser(true);
-		ISchedulingRule rule = new SerialRule();
-		sharingJob.setRule(rule);
-		sharingJob.schedule();
+			Display.getDefault().asyncExec(new Runnable()
+			{
+				public void run()
+				{
+					ResourceDecorator rd = (ResourceDecorator)PlatformUI.getWorkbench().getDecoratorManager().getBaseLabelProvider(ResourceDecorator.ID);
+					rd.fireChanged(null);
+				}
+			});
+		}
+		catch (final Exception ex)
+		{
+			ServoyLog.logError("Cannot share '" + servoyProject.getName() + "'", ex);
+			MessageDialog.openError(getShell(), "Error", "Cannot share '" + servoyProject.getName() + "'.\n" + ex.getMessage());
 
+		}
 
 		return true;
 	}
