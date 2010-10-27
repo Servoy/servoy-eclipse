61d911fd4a97248ab55daa64c832bf871ed1547b
Gabriel Ersze <gerzse@servoy.com>
2010-10-27 17:01:54 +0000

    Made the font/color chooser buttons on the toolbar be visible all the time and just get disabled when they should not be used. Case: 319964

--- com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/actions/ShowColorChooserActionDelegate.java
+++ com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/actions/ShowColorChooserActionDelegate.java
@@ -13,7 +13,7 @@
  You should have received a copy of the GNU Affero General Public License along
  with this program; if not, see http://www.gnu.org/licenses or write to the Free
  Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
-*/
+ */
 package com.servoy.eclipse.ui.actions;
 
 import java.awt.Color;
@@ -29,90 +29,98 @@ import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.ColorDialog;
-import org.eclipse.ui.IEditorActionDelegate;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 import org.eclipse.ui.editors.text.TextEditor;
 
 import com.servoy.eclipse.core.ServoyLog;
 import com.servoy.j2db.util.PersistHelper;
 
-public class ShowColorChooserActionDelegate implements IEditorActionDelegate
+public class ShowColorChooserActionDelegate implements IWorkbenchWindowActionDelegate
 {
-	private IEditorPart editor;
+	private IWorkbenchWindow window;
 
 	public void run(IAction action)
 	{
-		if (editor != null)
+		if (window != null)
 		{
-			ColorDialog dialog = new ColorDialog(editor.getEditorSite().getShell());
-			Object value = dialog.open();
-			RGB rgb = dialog.getRGB();
-			if (rgb != null)
+			IWorkbenchPart activePart = window.getPartService().getActivePart();
+			if (activePart instanceof IEditorPart)
 			{
-				Color color = new Color(rgb.red, rgb.green, rgb.blue);
-
-				if (editor instanceof ScriptEditor)
+				IEditorPart editor = (IEditorPart)activePart;
+				ColorDialog dialog = new ColorDialog(editor.getEditorSite().getShell());
+				Object value = dialog.open();
+				RGB rgb = dialog.getRGB();
+				if (rgb != null)
 				{
-					ScriptEditor scriptEditor = (ScriptEditor)editor;
-					String editorId = scriptEditor.getSite().getId();
-					ISourceViewer sv = scriptEditor.getScriptSourceViewer();
-					if (sv == null) return;
+					Color color = new Color(rgb.red, rgb.green, rgb.blue);
 
-					IDocument document = sv.getDocument();
-					if (document == null) return;
-
-					StyledText st = sv.getTextWidget();
-					if (st == null || st.isDisposed()) return;
-
-					Point textSelection = st.getSelectionRange();
-					if (textSelection.y <= 0)
+					if (editor instanceof ScriptEditor)
 					{
-						textSelection.x = st.getCaretOffset();
-						textSelection.y = 0;
-					}
-					st.replaceTextRange(textSelection.x, textSelection.y, "'" + PersistHelper.createColorString(color) + "'");
-					st.forceFocus();
-				}
-				else if (editor instanceof TextEditor)
-				{
-					TextEditor textEditor = (TextEditor)editor;
-					IEditorInput input = textEditor.getEditorInput();
-					if (textEditor.getDocumentProvider() != null && textEditor.getDocumentProvider().getDocument(input) != null)
-					{
-						IDocument document = textEditor.getDocumentProvider().getDocument(input);
-						ISelection selection = textEditor.getSelectionProvider().getSelection();
-						if (selection instanceof TextSelection)
+						ScriptEditor scriptEditor = (ScriptEditor)editor;
+						String editorId = scriptEditor.getSite().getId();
+						ISourceViewer sv = scriptEditor.getScriptSourceViewer();
+						if (sv == null) return;
+
+						IDocument document = sv.getDocument();
+						if (document == null) return;
+
+						StyledText st = sv.getTextWidget();
+						if (st == null || st.isDisposed()) return;
+
+						Point textSelection = st.getSelectionRange();
+						if (textSelection.y <= 0)
 						{
-							try
+							textSelection.x = st.getCaretOffset();
+							textSelection.y = 0;
+						}
+						st.replaceTextRange(textSelection.x, textSelection.y, "'" + PersistHelper.createColorString(color) + "'");
+						st.forceFocus();
+					}
+					else if (editor instanceof TextEditor)
+					{
+						TextEditor textEditor = (TextEditor)editor;
+						IEditorInput input = textEditor.getEditorInput();
+						if (textEditor.getDocumentProvider() != null && textEditor.getDocumentProvider().getDocument(input) != null)
+						{
+							IDocument document = textEditor.getDocumentProvider().getDocument(input);
+							ISelection selection = textEditor.getSelectionProvider().getSelection();
+							if (selection instanceof TextSelection)
 							{
-								document.replace(((TextSelection)selection).getOffset(), ((TextSelection)selection).getLength(),
-									PersistHelper.createColorString(color));
-							}
-							catch (BadLocationException e)
-							{
-								ServoyLog.logError(e);
+								try
+								{
+									document.replace(((TextSelection)selection).getOffset(), ((TextSelection)selection).getLength(),
+										PersistHelper.createColorString(color));
+								}
+								catch (BadLocationException e)
+								{
+									ServoyLog.logError(e);
+								}
 							}
 						}
-
 					}
-
-
 				}
-
 			}
 		}
-
-	}
-
-
-	public void setActiveEditor(IAction action, IEditorPart targetEditor)
-	{
-		editor = targetEditor;
 	}
 
 	public void selectionChanged(IAction action, ISelection selection)
 	{
+		boolean enabled = false;
+		if (selection instanceof TextSelection) enabled = true;
+		action.setEnabled(enabled);
+	}
 
+	public void dispose()
+	{
+		this.window = null;
+	}
+
+	public void init(IWorkbenchWindow window)
+	{
+		this.window = window;
 	}
 }
--- com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/actions/ShowFontChooserActionDelegate.java
+++ com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/actions/ShowFontChooserActionDelegate.java
@@ -13,7 +13,7 @@
  You should have received a copy of the GNU Affero General Public License along
  with this program; if not, see http://www.gnu.org/licenses or write to the Free
  Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
-*/
+ */
 package com.servoy.eclipse.ui.actions;
 
 import java.awt.Font;
@@ -29,86 +29,96 @@ import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.FontDialog;
-import org.eclipse.ui.IEditorActionDelegate;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 import org.eclipse.ui.editors.text.TextEditor;
 
 import com.servoy.eclipse.core.ServoyLog;
 import com.servoy.j2db.util.PersistHelper;
 
-public class ShowFontChooserActionDelegate implements IEditorActionDelegate
+public class ShowFontChooserActionDelegate implements IWorkbenchWindowActionDelegate
 {
-	private IEditorPart editor;
+	private IWorkbenchWindow window;
 
 	public void run(IAction action)
 	{
-		if (editor != null)
+		if (window != null)
 		{
-			FontDialog dialog = new FontDialog(editor.getEditorSite().getShell());
-			FontData fontData = dialog.open();
-			if (fontData != null)
+			IWorkbenchPart activePart = window.getPartService().getActivePart();
+			if (activePart instanceof IEditorPart)
 			{
-				Font font = new Font(fontData.getName(), fontData.getStyle(), fontData.getHeight());
-
-				if (editor instanceof ScriptEditor)
+				IEditorPart editor = (IEditorPart)activePart;
+				FontDialog dialog = new FontDialog(editor.getEditorSite().getShell());
+				FontData fontData = dialog.open();
+				if (fontData != null)
 				{
-					ScriptEditor scriptEditor = (ScriptEditor)editor;
-					ISourceViewer sv = scriptEditor.getScriptSourceViewer();
-					if (sv == null) return;
+					Font font = new Font(fontData.getName(), fontData.getStyle(), fontData.getHeight());
 
-					IDocument document = sv.getDocument();
-					if (document == null) return;
-
-					StyledText st = sv.getTextWidget();
-					if (st == null || st.isDisposed()) return;
-
-					Point textSelection = st.getSelectionRange();
-					if (textSelection.y <= 0)
+					if (editor instanceof ScriptEditor)
 					{
-						textSelection.x = st.getCaretOffset();
-						textSelection.y = 0;
-					}
-					st.replaceTextRange(textSelection.x, textSelection.y, "'" + PersistHelper.createFontString(font) + "'");
-					st.forceFocus();
-				}
-				else if (editor instanceof TextEditor)
-				{
-					TextEditor textEditor = (TextEditor)editor;
-					IEditorInput input = textEditor.getEditorInput();
-					if (textEditor.getDocumentProvider() != null && textEditor.getDocumentProvider().getDocument(input) != null)
-					{
-						IDocument document = textEditor.getDocumentProvider().getDocument(input);
-						ISelection selection = textEditor.getSelectionProvider().getSelection();
-						if (selection instanceof TextSelection)
+						ScriptEditor scriptEditor = (ScriptEditor)editor;
+						ISourceViewer sv = scriptEditor.getScriptSourceViewer();
+						if (sv == null) return;
+
+						IDocument document = sv.getDocument();
+						if (document == null) return;
+
+						StyledText st = sv.getTextWidget();
+						if (st == null || st.isDisposed()) return;
+
+						Point textSelection = st.getSelectionRange();
+						if (textSelection.y <= 0)
 						{
-							try
+							textSelection.x = st.getCaretOffset();
+							textSelection.y = 0;
+						}
+						st.replaceTextRange(textSelection.x, textSelection.y, "'" + PersistHelper.createFontString(font) + "'");
+						st.forceFocus();
+					}
+					else if (editor instanceof TextEditor)
+					{
+						TextEditor textEditor = (TextEditor)editor;
+						IEditorInput input = textEditor.getEditorInput();
+						if (textEditor.getDocumentProvider() != null && textEditor.getDocumentProvider().getDocument(input) != null)
+						{
+							IDocument document = textEditor.getDocumentProvider().getDocument(input);
+							ISelection selection = textEditor.getSelectionProvider().getSelection();
+							if (selection instanceof TextSelection)
 							{
-								document.replace(((TextSelection)selection).getOffset(), ((TextSelection)selection).getLength(),
-									PersistHelper.createFontCssString(font));
-							}
-							catch (BadLocationException e)
-							{
-								ServoyLog.logError(e);
+								try
+								{
+									document.replace(((TextSelection)selection).getOffset(), ((TextSelection)selection).getLength(),
+										PersistHelper.createFontCssString(font));
+								}
+								catch (BadLocationException e)
+								{
+									ServoyLog.logError(e);
+								}
 							}
 						}
-
 					}
 				}
 			}
 		}
-
-	}
-
-	public void setActiveEditor(IAction action, IEditorPart targetEditor)
-	{
-		editor = targetEditor;
 	}
 
 	public void selectionChanged(IAction action, ISelection selection)
 	{
-
+		boolean enabled = false;
+		if (selection instanceof TextSelection) enabled = true;
+		action.setEnabled(enabled);
 	}
 
+	public void dispose()
+	{
+		this.window = null;
+	}
 
+	public void init(IWorkbenchWindow window)
+	{
+		this.window = window;
+	}
 }
