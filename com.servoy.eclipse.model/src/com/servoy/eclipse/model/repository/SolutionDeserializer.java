6da8f78cdbd81346e0147d822fda5e4b343c3187
Johan Compagner <jcompagner@servoy.com>
2011-01-13 16:05:40 +0000

    dont overwrite []

--- com.servoy.eclipse.model/src/com/servoy/eclipse/model/repository/SolutionDeserializer.java
+++ com.servoy.eclipse.model/src/com/servoy/eclipse/model/repository/SolutionDeserializer.java
@@ -1030,7 +1030,7 @@ public class SolutionDeserializer
 					{
 						json.put(VARIABLE_TYPE_JSON_ATTRIBUTE, IColumnTypes.MEDIA);
 						String current = json.optString(JS_TYPE_JSON_ATTRIBUTE);
-						if (current == null || !current.startsWith("Array")) json.putOpt(JS_TYPE_JSON_ATTRIBUTE, "Array");
+						if (current == null || (!current.startsWith("Array") && !current.endsWith("[]"))) json.putOpt(JS_TYPE_JSON_ATTRIBUTE, "Array");
 					}
 					else
 					{
