ecf67328cf3c8d6942ba277a0cbb9266e80ff6bb
Gabi Boros <gboros@servoy.com>
2011-01-14 12:40:29 +0000

    added platform param to integrated marketplace

--- com.servoy.eclipse.ui/src/com/servoy/eclipse/marketplace/MarketplaceBrowserEditor.java
+++ com.servoy.eclipse.ui/src/com/servoy/eclipse/marketplace/MarketplaceBrowserEditor.java
@@ -39,6 +39,7 @@ import org.eclipse.ui.part.EditorPart;
 import com.servoy.eclipse.core.util.UIUtils;
 import com.servoy.eclipse.model.util.ServoyLog;
 import com.servoy.j2db.ClientVersion;
+import com.servoy.j2db.util.Utils;
 
 /**
  * Editor used to show the Servoy Marketplace. 
@@ -53,6 +54,7 @@ public class MarketplaceBrowserEditor extends EditorPart
 	public static final String MARKETPLACE_XML_VERSION = "1.0"; //$NON-NLS-1$
 	private static final String PARAM_SERVOY_VERSION = "servoyVersion"; //$NON-NLS-1$
 	private static final String PARAM_MARKETPLACE_XML_VERSION = "marketplaceXMLVersion"; //$NON-NLS-1$ 
+	private static final String PARAM_PLATFORM = "platform"; //$NON-NLS-1$
 	public static final MarketplaceBrowserEditorInput INPUT = new MarketplaceBrowserEditorInput();
 
 	private Browser browser;
@@ -111,7 +113,7 @@ public class MarketplaceBrowserEditor extends EditorPart
 	{
 		browser = new Browser(parent, SWT.NONE);
 		browser.setUrl(
-			new StringBuffer(MARKETPLACE_URL).append("&a=&").append(PARAM_SERVOY_VERSION).append("=").append(ClientVersion.getBundleVersion()).append("&").append(PARAM_MARKETPLACE_XML_VERSION).append("=").append(MARKETPLACE_XML_VERSION).toString(), null, new String[] { "Cache-Control: no-cache" }); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
+			new StringBuffer(MARKETPLACE_URL).append("&a=&").append(PARAM_SERVOY_VERSION).append("=").append(ClientVersion.getBundleVersion()).append("&").append(PARAM_MARKETPLACE_XML_VERSION).append("=").append(MARKETPLACE_XML_VERSION).append("&").append(PARAM_PLATFORM).append("=").append(Utils.getPlatformAsString()).toString(), null, new String[] { "Cache-Control: no-cache" }); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$
 		browser.addLocationListener(new LocationAdapter()
 		{
 			@Override
