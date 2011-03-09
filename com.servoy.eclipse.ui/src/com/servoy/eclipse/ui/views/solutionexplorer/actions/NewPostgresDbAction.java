1094a10b4005567650cbe8bd43e3662e2d2208e6
Andras Lorinz <alorincz@servoy.com>
2011-03-09 09:23:38 +0000

    Move the option of creating a new database in solex to the resource node. Case: 356013

--- com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/views/solutionexplorer/actions/NewPostgresDbAction.java
+++ com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/views/solutionexplorer/actions/NewPostgresDbAction.java
@@ -17,6 +17,7 @@
 package com.servoy.eclipse.ui.views.solutionexplorer.actions;
 
 import java.sql.PreparedStatement;
+import java.util.HashMap;
 
 import org.eclipse.core.resources.WorkspaceJob;
 import org.eclipse.core.runtime.CoreException;
@@ -29,14 +30,17 @@ import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.window.Window;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.dialogs.ElementListSelectionDialog;
 
 import com.servoy.eclipse.core.ServoyModel;
 import com.servoy.eclipse.model.util.ServoyLog;
 import com.servoy.eclipse.ui.Activator;
 import com.servoy.eclipse.ui.node.SimpleUserNode;
+import com.servoy.eclipse.ui.node.UserNodeType;
 import com.servoy.eclipse.ui.util.EditorUtil;
 import com.servoy.eclipse.ui.views.solutionexplorer.SolutionExplorerView;
 import com.servoy.j2db.persistence.IServerInternal;
@@ -71,13 +75,9 @@ public class NewPostgresDbAction extends Action implements ISelectionChangedList
 		if (sel.size() == 1)
 		{
 			SimpleUserNode node = (SimpleUserNode)sel.getFirstElement();
-			if (node.getRealObject() instanceof IServerInternal)
+			if (node.getRealType() == UserNodeType.SERVERS)
 			{
-				IServerInternal s = (IServerInternal)node.getRealObject();
-				if (s.getConfig().isPostgresDriver())
-				{
-					state = true;
-				}
+				state = true;
 			}
 		}
 		setEnabled(state);
@@ -86,113 +86,139 @@ public class NewPostgresDbAction extends Action implements ISelectionChangedList
 	@Override
 	public void run()
 	{
-		SimpleUserNode node = viewer.getSelectedTreeNode();
-		if (node.getRealObject() instanceof IServerInternal)
+		ServerConfig[] serverConfigs = ServoyModel.getServerManager().getServerConfigs();
+		HashMap<String, ServerConfig> serverMap = new HashMap<String, ServerConfig>();
+		for (ServerConfig sc : serverConfigs)
+		{
+			String serverURL = sc.getServerUrl();
+			serverURL = serverURL.replaceFirst(".*//", "");
+			serverURL = serverURL.replaceFirst("/.*", "");
+			serverMap.put(serverURL, sc);
+		}
+
+		if (serverMap.isEmpty())
+		{
+			MessageDialog.openInformation(viewer.getSite().getShell(), "Info", "No existing database connection, at least one is needed to create a new one.");
+		}
+
+		Object[] dbSelection = null;
+
+		if (serverMap.size() > 1)
+		{
+			ElementListSelectionDialog selectDBServerDialog = new ElementListSelectionDialog(viewer.getViewSite().getShell(), new LabelProvider());
+			selectDBServerDialog.setElements(serverMap.keySet().toArray());
+			selectDBServerDialog.setTitle("Select database server");
+			selectDBServerDialog.setMultipleSelection(false);
+			int dbSelectRes = selectDBServerDialog.open();
+			if (dbSelectRes == Window.OK)
+			{
+				dbSelection = selectDBServerDialog.getResult();
+			}
+			else return;
+		}
+		else
+		{
+			dbSelection = new String[1];
+			dbSelection[0] = serverMap.keySet().toArray()[0];
+		}
+
+		final InputDialog nameDialog = new InputDialog(viewer.getViewSite().getShell(), "Create PostgreSQL Database File", "Supply database name", "",
+			new IInputValidator()
+			{
+				public String isValid(String newText)
+				{
+					boolean valid = IdentDocumentValidator.isJavaIdentifier(newText);
+					return valid ? null : (newText.length() == 0 ? "" : "Invalid database name");
+				}
+			});
+		int dbNameRes = nameDialog.open();
+
+		final IServerInternal serverPrototype = (IServerInternal)ServoyModel.getServerManager().getServer(serverMap.get(dbSelection[0]).getServerName());
+		if (dbNameRes == Window.OK)
 		{
 			try
 			{
-				final IServerInternal s = (IServerInternal)node.getRealObject();
-				final InputDialog nameDialog = new InputDialog(viewer.getViewSite().getShell(), "Create PostgreSQL Database File", "Supply database name", "",
-					new IInputValidator()
-					{
-						public String isValid(String newText)
-						{
-							boolean valid = IdentDocumentValidator.isJavaIdentifier(newText);
-							return valid ? null : (newText.length() == 0 ? "" : "Invalid database name");
-						}
-					});
-				int res = nameDialog.open();
-				if (res == Window.OK)
+				final String name = nameDialog.getValue();
+				WorkspaceJob createDbJob = new WorkspaceJob("Creating PostgreSQL database") //$NON-NLS-1$
 				{
-					try
+					@Override
+					public IStatus runInWorkspace(IProgressMonitor monitor) throws CoreException
 					{
-						final String name = nameDialog.getValue();
-						WorkspaceJob createDbJob = new WorkspaceJob("Creating PostgreSQL database") //$NON-NLS-1$
+						ITransactionConnection connection = null;
+						PreparedStatement ps = null;
+						monitor.beginTask("Creating PostgreSQL database...", 1);
+						try
 						{
-							@Override
-							public IStatus runInWorkspace(IProgressMonitor monitor) throws CoreException
+							connection = serverPrototype.getUnmanagedConnection();
+							ps = connection.prepareStatement("CREATE DATABASE \"" + name + "\" WITH ENCODING 'UNICODE';");
+							ps.execute();
+							ps.close();
+							ps = null;
+							Display.getDefault().syncExec(new Runnable()
 							{
-								ITransactionConnection connection = null;
-								PreparedStatement ps = null;
-								monitor.beginTask("Creating PostgreSQL database...", 1);
-								try
+								public void run()
 								{
-									connection = s.getUnmanagedConnection();
-									ps = connection.prepareStatement("CREATE DATABASE \"" + name + "\" WITH ENCODING 'UNICODE';");
-									ps.execute();
-									ps.close();
-									ps = null;
-									Display.getDefault().syncExec(new Runnable()
-									{
-										public void run()
-										{
-											MessageDialog.openInformation(viewer.getSite().getShell(), "Info", "PostgreSQL database created.");
-										}
-									});
+									MessageDialog.openInformation(viewer.getSite().getShell(), "Info", "PostgreSQL database created.");
 								}
-								catch (Exception e)
-								{
-									ServoyLog.logError(e);
-									displayError("Could not create database: " + e.getMessage());
-								}
-								finally
-								{
-									Utils.closeConnection(connection);
-									Utils.closeStatement(ps);
-								}
-								try
-								{
-									ServerConfig origConfig = s.getConfig();
-									String dbname = null;
-									String serverUrl = origConfig.getServerUrl();
-									int startIndex = serverUrl.lastIndexOf("/");
-									int endIndex = serverUrl.indexOf("?", startIndex);
-									if (endIndex == -1) endIndex = serverUrl.length();
-									dbname = serverUrl.substring(startIndex + 1, endIndex);
-									if (dbname != null) serverUrl = serverUrl.replaceFirst("/" + dbname, "/" + name); //$NON-NLS-1$ //$NON-NLS-2$
-									if (serverUrl.equals(origConfig.getServerUrl()))
-									{
-										// hmm, no replace, fall back to default
-										serverUrl = "jdbc:postgresql://localhost/" + name;
-									}
-
-									IServerManagerInternal serverManager = ServoyModel.getServerManager();
-									String configName = name;
-									for (int i = 1; serverManager.getServerConfig(configName) != null && i < 100; i++)
-									{
-										configName = name + i;
-									}
-									serverManager.getServerConfig(name);
-									final ServerConfig serverConfig = new ServerConfig(configName, origConfig.getUserName(), origConfig.getPassword(),
-										serverUrl, origConfig.getDriver(), origConfig.getCatalog(), origConfig.getSchema(), origConfig.getMaxActive(),
-										origConfig.getMaxIdle(), origConfig.getMaxPreparedStatementsIdle(), origConfig.getConnectionValidationType(),
-										origConfig.getValidationQuery(), null, true, false);
-									serverManager.saveServerConfig(null, serverConfig);
-									Display.getDefault().asyncExec(new Runnable()
-									{
-										public void run()
-										{
-											EditorUtil.openServerEditor(serverConfig);
-										}
-									});
-								}
-								catch (Exception e)
-								{
-									ServoyLog.logError(e);
-								}
-								return Status.OK_STATUS;
+							});
+						}
+						catch (Exception e)
+						{
+							ServoyLog.logError(e);
+							displayError("Could not create database: " + e.getMessage());
+						}
+						finally
+						{
+							Utils.closeConnection(connection);
+							Utils.closeStatement(ps);
+						}
+						try
+						{
+							ServerConfig origConfig = serverPrototype.getConfig();
+							String dbname = null;
+							String serverUrl = origConfig.getServerUrl();
+							int startIndex = serverUrl.lastIndexOf("/");
+							int endIndex = serverUrl.indexOf("?", startIndex);
+							if (endIndex == -1) endIndex = serverUrl.length();
+							dbname = serverUrl.substring(startIndex + 1, endIndex);
+							if (dbname != null) serverUrl = serverUrl.replaceFirst("/" + dbname, "/" + name); //$NON-NLS-1$ //$NON-NLS-2$
+							if (serverUrl.equals(origConfig.getServerUrl()))
+							{
+								// hmm, no replace, fall back to default
+								serverUrl = "jdbc:postgresql://localhost/" + name;
 							}
-						};
 
-						createDbJob.setRule(ServoyModel.getWorkspace().getRoot());
-						createDbJob.setUser(true); // we want the progress to be visible in a dialog, not to stay in the status bar
-						createDbJob.schedule();
+							IServerManagerInternal serverManager = ServoyModel.getServerManager();
+							String configName = name;
+							for (int i = 1; serverManager.getServerConfig(configName) != null && i < 100; i++)
+							{
+								configName = name + i;
+							}
+							serverManager.getServerConfig(name);
+							final ServerConfig serverConfig = new ServerConfig(configName, origConfig.getUserName(), origConfig.getPassword(), serverUrl,
+								origConfig.getDriver(), origConfig.getCatalog(), origConfig.getSchema(), origConfig.getMaxActive(), origConfig.getMaxIdle(),
+								origConfig.getMaxPreparedStatementsIdle(), origConfig.getConnectionValidationType(), origConfig.getValidationQuery(), null,
+								true, false);
+							serverManager.saveServerConfig(null, serverConfig);
+							Display.getDefault().asyncExec(new Runnable()
+							{
+								public void run()
+								{
+									EditorUtil.openServerEditor(serverConfig);
+								}
+							});
+						}
+						catch (Exception e)
+						{
+							ServoyLog.logError(e);
+						}
+						return Status.OK_STATUS;
 					}
-					catch (Exception e)
-					{
-						ServoyLog.logError(e);
-					}
-				}
+				};
+
+				createDbJob.setRule(ServoyModel.getWorkspace().getRoot());
+				createDbJob.setUser(true); // we want the progress to be visible in a dialog, not to stay in the status bar
+				createDbJob.schedule();
 			}
 			catch (Exception e)
 			{
