28fe8cc5d6d6a362e21614304beefdbdd3dab1bf
Andras Lorinz <alorincz@servoy.com>
2011-06-09 08:12:32 +0000

    In some situation (probably os and machine dependent) the "..." button doesn't do anything when trying to open the text editor for a button or label. Case: 390297

--- com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/editors/TextDialogCellEditor.java
+++ com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/editors/TextDialogCellEditor.java
@@ -13,7 +13,7 @@
  You should have received a copy of the GNU Affero General Public License along
  with this program; if not, see http://www.gnu.org/licenses or write to the Free
  Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
-*/
+ */
 package com.servoy.eclipse.ui.editors;
 
 
@@ -217,9 +217,8 @@ public abstract class TextDialogCellEditor extends TextCellEditor
 		editing = true;
 		try
 		{
-			button.removeFocusListener(getButtonFocusListener());
+			setIgnoreFocusLost(true);
 			Object newValue = openDialogBox(control);
-			button.addFocusListener(getButtonFocusListener());
 			if (!CANCELVALUE.equals(newValue))
 			{
 				boolean newValidState = isCorrect(newValue);
