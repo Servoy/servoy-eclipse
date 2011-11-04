97a09bc91518e4b5aeadeb126f126ed4ec2bb7f9
Gabi Boros <gboros@servoy.com>
2011-11-04 11:40:26 +0000

    more fix on style & template charset when loading
    Case: SVY-958

--- com.servoy.eclipse.model/src/com/servoy/eclipse/model/repository/StringResourceDeserializer.java
+++ com.servoy.eclipse.model/src/com/servoy/eclipse/model/repository/StringResourceDeserializer.java
@@ -18,7 +18,6 @@ package com.servoy.eclipse.model.repository;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -205,7 +204,7 @@ public class StringResourceDeserializer
 		File contentFile = new File(wsd, getStringResourceContentFilePath(resourcesProjectName, resource.getName(), romd.getObjectTypeId()));
 		if (contentFile.exists())
 		{
-			resource.loadFromFile(contentFile, Charset.forName("UTF8"));
+			resource.loadFromFile(contentFile);
 		}
 		resource.setChangeHandler(new EclipseChangeHandler(eclipseRepository));
 		return resource;
