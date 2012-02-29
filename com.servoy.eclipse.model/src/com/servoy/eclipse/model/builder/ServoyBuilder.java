73722d99db83e50960d62be4055e97c8e4acf152
Laurian Vostinar <lvostinar@servoy.com>
2012-02-29 11:33:20 +0000

    Referencing modules from a post-import hook module results in a warning " Module xxxx is a solution import hook, so it should not contain any modules." Case: SVY-1537

--- com.servoy.eclipse.model/src/com/servoy/eclipse/model/builder/ServoyBuilder.java
+++ com.servoy.eclipse.model/src/com/servoy/eclipse/model/builder/ServoyBuilder.java
@@ -813,7 +813,7 @@ public class ServoyBuilder extends IncrementalProjectBuilder
 				}
 
 				// import hook modules should not contain other modules
-				if (SolutionMetaData.isImportHook(servoyProject.getSolution().getSolutionMetaData()) && modulesNames.length > 0)
+				if (SolutionMetaData.isPreImportHook(servoyProject.getSolution()) && modulesNames.length > 0)
 				{
 					String message = "Module " + servoyProject.getSolution().getName() + " is a solution import hook, so it should not contain any modules."; //$NON-NLS-1$//$NON-NLS-2$
 					addMarker(project, MISPLACED_MODULES_MARKER_TYPE, message, -1, MODULE_MISPLACED, IMarker.PRIORITY_LOW, null, null);
