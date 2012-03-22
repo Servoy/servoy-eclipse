98b6dfe083e47ae4b10d940468432f2b0085affc
Andrei Costescu <acostescu@servoy.com>
2012-03-22 16:11:20 +0000

    1 char NEWLINE instead of 2 and hopefully solved a "Cannot find marker with id..." exception.

--- com.servoy.eclipse.core/src/com/servoy/eclipse/core/quickfix/dbi/TableDifferenceQuickFix.java
+++ com.servoy.eclipse.core/src/com/servoy/eclipse/core/quickfix/dbi/TableDifferenceQuickFix.java
@@ -13,7 +13,7 @@
  You should have received a copy of the GNU Affero General Public License along
  with this program; if not, see http://www.gnu.org/licenses or write to the Free
  Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
-*/
+ */
 package com.servoy.eclipse.core.quickfix.dbi;
 
 import java.util.ArrayList;
@@ -41,6 +41,8 @@ public abstract class TableDifferenceQuickFix extends WorkbenchMarkerResolution
 
 	public static TableDifference getTableDifference(IMarker marker)
 	{
+		if (!marker.exists()) return null;
+
 		TableDifference difference = null;
 		try
 		{
