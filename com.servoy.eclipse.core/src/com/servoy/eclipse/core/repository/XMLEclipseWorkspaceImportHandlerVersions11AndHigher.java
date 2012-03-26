12f07f46df615aa2aa3efb434e15e32c09d05f31
Laurian Vostinar <lvostinar@servoy.com>
2012-03-26 12:06:43 +0000

    Protected solutions cannot overwrite existing solutions in the workspace
    Case: SVY-1834

--- com.servoy.eclipse.core/src/com/servoy/eclipse/core/repository/XMLEclipseWorkspaceImportHandlerVersions11AndHigher.java
+++ com.servoy.eclipse.core/src/com/servoy/eclipse/core/repository/XMLEclipseWorkspaceImportHandlerVersions11AndHigher.java
@@ -457,16 +457,6 @@ public class XMLEclipseWorkspaceImportHandlerVersions11AndHigher implements IXML
 		if (typeId == IRepository.SOLUTIONS)
 		{
 			rootObjectMetaData = repository.getRootObjectMetaData(uuid);
-			if (rootObjectMetaData != null)
-			{
-				if (importInfo.isProtected)
-				{
-					// Solution is protected; cannot rename.
-					throw new RepositoryException("The " + objectType + " '" + rootObjectMetaData.getName() +
-						"' already exists in the repository with the name '" + name + "', but the import is protected. Remove the " + objectType +
-						" from the workspace.");
-				}
-			}
 		}
 		else if (typeId == IRepository.STYLES && resourcesProject != null)
 		{
@@ -484,12 +474,6 @@ public class XMLEclipseWorkspaceImportHandlerVersions11AndHigher implements IXML
 				{
 					if (meta.getRootObjectUuid().equals(uuid))
 					{
-						if (importInfo.isProtected)
-						{
-							// Solution is protected; cannot rename.
-							throw new RepositoryException("The " + objectType + " '" + meta.getName() + "' already exists in the repository with the name '" +
-								name + "', but the import is protected. Remove the " + objectType + " from the workspace.");
-						}
 						rootObjectMetaData = meta;
 					}
 				}
@@ -517,7 +501,8 @@ public class XMLEclipseWorkspaceImportHandlerVersions11AndHigher implements IXML
 				if (rootObjectMetaData != null && importInfo.isProtected)
 				{
 					throw new RepositoryException("A different " + objectType + " with the name '" + name +
-						"' already exists, but the import is protected. Rename or remove the " + objectType + " in the repository.");
+						"' already exists, but the import is protected. Rename or remove the " + objectType +
+						" in the workspace (Servoy cannot automatically rename a protected solution).");
 				}
 
 			}
@@ -535,7 +520,8 @@ public class XMLEclipseWorkspaceImportHandlerVersions11AndHigher implements IXML
 				if (rootObjectMetaData != null && importInfo.isProtected)
 				{
 					throw new RepositoryException("A different " + objectType + " with the name '" + name +
-						"' already exists, but the import is protected. Rename or remove the " + objectType + " in the repository.");
+						"' already exists, but the import is protected. Rename or remove the " + objectType +
+						" in the workspace (Servoy cannot automatically rename a protected style).");
 				}
 			}
 
