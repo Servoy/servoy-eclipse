6b5e389d81378f5db775358d8edf0288dfe188a5
Rob Gansevles <rgansevles@servoy.com>
2012-06-05 13:37:54 +0000

    When form has bg image, it should show in form editor, do not paint white in that case Case: SVY-2312

--- com.servoy.eclipse.designer/src/com/servoy/eclipse/designer/internal/core/PartImageNotifier.java
+++ com.servoy.eclipse.designer/src/com/servoy/eclipse/designer/internal/core/PartImageNotifier.java
@@ -55,9 +55,6 @@ public class PartImageNotifier extends AbstractImageNotifier
 		StyledEnablePanel comp = new StyledEnablePanel(application);
 		comp.setSize(form.getWidth(), part.getHeight() - form.getPartStartYPos(part.getID()));
 
-		Color bg = ComponentFactory.getPartBackground(application, part, form);
-		comp.setBackground(bg == null && !form.getTransparent() ? Color.white : bg);
-
 		Pair<IStyleSheet, IStyleRule> formStyle = ComponentFactory.getCSSPairStyleForForm(application, form);
 		boolean formHasBgImage = formStyle != null && formStyle.getRight() != null &&
 			formStyle.getRight().hasAttribute(CSS.Attribute.BACKGROUND_IMAGE.toString());
@@ -72,6 +69,9 @@ public class PartImageNotifier extends AbstractImageNotifier
 		{
 			comp.setPaintBackgroundOnTopOfFormImage(true);
 		}
+
+		Color bg = ComponentFactory.getPartBackground(application, part, form);
+		comp.setBackground(bg == null && !formHasBgImage && !form.getTransparent() ? Color.white : bg);
 
 		comp.setOpaque(!form.getTransparent() && !formHasBgImage);
 
