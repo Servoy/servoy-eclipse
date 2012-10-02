f047ca26bd2e93da3609d0366867c6fa4b9b10cf
Rob Gansevles <rgansevles@servoy.com>
2012-10-02 10:58:45 +0000

    Some utility functions used by mobile form editor Case: SVY-2851

--- com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/property/DelegatePropertyController.java
+++ com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/property/DelegatePropertyController.java
@@ -13,7 +13,7 @@
  You should have received a copy of the GNU Affero General Public License along
  with this program; if not, see http://www.gnu.org/licenses or write to the Free
  Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
-*/
+ */
 package com.servoy.eclipse.ui.property;
 
 import org.eclipse.core.runtime.IAdaptable;
@@ -37,10 +37,15 @@ public class DelegatePropertyController<P, E> extends PropertyController<P, E> i
 {
 	protected final IPropertyDescriptor propertyDescriptor;
 
+	public DelegatePropertyController(IPropertyDescriptor propertyDescriptor, Object id, String displayName)
+	{
+		super(id, displayName);
+		this.propertyDescriptor = propertyDescriptor;
+	}
+
 	public DelegatePropertyController(IPropertyDescriptor propertyDescriptor, Object id)
 	{
-		super(id, propertyDescriptor.getDisplayName());
-		this.propertyDescriptor = propertyDescriptor;
+		this(propertyDescriptor, id, propertyDescriptor.getDisplayName());
 	}
 
 	public DelegatePropertyController(IPropertyDescriptor propertyDescriptor)
