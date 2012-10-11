ecd71cbfd7a2e6d999905244653136f3a5dbf285
Johan Compagner <jcompagner@servoy.com>
2012-10-11 12:13:34 +0000

    latest template for
    FormScope is only initalized (form variables are set) when called through scripting (forms.xxxx) not through java
    and
    
    make scopes lazy just like form scope (except globals itself that one should load)
    Case: SVY-3196,MC-74

--- com.servoy.eclipse.exporter.mobile/src/com/servoy/eclipse/mobileexporter/export/resources/solution.js
+++ com.servoy.eclipse.exporter.mobile/src/com/servoy/eclipse/mobileexporter/export/resources/solution.js
@@ -6,37 +6,18 @@ if (typeof(_ServoyInit_) == "undefined") {
 			
 			// define all the forms
 ${loop_forms}			Object.defineProperty(forms, "${formName}", {get: function() {
-				var _$form$ = _ServoyUtils_.getFormScope("${formName}");
-				if (!_$form$._$initialized$) {
-${loop_functions}					var ${functionName} = _ServoyUtils_.wrapFunction(${functionCode},_$form$);
-			        _$form$.${functionName} = ${functionName};
-${endloop_functions}					
-${loop_variables}			        _ServoyUtils_.defineVariable(_$form$,"${variableName}",${defaultValue},${variableType});
-${endloop_variables}
-					// define standard things (controller,foundset,elements)
-					_ServoyUtils_.defineStandardFormVariables(_$form$);
-										
-			        _$form$._$initialized$ = true;
-			     }
-			     return _$form$;
+				return _ServoyUtils_.getFormScope("${formName}");
 			  }, configurable: false});
 ${endloop_forms}			
 		    // create top level global scope.
 			scopes = { };
 			
-${loop_scopes}			var _$createScope$ = function() {
-				scopes.${scopeName} = _ServoyUtils_.getGlobalScope("${scopeName}");
-				
-${loop_functions}				var ${functionName} = _ServoyUtils_.wrapFunction( ${functionCode} ,scopes.${scopeName});
-				scopes.${scopeName}.${functionName} = ${functionName};
-${endloop_functions}				
-${loop_variables}				_ServoyUtils_.defineVariable(scopes.${scopeName},"${variableName}",${defaultValue},${variableType});
-${endloop_variables}				
-				return 
-			};
-			_$createScope$();
+			// define all scopes			
+${loop_scopes}			Object.defineProperty(scopes, "${scopeName}", {get: function() {
+				return _ServoyUtils_.getGlobalScope("${scopeName}");
+			  }, configurable: false});
 ${endloop_scopes}			
-			// support the 'old' notation "globals.xxx" without scopes.
+			// support the 'old' notation "globals.xxx" without scopes. (and init the standard globals directly)
 			globals = scopes.globals;
 
 			// define all variables on the toplevel, scope and form
@@ -45,6 +26,33 @@ ${endloop_scopes}
 			for(var i = 0;i<windowVariablesArray.length;i++) {
 				_ServoyUtils_.defineWindowVariable(windowVariablesArray[i]);
 			}
-		}	
+		},
+		
+		forms: {
+${loop_forms}		
+				_$${formName}$: function(_$form$) {
+${loop_functions}					var ${functionName} = _ServoyUtils_.wrapFunction(${functionCode},_$form$);
+			        _$form$.${functionName} = ${functionName};
+${endloop_functions}					
+${loop_variables}			        _ServoyUtils_.defineVariable(_$form$,"${variableName}",${defaultValue},${variableType});
+${endloop_variables}
+					// define standard things (controller,foundset,elements)
+					_ServoyUtils_.defineStandardFormVariables(_$form$);
+			     }
+${endloop_forms}			     
+		},
+		
+		scopes: {
+${loop_scopes}		
+				_$${scopeName}$: function(_$scope$) {		
+${loop_functions}				var ${functionName} = _ServoyUtils_.wrapFunction( ${functionCode} ,_$scope$);
+				_$scope$.${functionName} = ${functionName};
+${endloop_functions}				
+${loop_variables}				_ServoyUtils_.defineVariable(_$scope$,"${variableName}",${defaultValue},${variableType});
+${endloop_variables}				
+			};
+${endloop_scopes}			
+		
+		}
 	}
 }
