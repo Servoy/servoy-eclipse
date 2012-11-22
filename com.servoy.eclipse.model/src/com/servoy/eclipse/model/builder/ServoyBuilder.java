a4eaf9d58e62026c7afe67c781bd8be91828494c
Andrei Costache <acostache@servoy.com>
2012-11-22 15:19:04 +0000

    Same dbi markers sometimes errors sometimes warnings
    - caching datasourcecollectorvisitor
    Case: SVY-3028

--- com.servoy.eclipse.model/src/com/servoy/eclipse/model/builder/ServoyBuilder.java
+++ com.servoy.eclipse.model/src/com/servoy/eclipse/model/builder/ServoyBuilder.java
@@ -3559,13 +3559,30 @@ public class ServoyBuilder extends IncrementalProjectBuilder
 		}
 	}
 
+	private static DataSourceCollectorVisitor dataSourceCollectorVisitor;
+
+	public static DataSourceCollectorVisitor getDataSourceCollectorVisitor()
+	{
+		if (dataSourceCollectorVisitor == null)
+		{
+			dataSourceCollectorVisitor = new DataSourceCollectorVisitor();
+			for (ServoyProject sp : ServoyModelFinder.getServoyModel().getModulesOfActiveProject())
+			{
+				sp.getSolution().acceptVisitor(dataSourceCollectorVisitor);
+			}
+		}
+		return dataSourceCollectorVisitor;
+	}
+
 	public void refreshDBIMarkers()
 	{
 		// do not delete or add dbi marker here
-		DataSourceCollectorVisitor datasourceCollector = new DataSourceCollectorVisitor();
-		for (ServoyProject sp : getServoyModel().getModulesOfActiveProject())
+
+		// refresh also the dataSourceCollectorVisitor
+		if (dataSourceCollectorVisitor == null) dataSourceCollectorVisitor = new DataSourceCollectorVisitor();
+		for (ServoyProject sp : ServoyModelFinder.getServoyModel().getModulesOfActiveProject())
 		{
-			sp.getSolution().acceptVisitor(datasourceCollector);
+			sp.getSolution().acceptVisitor(dataSourceCollectorVisitor);
 		}
 
 		ServoyResourcesProject resourcesProject = getServoyModel().getActiveResourcesProject();
@@ -3583,7 +3600,7 @@ public class ServoyBuilder extends IncrementalProjectBuilder
 						if (serverName != null && tableName != null)
 						{
 							String datasource = DataSourceUtils.createDBTableDataSource(serverName, tableName);
-							if (!datasourceCollector.getDataSources().contains(datasource))
+							if (!dataSourceCollectorVisitor.getDataSources().contains(datasource))
 							{
 								Object markerSeverity = marker.getAttribute(IMarker.SEVERITY);
 								if (markerSeverity == null || (markerSeverity != null && ((Integer)markerSeverity).intValue() > IMarker.SEVERITY_WARNING))
