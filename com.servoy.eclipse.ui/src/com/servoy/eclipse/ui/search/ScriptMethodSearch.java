9514fed9dea51d30b47f1305536843b3cb5f1054
Johan Compagner <jcompagner@servoy.com>
2013-02-18 09:34:58 +0000

    A search for references on global variable doesn't return the results regarding the lookup value property on table column level
    also added *.val to the script method search so that global method valuelists are also found
    Case: SVY-3848

--- com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/search/ScriptMethodSearch.java
+++ com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/search/ScriptMethodSearch.java
@@ -68,7 +68,7 @@ public class ScriptMethodSearch extends DLTKSearchEngineSearch
 		IResource[] scopes = getScopes((Solution)method.getRootObject());
 		final TextSearchRequestor collector = getResultCollector();
 
-		FileTextSearchScope scope = FileTextSearchScope.newSearchScope(scopes, new String[] { "*.frm", "*.tbl" }, true);
+		FileTextSearchScope scope = FileTextSearchScope.newSearchScope(scopes, new String[] { "*.frm", "*.tbl", "*.val" }, true);
 		TextSearchEngine.create().search(scope, collector, createSearchPattern(method.getUUID().toString()), monitor);
 
 		if (method.getParent() instanceof Solution)
