f8f59058d8032fea4e33259162589d9653a2f775
Gabi Boros <gboros@servoy.com>
2013-03-12 14:56:58 +0000

    fix : When doing a WAR exporting, the JDBC driver for im-mem databases isn't automatically added, but is required by some of Servoy internal code Case: SVY-4090

--- com.servoy.eclipse.exporter.war/src/com/servoy/eclipse/warexporter/ui/wizard/DirectorySelectionPage.java
+++ com.servoy.eclipse.exporter.war/src/com/servoy/eclipse/warexporter/ui/wizard/DirectorySelectionPage.java
@@ -19,9 +19,14 @@ package com.servoy.eclipse.warexporter.ui.wizard;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.eclipse.jface.viewers.CheckStateChangedEvent;
 import org.eclipse.jface.viewers.CheckboxTableViewer;
+import org.eclipse.jface.viewers.ICheckStateListener;
+import org.eclipse.jface.viewers.ICheckable;
 import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.wizard.IWizardPage;
@@ -35,16 +40,19 @@ import org.eclipse.swt.widgets.Composite;
  * @author jcompagner
  * @since 6.1
  */
-public class DirectorySelectionPage extends WizardPage
+public class DirectorySelectionPage extends WizardPage implements ICheckStateListener
 {
+	private static final String REQUIRED_LABEL = " (required)";
+
 	private final File directory;
 	private final IWizardPage nextPage;
 	private final List<String> files;
+	private final String[] requiredFiles;
 	private CheckboxTableViewer checkboxTableViewer;
 
-	public DirectorySelectionPage(String pagename, String title, String description, File directory, List<String> files)
+	public DirectorySelectionPage(String pagename, String title, String description, File directory, List<String> files, String[] requiredFiles)
 	{
-		this(pagename, title, description, directory, files, null);
+		this(pagename, title, description, directory, files, requiredFiles, null);
 	}
 
 	/**
@@ -54,11 +62,13 @@ public class DirectorySelectionPage extends WizardPage
 	 * @param pluginDir
 	 * @param beanSelectionPage
 	 */
-	public DirectorySelectionPage(String pagename, String title, String description, File directory, List<String> files, IWizardPage nextPage)
+	public DirectorySelectionPage(String pagename, String title, String description, File directory, List<String> files, String[] requiredFiles,
+		IWizardPage nextPage)
 	{
 		super(pagename);
 		this.directory = directory;
 		this.files = files;
+		this.requiredFiles = requiredFiles;
 		this.nextPage = nextPage;
 		setTitle(title);
 		setDescription(description);
@@ -71,29 +81,33 @@ public class DirectorySelectionPage extends WizardPage
 		container.setLayout(new FillLayout(SWT.HORIZONTAL));
 
 		checkboxTableViewer = CheckboxTableViewer.newCheckList(container, SWT.BORDER | SWT.FULL_SELECTION);
-
 		checkboxTableViewer.setContentProvider(new DirectoryContentProvider());
 		checkboxTableViewer.setInput(directory);
+		if (requiredFiles != null && requiredFiles.length > 0) checkboxTableViewer.addCheckStateListener(this);
 		if (files.size() == 0)
 		{
 			checkboxTableViewer.setAllChecked(true);
 		}
 		else
 		{
-			checkboxTableViewer.setCheckedElements(files.toArray());
+			checkboxTableViewer.setCheckedElements(appendRequiredLabel(files.toArray()));
 		}
-
 	}
 
 
 	public void storeInput()
 	{
 		files.clear();
-		Object[] checkedElements = checkboxTableViewer.getCheckedElements();
-		for (Object object : checkedElements)
-		{
-			files.add(object.toString());
-		}
+		String[] checkedElements = removeRequiredLabel(checkboxTableViewer.getCheckedElements());
+		for (String el : checkedElements)
+			files.add(el);
+	}
+
+	@Override
+	public void dispose()
+	{
+		super.dispose();
+		if (checkboxTableViewer != null) checkboxTableViewer.removeCheckStateListener(this);
 	}
 
 	/*
@@ -106,6 +120,39 @@ public class DirectorySelectionPage extends WizardPage
 	{
 		if (nextPage != null) return nextPage;
 		return super.getNextPage();
+	}
+
+	private String[] appendRequiredLabel(Object[] fileNames)
+	{
+		ArrayList<String> appendRequiredFileNames = new ArrayList<String>();
+		List<String> requiredFilesAsList = requiredFiles != null && requiredFiles.length > 0 ? Arrays.asList(requiredFiles) : null;
+		String sFileName;
+		for (Object fileName : fileNames)
+		{
+			sFileName = fileName.toString();
+			if (requiredFilesAsList != null && requiredFilesAsList.indexOf(sFileName) != -1) appendRequiredFileNames.add(0, sFileName + REQUIRED_LABEL);
+			else appendRequiredFileNames.add(sFileName);
+		}
+		return appendRequiredFileNames.toArray(new String[appendRequiredFileNames.size()]);
+
+	}
+
+	private String[] removeRequiredLabel(Object[] fileNames)
+	{
+		ArrayList<String> removeRequiedFileNames = new ArrayList<String>();
+		List<String> requiredFilesAsList = requiredFiles != null && requiredFiles.length > 0 ? Arrays.asList(requiredFiles) : null;
+		String sFileName, requiredFileName;
+		for (Object fileName : fileNames)
+		{
+			sFileName = fileName.toString();
+			if (requiredFilesAsList != null && sFileName.endsWith(REQUIRED_LABEL))
+			{
+				requiredFileName = sFileName.substring(0, sFileName.length() - REQUIRED_LABEL.length());
+				if (requiredFilesAsList.indexOf(requiredFileName) != -1) sFileName = requiredFileName;
+			}
+			removeRequiedFileNames.add(sFileName);
+		}
+		return removeRequiedFileNames.toArray(new String[removeRequiedFileNames.size()]);
 	}
 
 	private class DirectoryContentProvider implements IStructuredContentProvider
@@ -122,17 +169,30 @@ public class DirectorySelectionPage extends WizardPage
 		{
 			if (inputElement instanceof File)
 			{
-				return ((File)inputElement).list(new FilenameFilter()
+				String[] fileNames = ((File)inputElement).list(new FilenameFilter()
 				{
 
 					public boolean accept(File dir, String name)
 					{
-						return name.toLowerCase().endsWith(".jar") || name.toLowerCase().endsWith(".zip"); //$NON-NLS-1$
+						return name.toLowerCase().endsWith(".jar") || name.toLowerCase().endsWith(".zip"); //$NON-NLS-1$ //$NON-NLS-2$
 					}
 				});
+				return appendRequiredLabel(fileNames);
 			}
 			return null;
 		}
+	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.jface.viewers.ICheckStateListener#checkStateChanged(org.eclipse.jface.viewers.CheckStateChangedEvent)
+	 */
+	@Override
+	public void checkStateChanged(CheckStateChangedEvent event)
+	{
+		ICheckable checkable = event.getCheckable();
+		for (String requiredFile : requiredFiles)
+			if (!checkable.getChecked(requiredFile + REQUIRED_LABEL)) checkable.setChecked(requiredFile + REQUIRED_LABEL, true);
 	}
 }
