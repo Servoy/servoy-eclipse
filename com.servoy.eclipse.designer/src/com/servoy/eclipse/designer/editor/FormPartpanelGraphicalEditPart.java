6c36709a92832b5f23ec4cc19b7a18314c1d7e8b
Rob Gansevles <rgansevles@servoy.com>
2013-03-27 09:50:41 +0000

    reverted unwanted commit

--- com.servoy.eclipse.designer/src/com/servoy/eclipse/designer/editor/FormPartpanelGraphicalEditPart.java
+++ com.servoy.eclipse.designer/src/com/servoy/eclipse/designer/editor/FormPartpanelGraphicalEditPart.java
@@ -67,7 +67,7 @@ public class FormPartpanelGraphicalEditPart extends AbstractGraphicalEditPart
 
 	protected IFigure updateFigure(IFigure fig)
 	{
-		Form flattenedForm = application.getFlattenedSolution().getFlattenedForm(getModel().context);// RAGTEST editing?
+		Form flattenedForm = application.getFlattenedSolution().getFlattenedForm(getModel().context);
 		int start = flattenedForm.getPartStartYPos(getModel().part.getID());
 		fig.setBounds(new Rectangle(0, start, flattenedForm.getWidth(), getModel().part.getHeight() - start));
 
