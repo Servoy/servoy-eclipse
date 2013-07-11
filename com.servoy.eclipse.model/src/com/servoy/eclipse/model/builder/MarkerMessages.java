979a07e8413b10ac7d6e6daf844febfa52f6a0d6
Ovidiu Buligan <obuligan@servoy.com>
2013-07-11 12:03:46 +0000

    Predefined arguments not cleared when method arguments are removed +typo Case: SVY-4602

--- com.servoy.eclipse.model/src/com/servoy/eclipse/model/builder/MarkerMessages.java
+++ com.servoy.eclipse.model/src/com/servoy/eclipse/model/builder/MarkerMessages.java
@@ -109,7 +109,7 @@ public class MarkerMessages
 	 * This message appears when the number of arguments of a script method which is used on an element of a form is lower than the the number of arguments stored in the custom properties of that element.
 	 */
 	public static ServoyMarker MethodNumberOfArgumentsMismatch = new ServoyMarker(
-		"Event handler \"{0}\" for {1}{2} called with more arguments than the function deffinition \"{3}\".", ServoyBuilder.METHOD_NUMBER_OF_ARGUMENTS_MISMATCH_TYPE); //$NON-NLS-1$
+		"Event handler \"{0}\" for {1}{2} called with more arguments than the function definition \"{3}\".", ServoyBuilder.METHOD_NUMBER_OF_ARGUMENTS_MISMATCH_TYPE); //$NON-NLS-1$
 
 	/*
 	 * The items below should work fine, they were tested.
