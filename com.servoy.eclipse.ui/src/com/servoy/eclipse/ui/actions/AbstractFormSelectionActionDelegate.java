0dcfe16286865d24fbdbc8c543c9e6d5d6a1e6ac
Laurian Vostinar <lvostinar@servoy.com>
2013-12-02 14:05:28 +0000

    ConcurrentModificationException when opening multiple form designers or script editors
    Case: SVY-5243

--- com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/actions/AbstractFormSelectionActionDelegate.java
+++ com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/actions/AbstractFormSelectionActionDelegate.java
@@ -13,7 +13,7 @@
  You should have received a copy of the GNU Affero General Public License along
  with this program; if not, see http://www.gnu.org/licenses or write to the Free
  Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
-*/
+ */
 package com.servoy.eclipse.ui.actions;
 
 import java.util.ArrayList;
@@ -93,7 +93,8 @@ public abstract class AbstractFormSelectionActionDelegate implements IWorkbenchW
 
 	public final void run(IAction action)
 	{
-		for (Openable openable : openables)
+		List<Openable> openablesCopy = new ArrayList<Openable>(openables);
+		for (Openable openable : openablesCopy)
 		{
 			open(openable);
 		}
