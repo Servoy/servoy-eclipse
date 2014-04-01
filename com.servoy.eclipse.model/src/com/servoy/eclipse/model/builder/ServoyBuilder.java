2883f4b9443874c3494817bf3e43b21bc56b2e61
Edit Mera <emera@servoy.com>
2014-04-01 07:55:03 +0000

    Warnings for tables without pk that have row ident
    Case: SVY-6250

--- com.servoy.eclipse.model/src/com/servoy/eclipse/model/builder/ServoyBuilder.java
+++ com.servoy.eclipse.model/src/com/servoy/eclipse/model/builder/ServoyBuilder.java
@@ -4488,7 +4488,7 @@ public class ServoyBuilder extends IncrementalProjectBuilder
 											"columnName", column.getName());
 									}
 								}
-								if (column.isDBIdentity() && !column.isDatabasePK())
+								if (column.isDBIdentity() && !column.isDatabasePK() && column.getRowIdentType() == 0)
 								{
 									ServoyMarker mk = MarkerMessages.ColumnDatabaseIdentityProblem.fill(tableName, column.getName());
 									addMarker(res, mk.getType(), mk.getText(), -1, COLUMN_DATABASE_IDENTITY_PROBLEM, IMarker.PRIORITY_NORMAL, null, null).setAttribute(
