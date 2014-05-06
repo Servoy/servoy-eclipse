b2dbd0149a66174aaa565f9bd6cd77cd3ab2ee3c
Johan Compagner <jcompagner@servoy.com>
2014-05-06 14:16:46 +0000

    "Error updating table" during "Update Meta Data..." into workspace
    use a job (with the workspace lock) and improved the progress reporting (and also fixes a few issues when it should terminate sooner)
    
    +null pointer fix when preference dialog uses a piece of javascript (formatting)
    Case: SVY-6388

--- com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/views/solutionexplorer/actions/SynchronizeTableDataAction.java
+++ com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/views/solutionexplorer/actions/SynchronizeTableDataAction.java
@@ -16,27 +16,27 @@
  */
 package com.servoy.eclipse.ui.views.solutionexplorer.actions;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.MultiStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.jface.dialogs.IMessageProvider;
-import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.PlatformUI;
 
 import com.servoy.base.util.DataSourceUtilsBase;
 import com.servoy.eclipse.core.ServoyModel;
@@ -65,10 +65,10 @@ import com.servoy.j2db.util.Debug;
 import com.servoy.j2db.util.Pair;
 
 /**
- * 
+ *
  * Action to update data for tables that are marked as meta data tables.
  * The data can be copied from workspace to database table, or from table to workspace.
- *  
+ *
  * @author rgansevles
  *
  */
@@ -159,39 +159,37 @@ public class SynchronizeTableDataAction extends Action implements ISelectionChan
 	@Override
 	public void run()
 	{
+		Job job = new Job("Generating metadata")
+		{
+			@Override
+			protected IStatus run(IProgressMonitor monitor)
+			{
+				runImpl(monitor);
+				return Status.OK_STATUS;
+			}
+		};
+		job.setSystem(false);
+		job.setUser(true);
+		job.setRule(ResourcesPlugin.getWorkspace().getRoot());
+		job.schedule();
+	}
+
+	private void runImpl(IProgressMonitor monitor)
+	{
 		if (selectedServer != null)
 		{
 			dataSources.clear();
-			try
-			{
-				PlatformUI.getWorkbench().getProgressService().busyCursorWhile(new IRunnableWithProgress()
-				{
-					public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException
-					{
-						getServerMetadataTables(selectedServer, monitor);
-					}
-				});
-			}
-			catch (InvocationTargetException e)
-			{
-				ServoyLog.logError(e);
-				dataSources.clear();
-				return;
-			}
-			catch (InterruptedException e)
-			{
-				ServoyLog.logError(e);
-				dataSources.clear();
-				return;
-			}
+			getServerMetadataTables(selectedServer, monitor);
 
 			if (dataSources.size() == 0)
 			{
 				UIUtils.showInformation(shell, "Metadata update", "The server you selected does not have any metadata tables."); //$NON-NLS-1$ //$NON-NLS-2$
+				return;
 			}
 		}
 		MultiStatus warnings = new MultiStatus(Activator.PLUGIN_ID, 0, "For more information please click 'Details'.", null);
 		List<Table> tables = new ArrayList<Table>();
+		monitor.beginTask("Checking for metadata tables", dataSources.size()); //$NON-NLS-1$
 		for (String dataSource : dataSources)
 		{
 			String[] stn = DataSourceUtilsBase.getDBServernameTablename(dataSource);
@@ -228,7 +226,7 @@ public class SynchronizeTableDataAction extends Action implements ISelectionChan
 					MetaDataUtils.METADATA_DELETION_COLUMN + " date column."));
 				continue;
 			}
-			//the folowing "if" is very rare in practice because when you define : 2 columns of type date with the name modification_date and deletion_date and id of type UUID you usually think about metadata tables 
+			//the folowing "if" is very rare in practice because when you define : 2 columns of type date with the name modification_date and deletion_date and id of type UUID you usually think about metadata tables
 			if (!table.isMarkedAsMetaData())
 			{
 				if (!UIUtils.askQuestion(shell, "Table not marked as metadata table", "Table " + table.getName() + " in server " + table.getServerName() +
@@ -245,6 +243,7 @@ public class SynchronizeTableDataAction extends Action implements ISelectionChan
 				}
 			}
 			tables.add(table);
+			monitor.worked(1);
 		}
 		// show warning multi Status
 		if (warnings.getChildren().length > 0)
@@ -256,19 +255,31 @@ public class SynchronizeTableDataAction extends Action implements ISelectionChan
 				{
 					ErrorDialog.openError(shell, null, null, fw);
 				}
-			}, false);
+			}, true);
+			if (tables.size() == 0) return;
 		}
+		final int[] retValue = new int[1];
+
+		Display.getDefault().syncExec(new Runnable()
+		{
+
+			@Override
+			public void run()
+			{
+				retValue[0] = new SynchronizeTableDataDialog(shell).open();
+			}
+		});
 
 		if (tables.size() > 0)
 		{
-			switch (new SynchronizeTableDataDialog(shell).open())
+			switch (retValue[0])
 			{
 				case SynchronizeTableDataDialog.IMPORT_TO_DB :
 					importTableData(tables);
 					break;
 
 				case SynchronizeTableDataDialog.SAVE_TO_WS :
-					generateTableDataFile(tables);
+					generateTableDataFile(tables, monitor);
 					break;
 			}
 		}
@@ -282,14 +293,21 @@ public class SynchronizeTableDataAction extends Action implements ISelectionChan
 			UIUtils.reportWarning("Error updating table(s)", "Cannot find internal data model manager.");
 			return;
 		}
-		Pair<List<Table>, List<Table>> result = getTablesThatContainDataInDb(tables);
+		final Pair<List<Table>, List<Table>> result = getTablesThatContainDataInDb(tables);
 		if (result.getLeft().isEmpty() && result.getRight().isEmpty())
 		{
 			UIUtils.reportWarning("Info", "There is no meta data to be imported in the DB.");
 			return;
 		}
 
-		new WizardDialog(shell, new UpdateMetaDataWziard(result.getLeft(), result.getRight(), shell)).open();
+		Display.getDefault().asyncExec(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				new WizardDialog(shell, new UpdateMetaDataWziard(result.getLeft(), result.getRight(), shell)).open();
+			}
+		});
 	}
 
 	/**
@@ -341,7 +359,7 @@ public class SynchronizeTableDataAction extends Action implements ISelectionChan
 		return new Pair<List<Table>, List<Table>>(tablesWithDataInDB, tablesWithoutDataInDB);
 	}
 
-	private void generateTableDataFile(List<Table> tables)
+	private void generateTableDataFile(List<Table> tables, IProgressMonitor monitor)
 	{
 		DataModelManager dmm = ServoyModelManager.getServoyModelManager().getServoyModel().getDataModelManager();
 		if (dmm == null)
@@ -353,6 +371,7 @@ public class SynchronizeTableDataAction extends Action implements ISelectionChan
 		StringBuilder sb = new StringBuilder();
 		MultiStatus warnings = new MultiStatus(Activator.PLUGIN_ID, 0, "For more information please click 'Details'.", null);
 
+		monitor.beginTask("Creating metadata files for the tables", tables.size()); //$NON-NLS-1$
 		for (Table table : tables)
 		{
 			try
@@ -393,12 +412,13 @@ public class SynchronizeTableDataAction extends Action implements ISelectionChan
 				ServoyLog.logError("Error updating table", e);
 				warnings.add(new Status(IStatus.WARNING, Activator.PLUGIN_ID, "Error updating table", e));
 			}
+			monitor.worked(1);
 		}
 
 		/*
 		 * handle the updating result
 		 */
-		// show warning status messages in eclipse Platform UI way 
+		// show warning status messages in eclipse Platform UI way
 		if (warnings.getChildren().length > 0)
 		{
 			final MultiStatus fw = warnings;
@@ -422,5 +442,4 @@ public class SynchronizeTableDataAction extends Action implements ISelectionChan
 		}
 
 	}
-
 }
