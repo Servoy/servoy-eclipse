2d0e0e3ce1692b4f1e13df104c043019ea05170c
jcompagner <jcompagner@servoy.com>
2014-07-29 15:58:44 +0200

    SVY-6877 (multi) selection
    first cut, single and multi select (with ctrl) works, also deselect from
    a multi select. 
    It directly selects on mouse down when the node didn't have a selection,
    if the node already was in the selection it only selects that one after
    mouse up if the mouse was still in the same position.

--- com.servoy.eclipse.designer.rfb/src/rfb/angular/js/editor.js
+++ com.servoy.eclipse.designer.rfb/src/rfb/angular/js/editor.js
@@ -1,101 +1,114 @@
-angular.module('editor', ['palette','toolbar','mouseselection']).factory("$editor",function($rootScope, EDITOR_EVENTS) {
+angular.module('editor', ['palette','toolbar','mouseselection']).factory("$pluginRegistry",function($rootScope) {
 	var plugins = [];
-	//TODO selection and iframe should move to directive (so all state is in the directive not global in a factor)
-	var selection = [];
-	var iframeDoc = null;
-	function fireSelectionChanged(){
-		$rootScope.$broadcast(EDITOR_EVENTS.SELECTION_CHANGED,selection)
-	}
 
 	return {
-		setIFrameDoc: function(contentDocument) {
-			iframeDoc = contentDocument;
-			
-			// editor is ready init plugins
+		registerEditor: function(editorScope) {
 			for(var i=0;i<plugins.length;i++) {
-				//TODO this call should get the editor instance (the scope of the directive below?)
-				plugins[i]();
+				plugins[i](editorScope);
 			}
-		},
-		
-		getSelection: function() {
-			//Returning a copy so selection can't be changed my modifying the selection array
-			return this.selection.slice(0)
-		},
-
-		extendSelection: function(nodes) {
-			var ar = Array.isArray(nodes) ? nodes : [nodes]
-			var dirty = false
-			
-			for (var i = 0; i < ar.length; i++) {
-				if (this.selection.indexOf(ar[i]) === -1) {
-					this.selection.push(ar[i])
-				}
-			}
-			if (dirty) {
-				fireSelectionChanged()
-			}
-		},
-
-		reduceSelection: function(nodes) {
-			var ar = Array.isArray(nodes) ? nodes : [nodes]
-			var dirty = false
-			for (var i = 0; i < ar.length; i++) {
-				var idx = this.selection.indexOf(ar[i])
-				if (idx !== -1) {
-					this.selection.splice(idx)
-				}
-			}
-			if (dirty) {
-				fireSelectionChanged()
-			}
-		},
-
-		setSelection:function(node) {
-//			Array.prototype.push.apply(delta.removedNodes, this.selection)
-//			this.selection.length = 0
-			
-//			Array.prototype.push.call(delta.addedNodes, node)
-//			Array.prototype.push.call(this.selection, node)
-			
-			fireSelectionChanged()
 		},
 		
 		registerPlugin: function(plugin) {
 			plugins[plugins.length] = plugin;
-			// if there is already a iframeDoc just init directly.
-			if (iframeDoc) plugin();
 		},
-		
-		// can only be called after a plugin gets the callback from the registerPlugin
-		registerDOMEvent:function(eventType, target,callback) {
-			if (target == "FORM") {
-				// $(iframeDoc) == internal iframe document.
-				// TODO can we set a selector/class to filter? 
-				$(iframeDoc).on(eventType, null, callback.bind(this))
-			} else if (target == "EDITOR") {
-				console.log("registering dom event: " + eventType)
-				// $(doc) is the document of the editor (or a div)
-//				$(doc).on(eventType, context, callback.bind(this))
-			}
-		}
 	}
 }).value("EDITOR_EVENTS", {
     SELECTION_CHANGED : "SELECTION_CHANGED"
-}).directive("editor", function( $window, $editor){
+}).directive("editor", function( $window, $pluginRegistry,$rootScope,EDITOR_EVENTS){
 	return {
 	      restrict: 'E',
 	      transclude: true,
 	      scope: {},
 	      link: function($scope, $element, $attrs) {
+			var iframeDoc;  
+			var timeout;
+			var delta = {
+				addedNodes: [],
+				removedNodes: []
+			}
+			var selection = [];
+			function markDirty() {
+				if (timeout) {
+					clearTimeout(timeout)
+				}
+				timeout = setTimeout(fireSelectionChanged, 1)
+			}
+			
+			function fireSelectionChanged(){
+				console.log(selection)
+				//Reference to editor should be gotten from Editor instance somehow
+				//instance.fire(Editor.EVENT_TYPES.SELECTION_CHANGED, delta)
+				$rootScope.$broadcast(EDITOR_EVENTS.SELECTION_CHANGED,selection)
+				delta.addedNodes.length = delta.removedNodes.length = 0
+				timeout = null
+			}
+			
 			function getURLParameter(name) {
 				return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec($window.location.search)||[,""])[1].replace(/\+/g, '%20'))||null
 			}
-			//TODO the "editor" part in this url should be dynamic (like the id='editor' in the editor.template)
-			$scope.contentframe = "editor-content.html?id=%23editor&f=" + getURLParameter("f") +"&s=" + getURLParameter("s");
+			$scope.contentframe = "editor-content.html?id=%23" + $element.attr("id") + "&f=" + getURLParameter("f") +"&s=" + getURLParameter("s");
+			$scope.registerDOMEvent = function(eventType, target,callback) {
+				if (target == "FORM") {
+				$(iframeDoc).on(eventType, null, callback.bind(this))
+			} else if (target == "EDITOR") {
+				console.log("registering dom event: " + eventType)
+				// $(doc) is the document of the editor (or a div)
+				//	$(doc).on(eventType, context, callback.bind(this))
+				}
+			}
+			$scope.getSelection = function() {
+				//Returning a copy so selection can't be changed my modifying the selection array
+				return selection.slice(0)
+			}
+		
+			$scope.extendSelection = function(nodes) {
+				var ar = Array.isArray(nodes) ? nodes : [nodes]
+				var dirty = false
+				
+				for (var i = 0; i < ar.length; i++) {
+					if (selection.indexOf(ar[i]) === -1) {
+						dirty = true
+						delta.addedNodes.push(ar[i])
+						selection.push(ar[i])
+					}
+				}
+				if (dirty) {
+					markDirty()
+				}
+			}
+		
+			$scope.reduceSelection = function(nodes) {
+				var ar = Array.isArray(nodes) ? nodes : [nodes]
+				var dirty = false
+				for (var i = 0; i < ar.length; i++) {
+					var idx = selection.indexOf(ar[i])
+					if (idx !== -1) {
+						dirty = true
+						delta.removedNodes.push(ar[i])
+						selection.splice(idx)
+					}
+				}
+				if (dirty) {
+					markDirty()
+				}
+			}
+			$scope.setSelection = function(node) {
+				var ar = Array.isArray(node) ? node : node ? [node] : []
+				var dirty = ar.length||selection.length
+				Array.prototype.push.apply(delta.removedNodes, selection)
+				selection.length = 0
+				
+				Array.prototype.push.apply(delta.addedNodes, ar)
+				Array.prototype.push.apply(selection, ar)
+				
+				if (dirty) {
+					markDirty()
+				}
+			}
 			
 			$element.on('documentReady.content', function(event, contentDocument) {
-				$editor.setIFrameDoc(contentDocument);
+				iframeDoc = contentDocument;
+				$pluginRegistry.registerEditor($scope);
 			});
 	      },
 	      templateUrl: 'templates/editor.html',
--- com.servoy.eclipse.designer.rfb/src/rfb/angular/js/modules/mouseselection.js
+++ com.servoy.eclipse.designer.rfb/src/rfb/angular/js/modules/mouseselection.js
@@ -1,12 +1,64 @@
-angular.module('mouseselection',['editor']).run(function($rootScope, $editor){
+angular.module('mouseselection',['editor']).run(function($rootScope, $pluginRegistry){
 	
-	$editor.registerPlugin(function() {
+	$pluginRegistry.registerPlugin(function(editorScope) {
+		var lastMouseDownEvent;
+		function getNode(event) {
+			var node = null;
+			var element = event.target;
+			do
+			{
+				if (element.hasAttribute("svy-model") && element.hasAttribute("name")) {
+					node = element;
+					// TODO search for the most top level?
+					// but if we have layout components in designer then we do need to select the nested.
+				}
+				element = element.parentElement;
+			} while(element)
+			return node;
+		}
+		function select(event, node) {
+			if (event.ctrlKey) {
+				if (editorScope.getSelection().indexOf(node) !== -1) {
+					editorScope.reduceSelection(node)
+				} else {
+					editorScope.extendSelection(node)
+				}
+			} else if (event.shiftKey) {
+				//TODO: implement
+				/*
+				 * Shift-Click does range select: all elements within the box defined by the uttermost top/left point of selected elements
+				 * to the uttermost bottom-right of the clicked element
+				 */
+			} else {
+				editorScope.setSelection(node)
+			}
+		}
 		function onmousedown(event) {
-			console.log(event);
-			$rootScope.$apply(function() {$editor.setSelection(event.target);} )
+			var node = getNode(event);
+			if (node) {
+				if (editorScope.getSelection().indexOf(node) !== -1) {
+					// its in the current selection, remember this for mouse up.
+					lastMouseDownEvent = event;
+				}
+				else select(event,node);
+			}
+			else {
+				editorScope.setSelection([])
+			}
+			event.preventDefault();
+		}
+		function onmouseup(event) {
+			if (lastMouseDownEvent) {
+				if (event.pageX == lastMouseDownEvent.pageX && event.pageY == lastMouseDownEvent.pageY) {
+					var node = getNode(event);
+					select(event,node);
+				}
+			}
+			lastMouseDownEvent = null;
 			event.preventDefault();
 		}
 		// register event on editor form iframe (see register event in the editor.js)
-		$editor.registerDOMEvent("mousedown","FORM", onmousedown); // real selection in editor content iframe
+		editorScope.registerDOMEvent("mousedown","FORM", onmousedown); // real selection in editor content iframe
+		editorScope.registerDOMEvent("mouseup","FORM", onmouseup); // real selection in editor content iframe
 	})
 });
