1f4d4b363975c79c5f0379f985ced59b0c166bad
Gabi Boros <gboros@servoy.com>
2014-08-20 17:22:37 +0300

    SVY-6895 : added support for split-dropdown toolbar button; made the toolbar buttons use bootstrap;

--- com.servoy.eclipse.designer.rfb/src/rfb/angular/js/toolbar.js
+++ com.servoy.eclipse.designer.rfb/src/rfb/angular/js/toolbar.js
@@ -1,4 +1,33 @@
-angular.module("toolbar",['alignment','designsize']).directive("toolbar", function(TOOLBAR_CATEGORIES){
+angular.module("toolbar",['alignment','designsize'])
+.run(["$templateCache","$http",function($templateCache,$http){
+	$http.get("templates/toolbaritem.html").then(function(result){
+		$templateCache.put("templates/toolbaritem.html", result.data);
+    });
+	$http.get("templates/toolbaritemdropdown.html").then(function(result){
+		$templateCache.put("templates/toolbaritemdropdown.html", result.data);
+    });	
+}])
+.directive("toolbarItem", ['$templateCache','$compile',function($templateCache,$compile){
+	return {
+	      restrict: 'E',
+	      transclude: true,
+	      scope: {
+	    	  model: "=model",
+	      },
+	      link: function($scope, $element, $attrs) {
+	    	  $element.html($templateCache.get($scope.model.list ? "templates/toolbaritemdropdown.html" : "templates/toolbaritem.html"));
+	          $compile($element.contents())($scope);
+	          if($scope.model.list) {
+	        	  $scope.onselection = function(selection) {
+	        		  var text = $scope.model.onselection(selection);
+	        		  if(text) $scope.model.text = text;
+	        	  }
+	          }
+	      },
+	      replace: true
+	    };
+}])
+.directive("toolbar", function(TOOLBAR_CATEGORIES){
 	return {
 	      restrict: 'E',
 	      transclude: true,
@@ -12,7 +41,8 @@ angular.module("toolbar",['alignment','designsize']).directive("toolbar", functi
 	      replace: true
 	    };
 	
-}).factory("$toolbar", function(TOOLBAR_CATEGORIES){
+})
+.factory("$toolbar", function(TOOLBAR_CATEGORIES){
 	var buttons= [];
 	return {
 		add: function(buttonState, category) {
@@ -26,7 +56,8 @@ angular.module("toolbar",['alignment','designsize']).directive("toolbar", functi
 			return buttons[category];
 		}
 	}
-}).value("TOOLBAR_CATEGORIES", {
+})
+.value("TOOLBAR_CATEGORIES", {
 	EDITOR: "editor",
 	ELEMENTS: "elements",
 	FORM: "forms",
