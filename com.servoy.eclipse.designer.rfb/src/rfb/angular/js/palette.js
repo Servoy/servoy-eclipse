473210f303cafa7fb18d423da4ccb69f8184dc12
georgeganea <gganea@servoy.com>
2014-08-29 15:38:55 +0300

    SVY-6911 pallete is now received as a correct json, all that is left is to display it

--- com.servoy.eclipse.designer.rfb/src/rfb/angular/js/palette.js
+++ com.servoy.eclipse.designer.rfb/src/rfb/angular/js/palette.js
@@ -3,32 +3,11 @@ angular.module("palette",['ui.bootstrap']).directive("palette", function($editor
 	      restrict: 'E',
 	      transclude: true,
 	      controller: function($scope, $element, $attrs, $http) {
-	    	 $scope.categories = [];
-	    	  
-	    	 function addCategory(name, isOpen) {
-	    		 for(var c in $scope.categories) {
-	    			 if($scope.categories[c].name == name) {
-	    				 return $scope.categories[c];
-	    			 }
-	    		 }
-	    		 var category = {name : name, components: [], isOpen: isOpen};
-	    		 $scope.categories.push(category);
-	    		 
-	    		 return category;
-	    	 }
-	    	 
-	    	 function addComponent(categoryName,displayName, name, icon) {
-	    		 var category = addCategory(categoryName, true);
-	    		 category.components.push({displayName : displayName, name: name, icon : icon});
-	    	 }
-	    	 
-	    	 addCategory("Elements", true);
-	    	 addCategory("Shapes", true);
-	    	 addCategory("Containers", true);
-	    	 addCategory("Beans", true);
+	    	 $scope.packages = [];
 	    	 $http({method: 'GET', url: '/designer/palette'}).success(function(data) {
+	    		 $scope.packages = data;
 		    		for(var i = 0; i < data.length; i++) {
-		    			addComponent(data[i].categoryName ? data[i].categoryName : "Beans", data[i].displayName, data[i].name, data[i].icon ? "/" + data[i].icon : "");
+		    			data[i].isOpen = "true";
 		    		}
 	    	 });
 	    	 
