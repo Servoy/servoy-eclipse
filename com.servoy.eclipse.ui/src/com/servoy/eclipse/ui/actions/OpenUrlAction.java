ca01a69dbabdb695d494157413f2496548662a93
Jan Blok <jblok@servoy.com>
2014-10-09 19:34:17 +0200

    use more correct url

--- com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/actions/OpenUrlAction.java
+++ com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/actions/OpenUrlAction.java
@@ -13,7 +13,7 @@
  You should have received a copy of the GNU Affero General Public License along
  with this program; if not, see http://www.gnu.org/licenses or write to the Free
  Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
-*/
+ */
 package com.servoy.eclipse.ui.actions;
 
 import java.net.URL;
@@ -33,7 +33,6 @@ public class OpenUrlAction implements IWorkbenchWindowActionDelegate
 
 	public void dispose()
 	{
-
 	}
 
 	public void init(IWorkbenchWindow window)
@@ -50,7 +49,7 @@ public class OpenUrlAction implements IWorkbenchWindowActionDelegate
 			}
 			else
 			{
-				PlatformUI.getWorkbench().getBrowserSupport().getExternalBrowser().openURL(new URL("http://www.servoy.com/forum/forum_index.php"));
+				PlatformUI.getWorkbench().getBrowserSupport().getExternalBrowser().openURL(new URL("http://forum.servoy.com"));
 			}
 		}
 		catch (Exception e)
@@ -61,7 +60,5 @@ public class OpenUrlAction implements IWorkbenchWindowActionDelegate
 
 	public void selectionChanged(IAction action, ISelection selection)
 	{
-
 	}
-
 }
