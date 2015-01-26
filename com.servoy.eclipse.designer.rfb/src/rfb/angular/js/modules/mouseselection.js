e4dbb929140e96393142599f3ed6995f5f6a451c
georgeganea <gganea@servoy.com>
2015-01-26 11:40:45 +0200

    SVY-7690 lasso rectangle now selects only elements that are completely inside the rectangle

--- com.servoy.eclipse.designer.rfb/src/rfb/angular/js/modules/mouseselection.js
+++ com.servoy.eclipse.designer.rfb/src/rfb/angular/js/modules/mouseselection.js
@@ -105,7 +105,7 @@ angular.module('mouseselection',['editor']).run(function($rootScope, $pluginRegi
 				var p1 = mouseDownPosition;
 				var p2 = lassoMouseSelectPosition;
 				if(Math.abs(p1.left - p2.left) > 1 && Math.abs(p1.top - p2.top) > 1) {
-					var selectedElements = utils.getElementsByRectangle(p1,p2,1,true,true);
+					var selectedElements = utils.getElementsByRectangle(p1,p2,100,true,true);
 					editorScope.setSelection(selectedElements);	
 				}
 				lassoStarted = false;
