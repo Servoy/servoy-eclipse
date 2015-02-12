170d56e3b2f889c47690d29982f2da5fe97d144f
lvostinar@servoy.com <lvostinar@servoy.com>
2015-02-12 15:13:10 +0200

    NPE fix

--- com.servoy.eclipse.designer/src/com/servoy/eclipse/designer/Activator.java
+++ com.servoy.eclipse.designer/src/com/servoy/eclipse/designer/Activator.java
@@ -333,7 +333,7 @@ public class Activator extends AbstractUIPlugin
 
 		/*
 		 * (non-Javadoc)
-		 *
+		 * 
 		 * @see com.servoy.j2db.server.ngclient.NGClient#shutDown(boolean)
 		 */
 		@Override
@@ -385,7 +385,7 @@ public class Activator extends AbstractUIPlugin
 
 		/*
 		 * (non-Javadoc)
-		 *
+		 * 
 		 * @see com.servoy.j2db.persistence.IPersistChangeListener#persistChanges(java.util.Collection)
 		 */
 		@Override
@@ -465,7 +465,7 @@ public class Activator extends AbstractUIPlugin
 
 	/*
 	 * (non-Javadoc)
-	 *
+	 * 
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
 	 */
 	@Override
@@ -505,7 +505,7 @@ public class Activator extends AbstractUIPlugin
 			{
 				if (client != null)
 				{
-					if (toProject.getProject().getName().startsWith("import_placeholder"))
+					if (toProject != null && toProject.getProject().getName().startsWith("import_placeholder"))
 					{
 						if (client.isSolutionLoaded())
 						{
@@ -554,7 +554,7 @@ public class Activator extends AbstractUIPlugin
 
 	/*
 	 * (non-Javadoc)
-	 *
+	 * 
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
 	@Override
