4e09a8a5387caaee53c57000906b8682855d098b
emera <emera@servoy.com>
2015-04-16 17:34:14 +0300

    SVY-8057 mouse hover on resize knobs can show highlight on a different component in HTML editor

--- com.servoy.eclipse.designer.rfb/src/rfb/angular/js/modules/mouseselection.js
+++ com.servoy.eclipse.designer.rfb/src/rfb/angular/js/modules/mouseselection.js
@@ -277,7 +277,7 @@ angular.module('mouseselection',['editor']).run(function($rootScope, $pluginRegi
 							xMouseDown = event.clientX;
 							yMouseDown = event.clientY;			
 						}
-					if (lassoStarted || hasClass(event.target,"contentframe-overlay") || hasClass(event.target,"ghost") || event.target.id == "highlight") {
+					if (lassoStarted || hasClass(event.target,"contentframe-overlay") || hasClass(event.target,"ghost") || hasClass(event.target, "knob") || event.target.id == "highlight") {
 						xMouseDown -= editorScope.glasspane.parentElement.offsetLeft;
 						yMouseDown -= editorScope.glasspane.parentElement.offsetTop;
 						xMouseDown += editorScope.glasspane.parentElement.scrollLeft;
