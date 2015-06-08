f3af569a9176145ac4f16ec350c79a131d7b39a7
Gabi Boros <gboros@servoy.com>
2015-06-08 16:09:16 +0300

    SVY-6276 added wro4j with js/css minify

--- com.servoy.eclipse.model/src/com/servoy/eclipse/model/war/exporter/WarExporter.java
+++ com.servoy.eclipse.model/src/com/servoy/eclipse/model/war/exporter/WarExporter.java
@@ -111,7 +111,7 @@ public class WarExporter
 	private final IWarExportModel exportModel;
 	private static final String[] EXCLUDE_FROM_NG_JAR = new String[] { "com/servoy/j2db/server/ngclient/startup", "war/", "META-INF/MANIFEST.", "META-INF/SERVOYCL." };
 	private static final String[] NG_LIBS = new String[] { "slf4j.api_*.jar", "log4j_*.jar", "org.freemarker*.jar", "org.jsoup*.jar", "servoy_ngclient_" +
-		ClientVersion.getBundleVersion() + ".jar", "sablo_" + ClientVersion.getBundleVersion() + ".jar" };
+		ClientVersion.getBundleVersion() + ".jar", "sablo_" + ClientVersion.getBundleVersion() + ".jar", "commons-lang3_*.jar", "wro4j-core_*.jar" };
 
 	/**
 	 * @param exportModel
