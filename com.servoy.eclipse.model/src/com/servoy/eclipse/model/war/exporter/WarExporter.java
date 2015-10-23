89990d7e9ba150d77ebd3f0f3bd3e475edf9e59a
George Ganea <georgeganea@gmail.com>
2015-10-23 17:21:46 +0300

    SVY-9006 changed name of hardcoded jar file

--- com.servoy.eclipse.model/src/com/servoy/eclipse/model/war/exporter/WarExporter.java
+++ com.servoy.eclipse.model/src/com/servoy/eclipse/model/war/exporter/WarExporter.java
@@ -114,7 +114,7 @@ public class WarExporter
 	private static final String SERVICES_DIR_NAME = "services";
 	private final IWarExportModel exportModel;
 	private static final String[] EXCLUDE_FROM_NG_JAR = new String[] { "com/servoy/j2db/server/ngclient/startup", "war/", "META-INF/MANIFEST.", "META-INF/SERVOYCL." };
-	private static final String[] NG_LIBS = new String[] { "slf4j.api_*.jar", "log4j_*.jar", "org.freemarker*.jar", "org.jsoup*.jar", "servoy_ngclient_" +
+	private static final String[] NG_LIBS = new String[] { "org.slf4j.api_*.jar", "log4j_*.jar", "org.freemarker*.jar", "org.jsoup*.jar", "servoy_ngclient_" +
 		ClientVersion.getBundleVersion() + ".jar", "sablo_" + ClientVersion.getBundleVersion() + ".jar", "commons-lang3_*.jar", "wro4j-core_*.jar" };
 
 	/**
