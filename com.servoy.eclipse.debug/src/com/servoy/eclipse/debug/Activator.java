17f9b08e7a55aecbf877f0e04c83e2f087a4997a
lvostinar <lvostinar@laurian>
2015-11-26 12:42:03 +0200

    SVY-9079 components are not loaded (move code to another activator)

--- com.servoy.eclipse.debug/src/com/servoy/eclipse/debug/Activator.java
+++ com.servoy.eclipse.debug/src/com/servoy/eclipse/debug/Activator.java
@@ -17,57 +17,23 @@
 package com.servoy.eclipse.debug;
 
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.jar.Manifest;
 
 import javax.swing.Action;
 
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IFolder;
-import org.eclipse.core.resources.IMarker;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IResourceChangeEvent;
-import org.eclipse.core.resources.IResourceChangeListener;
-import org.eclipse.core.resources.IResourceDelta;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtension;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Platform;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.jface.util.IPropertyChangeListener;
-import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.ui.IStartup;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
-import org.sablo.specification.WebComponentPackage;
-import org.sablo.specification.WebComponentPackage.DirPackageReader;
-import org.sablo.specification.WebComponentPackage.DuplicatePackageException;
-import org.sablo.specification.WebComponentPackage.IPackageReader;
 
-import com.servoy.eclipse.core.IActiveProjectListener;
-import com.servoy.eclipse.core.ServoyModel;
-import com.servoy.eclipse.model.ServoyModelFinder;
-import com.servoy.eclipse.model.nature.ServoyProject;
-import com.servoy.eclipse.model.nature.ServoyResourcesProject;
-import com.servoy.eclipse.model.repository.SolutionSerializer;
 import com.servoy.eclipse.model.util.ModelUtils;
 import com.servoy.eclipse.model.util.ServoyLog;
-import com.servoy.j2db.server.ngclient.startup.resourceprovider.ResourceProvider;
 import com.servoy.j2db.util.Utils;
 
 /**
@@ -82,18 +48,6 @@ public class Activator extends AbstractUIPlugin implements IStartup
 	private static Activator plugin;
 
 	private final List<Image> imageList = Collections.synchronizedList(new ArrayList<Image>());
-
-	private final Map<String, IPackageReader> componentReaders = new HashMap<String, IPackageReader>();
-	private final Map<String, IPackageReader> serviceReaders = new HashMap<String, IPackageReader>();
-
-	private IResourceChangeListener resourceChangeListener;
-
-	private IActiveProjectListener activeProjectListenerForRegisteringResources;
-	private Job registerResourcesJob;
-
-	private static final String DUPLICATE_COMPONENT_MARKER = "com.servoy.eclipse.debug.DUPLICATE_COMPONENT_MARKER";
-	private static final String SPEC_READ_MARKER = "com.servoy.eclipse.debug.SPEC_READ_MARKER";
-
 
 	/**
 	 * The constructor
@@ -116,250 +70,12 @@ public class Activator extends AbstractUIPlugin implements IStartup
 			// GTK LaF causes crashes or hangs on linux in developer
 			System.setProperty("swing.defaultlaf", "javax.swing.plaf.metal.MetalLookAndFeel");
 		}
-		Set<String> defaultPackageNames = ResourceProvider.getDefaultPackageNames();
-		for (String packageName : defaultPackageNames)
-		{
-			getPreferenceStore().setDefault("com.servoy.eclipse.designer.rfb.packages.enable." + packageName, true);
-		}
-		getPreferenceStore().addPropertyChangeListener(new IPropertyChangeListener()
-		{
-
-			@Override
-			public void propertyChange(PropertyChangeEvent event)
-			{
-				registerResources(null);
-
-			}
-		});
-		registerResources(null);
-
-		resourceChangeListener = new IResourceChangeListener()
-		{
-			@Override
-			public void resourceChanged(IResourceChangeEvent event)
-			{
-				ServoyResourcesProject activeResourcesProject = ServoyModelFinder.getServoyModel().getActiveResourcesProject();
-				if (activeResourcesProject != null)
-				{
-					IProject resourceProject = activeResourcesProject.getProject();
-					IResourceDelta delta = event.getDelta();
-					IResourceDelta[] affectedChildren = delta.getAffectedChildren();
-					boolean refreshServices = shouldRefresh(resourceProject, affectedChildren, SolutionSerializer.SERVICES_DIR_NAME);
-					boolean refreshComponents = shouldRefresh(resourceProject, affectedChildren, SolutionSerializer.COMPONENTS_DIR_NAME);
-					if (refreshServices || refreshComponents)
-					{
-						registerResources(refreshServices && refreshComponents ? null : (refreshComponents ? Boolean.TRUE : Boolean.FALSE));
-					}
-				}
-			}
-
-			/**
-			 * @param resourceProject
-			 * @param affectedChildren
-			 */
-			private boolean shouldRefresh(IProject resourceProject, IResourceDelta[] affectedChildren, String parentDir)
-			{
-				for (IResourceDelta rd : affectedChildren)
-				{
-					if (rd.getFlags() == IResourceDelta.MARKERS) continue;
-					IResource resource = rd.getResource();
-					if (resourceProject.equals(resource.getProject()))
-					{
-						IPath path = resource.getProjectRelativePath();
-						if (path.segmentCount() > 1)
-						{
-							if (path.segment(0).equals(parentDir))
-							{
-								if (path.segmentCount() == 2 && resource instanceof IFile)
-								{
-									// a zip is changed refresh
-									return true;
-								}
-								else if (path.lastSegment().equalsIgnoreCase("MANIFEST.MF") || path.lastSegment().toLowerCase().endsWith(".spec"))
-								{
-									return true;
-								}
-							}
-						}
-						if (path.segmentCount() == 0 || (path.segmentCount() > 0 && path.segment(0).equals(parentDir)))
-						{
-							if (shouldRefresh(resourceProject, rd.getAffectedChildren(), parentDir))
-							{
-								return true;
-							}
-						}
-					}
-				}
-				return false;
-			}
-		};
-
-		ResourcesPlugin.getWorkspace().addResourceChangeListener(resourceChangeListener, IResourceChangeEvent.POST_CHANGE);
 	}
 
-	/**
-	 * @param activeResourcesProject
-	 */
-	private void registerResources(final Boolean component)
-	{
-		if (registerResourcesJob == null)
-		{
-			registerResourcesJob = new Job("registering resources")
-			{
-				@Override
-				public IStatus run(IProgressMonitor monitor)
-				{
-					try
-					{
-						if (activeProjectListenerForRegisteringResources == null)
-						{
-							activeProjectListenerForRegisteringResources = new IActiveProjectListener()
-							{
-								public boolean activeProjectWillChange(ServoyProject activeProject, ServoyProject toProject)
-								{
-									return true;
-								}
-
-								public void activeProjectUpdated(ServoyProject activeProject, int updateInfo)
-								{
-									// todo maybe fush on certain things?
-								}
-
-								public void activeProjectChanged(ServoyProject activeProject)
-								{
-									registerResources(null);
-								}
-							};
-							((ServoyModel)ServoyModelFinder.getServoyModel()).addActiveProjectListener(activeProjectListenerForRegisteringResources);
-						}
-						ServoyResourcesProject activeResourcesProject = ServoyModelFinder.getServoyModel().getActiveResourcesProject();
-						if (activeResourcesProject != null)
-						{
-							try
-							{
-								IFolder components = activeResourcesProject.getProject().getFolder(SolutionSerializer.COMPONENTS_DIR_NAME);
-								if (components != null && components.exists())
-								{
-									components.refreshLocal(IResource.DEPTH_INFINITE, monitor);
-									components.deleteMarkers(DUPLICATE_COMPONENT_MARKER, false, IResource.DEPTH_INFINITE);
-									components.refreshLocal(IResource.DEPTH_INFINITE, monitor);
-								}
-								IFolder services = activeResourcesProject.getProject().getFolder(SolutionSerializer.SERVICES_DIR_NAME);
-								if (services != null && services.exists())
-								{
-									services.refreshLocal(IResource.DEPTH_INFINITE, monitor);
-									services.deleteMarkers(DUPLICATE_COMPONENT_MARKER, false, IResource.DEPTH_INFINITE);
-									services.refreshLocal(IResource.DEPTH_INFINITE, monitor);
-								}
-							}
-							catch (CoreException e)
-							{
-								ServoyLog.logError(e);
-							}
-
-							Set<String> defaultPackageNames = ResourceProvider.getDefaultPackageNames();
-							List<String> toRemove = new ArrayList<String>();
-							for (String packageName : defaultPackageNames)
-							{
-								if (!isEnabledInPreferences(packageName)) toRemove.add(packageName);
-							}
-							ResourceProvider.setRemovedPackages(toRemove);
-
-							if (!Boolean.FALSE.equals(component))
-							{
-								if (componentReaders.size() > 0)
-								{
-									ResourceProvider.refreshComponentResources(componentReaders.values());
-									componentReaders.clear();
-								}
-								componentReaders.putAll(readDir(monitor, activeResourcesProject, SolutionSerializer.COMPONENTS_DIR_NAME));
-								ResourceProvider.addComponentResources(componentReaders.values());
-							}
-
-							if (component == null || Boolean.FALSE.equals(component))
-							{
-								if (serviceReaders.size() > 0)
-								{
-									ResourceProvider.refreshServiceResources(serviceReaders.values());
-									serviceReaders.clear();
-								}
-								serviceReaders.putAll(readDir(monitor, activeResourcesProject, SolutionSerializer.SERVICES_DIR_NAME));
-
-								ResourceProvider.addServiceResources(serviceReaders.values());
-							}
-
-							com.servoy.eclipse.core.Activator.getDefault().webResourcesChanged(component);
-						}
-					}
-					finally
-					{
-						registerResourcesJob = null;
-					}
-					return Status.OK_STATUS;
-				}
-
-				private boolean isEnabledInPreferences(String packageName)
-				{
-					return Activator.getDefault().getPreferenceStore().getBoolean("com.servoy.eclipse.designer.rfb.packages.enable." + packageName);
-				}
-
-
-				/**
-				 * @param monitor
-				 * @param activeResourcesProject
-				 */
-				private Map<String, IPackageReader> readDir(IProgressMonitor monitor, ServoyResourcesProject activeResourcesProject, String folderName)
-				{
-					Map<String, IPackageReader> readers = new HashMap<String, IPackageReader>();
-					IFolder folder = activeResourcesProject.getProject().getFolder(folderName);
-					if (folder.exists())
-					{
-						try
-						{
-							folder.refreshLocal(IResource.DEPTH_INFINITE, monitor);
-							IResource[] members = folder.members();
-							for (IResource resource : members)
-							{
-								String name = resource.getName();
-								int index = name.lastIndexOf('.');
-								if (index != -1)
-								{
-									name = name.substring(0, index);
-								}
-								if (resource instanceof IFolder)
-								{
-									if (((IFolder)resource).getFile("META-INF/MANIFEST.MF").exists())
-									{
-										readers.put(name, new FolderPackageReader(new File(resource.getRawLocationURI()), (IFolder)resource));
-									}
-								}
-								else if (resource instanceof IFile)
-								{
-									readers.put(name, new FilePackageReader(resource));
-								}
-							}
-						}
-						catch (CoreException e)
-						{
-							ServoyLog.logError(e);
-						}
-					}
-					return readers;
-				}
-			};
-			registerResourcesJob.setRule(ResourcesPlugin.getWorkspace().getRoot());
-			registerResourcesJob.schedule();
-		}
-	}
 
 	@Override
 	public void stop(BundleContext context) throws Exception
 	{
-		ResourcesPlugin.getWorkspace().removeResourceChangeListener(resourceChangeListener);
-		ResourceProvider.removeComponentResources(componentReaders.values());
-		ResourceProvider.removeServiceResources(serviceReaders.values());
-		if (activeProjectListenerForRegisteringResources != null)
-			((ServoyModel)ServoyModelFinder.getServoyModel()).removeActiveProjectListener(activeProjectListenerForRegisteringResources);
 		plugin = null;
 		super.stop(context);
 		for (Image image : imageList)
@@ -426,49 +142,6 @@ public class Activator extends AbstractUIPlugin implements IStartup
 		return shortcuts;
 	}
 
-	private static void addErrorMarker(IResource resource, Exception e)
-	{
-		try
-		{
-			IMarker marker = null;
-			if (e instanceof DuplicatePackageException)
-			{
-				resource.deleteMarkers(DUPLICATE_COMPONENT_MARKER, false, IResource.DEPTH_ONE);
-				marker = resource.createMarker(DUPLICATE_COMPONENT_MARKER);
-			}
-			else
-			{
-				marker = resource.createMarker(SPEC_READ_MARKER);
-			}
-			marker.setAttribute(IMarker.MESSAGE, e.getMessage());
-			marker.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_ERROR);
-			marker.setAttribute(IMarker.PRIORITY, IMarker.PRIORITY_NORMAL);
-			marker.setAttribute(IMarker.LOCATION, resource.getLocation().toString());
-		}
-		catch (CoreException ex)
-		{
-			ServoyLog.logError(ex);
-		}
-	}
-
-	private class FilePackageReader extends WebComponentPackage.JarPackageReader
-	{
-		private final IResource resource;
-
-		public FilePackageReader(IResource resource)
-		{
-			super(new File(resource.getRawLocationURI()));
-			this.resource = resource;
-		}
-
-		@Override
-		public void reportError(String specpath, Exception e)
-		{
-			super.reportError(specpath, e);
-			addErrorMarker(resource, e);
-		}
-	}
-
 	public static class ShortcutDefinition
 	{
 
@@ -483,64 +156,5 @@ public class Activator extends AbstractUIPlugin implements IStartup
 			this.action = action;
 		}
 
-	}
-
-	public static class FolderPackageReader extends DirPackageReader
-	{
-		private final IFolder folder;
-
-		public FolderPackageReader(File dir, IFolder folder)
-		{
-			super(dir);
-			this.folder = folder;
-		}
-
-		@Override
-		public Manifest getManifest() throws IOException
-		{
-			IFile file = folder.getFile("META-INF/MANIFEST.MF");
-			try
-			{
-				file.deleteMarkers(SPEC_READ_MARKER, false, IResource.DEPTH_ONE);
-			}
-			catch (CoreException e)
-			{
-				ServoyLog.logError(e);
-			}
-			try
-			{
-				return super.getManifest();
-			}
-			catch (IOException ex)
-			{
-				addErrorMarker(file, ex);
-				throw ex;
-			}
-		}
-
-		@Override
-		public String readTextFile(String path, Charset charset) throws IOException
-		{
-			IFile file = folder.getFile(path);
-			if (file != null && file.exists())
-			{
-				try
-				{
-					file.deleteMarkers(SPEC_READ_MARKER, false, IResource.DEPTH_ONE);
-				}
-				catch (CoreException e)
-				{
-					ServoyLog.logError(e);
-				}
-			}
-			return super.readTextFile(path, charset);
-		}
-
-		@Override
-		public void reportError(String specpath, Exception e)
-		{
-			super.reportError(specpath, e);
-			addErrorMarker(e instanceof DuplicatePackageException ? folder : folder.getFile(specpath), e);
-		}
 	}
 }
