42ab132697a77fe9c5c9dfd8ea8a349101a4a657
Andrei Costescu <costescuandrei@gmail.com>
2016-05-27 13:17:38 +0300

    Fixed SVY-9895 : Inherited event arguments of an element disapear when overriding some property
    Now developer shows inherited arguments but overriding still happens at
    entire arguments level not on each argument.
    Also fixed restore to default on one specific argument.
    
    Still to fix in following commits: restore to default of an inherited
    handler with overridden elements + changing the inherited function with
    inherited args into a new function doesn't override&clear inherited
    arguments.
    
    Conflicts:
    	com.servoy.eclipse.model.exporter.mobile/src/com/servoy/eclipse/model/mobile/exporter/MobileExporter.java

--- com.servoy.eclipse.model.exporter.mobile/src/com/servoy/eclipse/model/mobile/exporter/MobileExporter.java
+++ com.servoy.eclipse.model.exporter.mobile/src/com/servoy/eclipse/model/mobile/exporter/MobileExporter.java
@@ -1117,7 +1117,7 @@ public class MobileExporter
 		if (mediaOrder == null)
 		{
 			FlattenedSolution fs = getFlattenedSolution();
-			java.util.List<String> lst = (java.util.List<String>)fs.getSolution().getFlattenedCustomProperty(new String[] { "mobileexport", "mediaorder" });
+			java.util.List<String> lst = (java.util.List<String>)fs.getSolution().getCustomProperty(new String[] { "mobileexport", "mediaorder" });
 			if (lst == null)
 			{
 				lst = new ArrayList<String>();
