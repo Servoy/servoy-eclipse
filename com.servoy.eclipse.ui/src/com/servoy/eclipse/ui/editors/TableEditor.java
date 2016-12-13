02abde3757a38072177e71696fe00058da04131b
lvostinar <lvostinar@laurian>
2016-12-13 10:35:36 +0200

    SVY-10672 inmem table - no pk warnings

--- com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/editors/TableEditor.java
+++ com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/editors/TableEditor.java
@@ -725,16 +725,19 @@ public class TableEditor extends MultiPageEditorPart implements IActiveProjectLi
 				{
 					propertiesComposite.saveValues();
 				}
-				if (table.getRowIdentColumnsCount() == 0)
+				if (!(table instanceof MemTable))
 				{
-					MessageDialog.openWarning(getSite().getShell(), "Warning", "No primary key specified on table.");
-					table.setHiddenInDeveloperBecauseNoPk(true);
-					server.setTableMarkedAsHiddenInDeveloper(table.getName(), true);
-				}
-				else if (table.isHiddenInDeveloperBecauseNoPk())
-				{
-					table.setHiddenInDeveloperBecauseNoPk(false);
-					server.setTableMarkedAsHiddenInDeveloper(table.getName(), false);
+					if (table.getRowIdentColumnsCount() == 0)
+					{
+						MessageDialog.openWarning(getSite().getShell(), "Warning", "No primary key specified on table.");
+						table.setHiddenInDeveloperBecauseNoPk(true);
+						server.setTableMarkedAsHiddenInDeveloper(table.getName(), true);
+					}
+					else if (table.isHiddenInDeveloperBecauseNoPk())
+					{
+						table.setHiddenInDeveloperBecauseNoPk(false);
+						server.setTableMarkedAsHiddenInDeveloper(table.getName(), false);
+					}
 				}
 				for (Column column : table.getColumns())
 				{
