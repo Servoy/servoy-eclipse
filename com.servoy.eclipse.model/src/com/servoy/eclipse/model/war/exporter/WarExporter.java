823d019fd285a3e86555d94f56330975ff170f21
Johan Compagner <jcompagner@servoy.com>
2017-07-25 10:27:17 +0200

    use the new getBundleVersionWithPostFix() because plugins now have that name including the _rc1

--- com.servoy.eclipse.model/src/com/servoy/eclipse/model/war/exporter/WarExporter.java
+++ com.servoy.eclipse.model/src/com/servoy/eclipse/model/war/exporter/WarExporter.java
@@ -139,7 +139,8 @@ public class WarExporter
 {
 	private static final String[] EXCLUDE_FROM_NG_JAR = new String[] { "com/servoy/j2db/server/ngclient/startup", "war/", "META-INF/MANIFEST.", "META-INF/SERVOYCL." };
 	private static final String[] NG_LIBS = new String[] { "org.slf4j.api_*.jar", "log4j_*.jar", "org.freemarker*.jar", "servoy_ngclient_" +
-		ClientVersion.getBundleVersion() + ".jar", "sablo_" + ClientVersion.getBundleVersion() + ".jar", "commons-lang3_*.jar", "wro4j-core_*.jar" };
+		ClientVersion.getBundleVersionWithPostFix() +
+		".jar", "sablo_" + ClientVersion.getBundleVersionWithPostFix() + ".jar", "commons-lang3_*.jar", "wro4j-core_*.jar" };
 
 	private static final String WRO4J_RUNNER = "wro4j-runner-1.7.7";
 
