46096e42f76ac79dcb101512b208ea5f63218236
Andrei Costescu <costescuandrei@g.....com>
2018-02-28 10:30:39 +0200

    Just some auto-format and a method rename.

--- com.servoy.eclipse.ngclient.ui/node/src/ngclient/main.component.ts
+++ com.servoy.eclipse.ngclient.ui/node/src/ngclient/main.component.ts
@@ -1,22 +1,23 @@
 import { Component } from '@angular/core';
 
-import {ServoyService} from './servoy.service';
-import {AllServiceService} from './allservices.service';
+import { ServoyService } from './servoy.service';
+import { AllServiceService } from './allservices.service';
 
-@Component({
-  selector: 'servoy-main',
-  templateUrl: './main.component.html'
-})
+@Component( {
+    selector: 'servoy-main',
+    templateUrl: './main.component.html'
+} )
+
 export class MainComponent {
-  title = 'Servoy NGClient';
-  
-  constructor(private servoyService:ServoyService,    private allService: AllServiceService, ){
-      this.servoyService.connect();
-  }
-  
-  public get mainForm() {
-      const  mainForm = this.servoyService.getSolutionSettings().mainForm;
-      if (mainForm && mainForm.name) return mainForm.name;
-      return null;
-  }
+    title = 'Servoy NGClient';
+
+    constructor( private servoyService: ServoyService, private allService: AllServiceService ) {
+        this.servoyService.connect();
+    }
+
+    public get mainForm() {
+        const mainForm = this.servoyService.getSolutionSettings().mainForm;
+        if ( mainForm && mainForm.name ) return mainForm.name;
+        return null;
+    }
 }
