4bf31bcb2e4327ced9e39221b304e405301c58c5
Gabi Boros <gboros@servoy.com>
2018-06-04 15:41:36 +0300

    SVY-12218 fix default navigator test

--- com.servoy.eclipse.ngclient.ui/node/src/ngclient/main.component.spec.ts
+++ com.servoy.eclipse.ngclient.ui/node/src/ngclient/main.component.spec.ts
@@ -3,15 +3,18 @@ import { TestBed, async } from '@angular/core/testing';
 import { MainComponent } from './main.component';
 import {ServoyService} from './servoy.service';
 import {AllServiceService} from './allservices.service';
+import { FormService } from './form.service';
 
 describe('MainComponent', () => {
   const servicesService = jasmine.createSpyObj('ServoyService', ['connect']);
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        MainComponent,MockFormComponent
+        MainComponent,MockFormComponent,MockDefaultNavigator
       ],
-      providers:    [ {provide: ServoyService, useValue: servicesService },{ provide:AllServiceService, useValue: {} }]
+      providers:    [ {provide: ServoyService, useValue: servicesService },
+        { provide:AllServiceService, useValue: {} },
+        { provide:FormService, useValue: {} }]
     }).compileComponents();
   }));
   it('should create the main component', async(() => {
@@ -30,3 +33,10 @@ describe('MainComponent', () => {
     public name: string;
   }
 
+@Directive({
+    selector: 'svy-default-navigator'
+  })
+  class MockDefaultNavigator{
+    @Input('name')
+    public name: string;
+  }
