4813e9a96859361873a7f1820362168a0bb31761
lvostinar <lvostinar@servoy.com>
2018-08-10 13:58:50 +0300

    SVY-12686 The outline allow me to move elements out of an Absolute Layout Div

--- com.servoy.eclipse.designer/src/com/servoy/eclipse/designer/outline/FormOutlinePage.java
+++ com.servoy.eclipse.designer/src/com/servoy/eclipse/designer/outline/FormOutlinePage.java
@@ -84,6 +84,7 @@ import com.servoy.j2db.persistence.ISupportChilds;
 import com.servoy.j2db.persistence.ISupportExtendsID;
 import com.servoy.j2db.persistence.LayoutContainer;
 import com.servoy.j2db.persistence.WebComponent;
+import com.servoy.j2db.server.ngclient.utils.NGUtils;
 import com.servoy.j2db.util.PersistHelper;
 import com.servoy.j2db.util.UUID;
 
@@ -146,6 +147,12 @@ public class FormOutlinePage extends ContentOutlinePage implements ISelectionLis
 							IPersist real = ((PersistContext)element).getPersist();
 							if (real != null)
 							{
+								if (real.getParent() instanceof LayoutContainer && NGUtils.isAbsoluteLayoutDiv((LayoutContainer)real.getParent()))
+								{
+									// do not allow d&d from absolute layout div
+									event.doit = false;
+									return;
+								}
 								lst.add(real);
 							}
 						}
