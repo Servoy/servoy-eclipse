e43ddf1895427194cd2312968eecdca5f40b2215
Costin Chiulan <costinchiulan@gmail.com>
2018-11-16 16:01:05 +0200

    SVY-13014 Password component for NGClient2 ReviewRefactor - Removed listener functions and ngOnInit as component will use the ones from super.

--- com.servoy.eclipse.ngclient.ui/node/src/servoydefault/password/password.spec.ts
+++ com.servoy.eclipse.ngclient.ui/node/src/servoydefault/password/password.spec.ts
@@ -3,23 +3,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ServoyDefaultPassword } from './password';
 import {SabloModule} from "../../sablo/sablo.module";
 import {FormsModule} from "@angular/forms";
-import {FormattingService, PropertyUtils, ServoyApi} from "../../ngclient/servoy_public";
-import { ServoyService } from '../../ngclient/servoy.service'
+import {FormattingService, ServoyApi,TooltipService,TooltipDirective} from "../../ngclient/servoy_public";
 describe('PasswordComponent', () => {
   let component: ServoyDefaultPassword;
   let fixture: ComponentFixture<ServoyDefaultPassword>;
   let servoyApi;
 
   beforeEach(async(() => {
-    servoyApi =  jasmine.createSpyObj("ServoyApi", ["getMarkupId","trustAsHtml"]);
-    TestBed.configureTestingModule({
-      declarations: [ ServoyDefaultPassword ],
-      imports: [SabloModule, FormsModule],
-      providers: [FormattingService]
-    })
-    .compileComponents();
-  }));
-
+      servoyApi =  jasmine.createSpyObj("ServoyApi", ["getMarkupId","trustAsHtml"]);
+      TestBed.configureTestingModule({
+        declarations: [ ServoyDefaultPassword, TooltipDirective ],
+        imports: [SabloModule, FormsModule],
+        providers: [FormattingService, TooltipService]
+      })
+      .compileComponents();
+    }));
+  
   beforeEach(() => {
     fixture = TestBed.createComponent(ServoyDefaultPassword);
     fixture.componentInstance.servoyApi = servoyApi as ServoyApi;
--- com.servoy.eclipse.ngclient.ui/node/src/servoydefault/password/password.ts
+++ com.servoy.eclipse.ngclient.ui/node/src/servoydefault/password/password.ts
@@ -1,42 +1,12 @@
-import {Component, OnInit, Renderer2, SimpleChanges} from '@angular/core';
+import {Component, OnInit, Renderer2} from '@angular/core';
 import {FormattingService} from "../../ngclient/servoy_public";
 import {ServoyDefaultBaseField} from "../basefield";
 @Component({
   selector: 'servoydefault-password',
   templateUrl: './password.html'
 })
-export class ServoyDefaultPassword extends ServoyDefaultBaseField implements OnInit {
+export class ServoyDefaultPassword extends ServoyDefaultBaseField implements OnInit{
   constructor(renderer: Renderer2, formattingService: FormattingService) {
     super(renderer, formattingService);
   }
-
-  ngOnInit() {
-  }
-
-  ngOnChanges(changes: SimpleChanges){
-    super.ngOnChanges(changes);
-  }
-
-  onClick(event) {
-    if (this.editable == false && this.onActionMethodID) {
-      this.onActionMethodID(event);
-    }
-  };
-  
-  focus(event){
-    if(this.onFocusGainedMethodID){
-        this.onFocusGainedMethodID(event);
-    }
-  }
-
-  blur(event){
-    if(this.onFocusLostMethodID)
-      this.onFocusLostMethodID(event);
-  }
-
-  contextMenu(event){
-    if(this.onRightClickMethodID)
-      this.onRightClickMethodID(event);
-  }
-
 }
