78aa4a2dccad5fb66e851032ad8700dc7a1d64c0
emera <emera@servoy.com>
2019-01-16 14:13:12 +0200

    SVY-13223 filter templates by layout type also in the old editor action

--- com.servoy.eclipse.designer/src/com/servoy/eclipse/designer/actions/ApplyTemplateActionDelegate.java
+++ com.servoy.eclipse.designer/src/com/servoy/eclipse/designer/actions/ApplyTemplateActionDelegate.java
@@ -80,7 +80,8 @@ public class ApplyTemplateActionDelegate extends AbstractEditpartActionDelegate
 						{
 							Template template = (Template)templates.get(i);
 							JSONObject templateJSON = new ServoyJSONObject(template.getContent(), false);
-							if (!templateJSON.has(Template.PROP_LAYOUT) || templateJSON.get(Template.PROP_LAYOUT).equals(layout))
+							if (!templateJSON.has(Template.PROP_LAYOUT) && Template.LAYOUT_TYPE_ABSOLUTE.equals(layout) ||
+								templateJSON.get(Template.PROP_LAYOUT).equals(layout))
 							{
 								elements.add(new TemplateElementHolder(template));
 							}
