5e1e98083baa3b25d31062832bc7b6f22e29ad1a
emera <emera@servoy.com>
2019-02-04 20:31:32 +0200

    SVY-13314 template elements are duplicated when the selection contains nested elements

--- com.servoy.eclipse.designer/src/com/servoy/eclipse/designer/editor/commands/SaveAsTemplateAction.java
+++ com.servoy.eclipse.designer/src/com/servoy/eclipse/designer/editor/commands/SaveAsTemplateAction.java
@@ -19,6 +19,8 @@ package com.servoy.eclipse.designer.editor.commands;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.BiPredicate;
+import java.util.stream.Collectors;
 
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IStatus;
@@ -48,6 +50,7 @@ import com.servoy.j2db.persistence.IPersist;
 import com.servoy.j2db.persistence.IRepository;
 import com.servoy.j2db.persistence.IRootObject;
 import com.servoy.j2db.persistence.IScriptElement;
+import com.servoy.j2db.persistence.ISupportChilds;
 import com.servoy.j2db.persistence.LayoutContainer;
 import com.servoy.j2db.persistence.RepositoryException;
 import com.servoy.j2db.persistence.StringResource;
@@ -86,6 +89,8 @@ public class SaveAsTemplateAction extends SelectionAction
 	}
 
 	private boolean groupTemplateElements = false;
+	private final BiPredicate<List<IPersist>, ISupportChilds> isNested = (List<IPersist> selected, ISupportChilds t) -> selected.contains(t) ? true
+		: t.getParent() instanceof LayoutContainer && this.isNested.test(selected, t.getParent());
 
 	private static Pair<String, Boolean> askForTemplateName(Shell shell, boolean grouping)
 	{
@@ -223,7 +228,8 @@ public class SaveAsTemplateAction extends SelectionAction
 			return;
 		}
 
-		persists = completeContainment(form, persists);
+		List<IPersist> selected = completeContainment(form, persists);
+		List<IPersist> sel = selected.stream().filter(p -> !isNested.test(selected, p.getParent())).collect(Collectors.toList());
 
 		ServoyModelManager.getServoyModelManager().getServoyModel();
 		EclipseRepository repository = (EclipseRepository)ServoyModel.getDeveloperRepository();
@@ -241,7 +247,7 @@ public class SaveAsTemplateAction extends SelectionAction
 				template = existingTemplate;
 			}
 			template.setResourceType(resourceType);
-			template.setContent(ElementFactory.createTemplateContent(repository, form, persists, resourceType, groupTemplateElements));
+			template.setContent(ElementFactory.createTemplateContent(repository, form, sel, resourceType, groupTemplateElements));
 			repository.updateRootObject(template);
 		}
 		catch (JSONException e)
