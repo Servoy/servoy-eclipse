30d7474987360bfe0a3da4cbb78c96f474dcfe07
Johan Compagner <jcompagner@servoy.com>
2019-06-20 09:40:15 +0200

    SVY-13826 Download automatically newer NGDesktop version
    e.printStacktrace -> logError

--- com.servoy.eclipse.debug/src/com/servoy/eclipse/debug/handlers/StartNGDesktopClientHandler.java
+++ com.servoy.eclipse.debug/src/com/servoy/eclipse/debug/handlers/StartNGDesktopClientHandler.java
@@ -282,7 +282,7 @@ public class StartNGDesktopClientHandler extends StartDebugHandler implements IR
 					}
 					catch (IOException e)
 					{
-						e.printStackTrace();
+						ServoyLog.logError("error adjusting the ngdesktop executable (servoy.json)", e);
 					}
 				}
 				else
