56a4875a4de074d1014ffc938b5263428ec607b2
lvostinar <lvostinar@servoy.com>
2019-08-22 13:32:28 +0300

    SVY-13931 Form Editor: Ghost Insert at drag&drop

--- com.servoy.eclipse.designer.rfb/src/rfb/angular/js/modules/highlight/highlight.js
+++ com.servoy.eclipse.designer.rfb/src/rfb/angular/js/modules/highlight/highlight.js
@@ -10,7 +10,7 @@ angular.module('highlight', ['editor']).run(function($pluginRegistry, $editorSer
 		function getHighlightNode(event) {
 			if (utils.getDraggingFromPallete() != null && editorScope.getEditorContentRootScope().drop_highlight) {
 				var drop = editorScope.getEditorContentRootScope().drop_highlight.split(".");
-				var canDrop = utils.getDropNode(utils.getDraggingFromPallete(), null, drop[drop.length-1], event);
+				var canDrop = utils.getDropNode(utils.getDraggingFromPallete(), null, editorScope.getEditorContentRootScope().drop_highlight, event);
 				if (canDrop && canDrop.dropAllowed && canDrop.dropTarget)
 				{
 					return canDrop.dropTarget;
