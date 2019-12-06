82c2eaa95acf5f2f09d82d387cea0bc49c444c35
lvostinar <lvostinar@servoy.com>
2019-12-06 17:45:03 +0200

    SVY-13208 add html area in ng2

--- com.servoy.eclipse.ngclient.ui/node/src/servoydefault/htmlarea/htmlarea.ts
+++ com.servoy.eclipse.ngclient.ui/node/src/servoydefault/htmlarea/htmlarea.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit, Renderer2, SimpleChanges} from '@angular/core';
+import {Component, Input, OnInit, Renderer2, SimpleChanges,ViewChild, ElementRef, AfterViewInit} from '@angular/core';
 import {ServoyDefaultBaseField} from "../basefield";
 import {FormattingService} from "../../ngclient/servoy_public";
 import { AngularEditorConfig } from '@kolkov/angular-editor';
@@ -7,10 +7,10 @@ import { AngularEditorConfig } from '@kolkov/angular-editor';
   selector: 'servoydefault-htmlarea',
   templateUrl: './htmlarea.html',
 })
-export class ServoyDefaultHtmlarea extends ServoyDefaultBaseField implements OnInit {
+export class ServoyDefaultHtmlarea extends ServoyDefaultBaseField implements AfterViewInit {
 
   @Input() editable;
-
+  
   config: AngularEditorConfig = {
           editable: true,
           spellcheck: true,
@@ -22,11 +22,14 @@ export class ServoyDefaultHtmlarea extends ServoyDefaultBaseField implements OnI
     super(renderer, formattingService);
   }
   
-  ngOnInit(){
-    super.ngOnInit();
-  }
-
-  ngOnChanges(changes: SimpleChanges){
-    super.ngOnChanges(changes);
+  ngAfterViewInit() {
+      // ugly hack to fix the height
+      let nativeElement = this.getNativeElement();
+      let componentHeight = nativeElement.offsetHeight;
+      //let toolBarHeight = nativeElement.childNodes[0].childNodes[0].childNodes[1].childNodes[1].offsetHeight;
+      let initialContentHeight = nativeElement.childNodes[0].childNodes[0].childNodes[2].childNodes[0].offsetHeight;
+      let initialEditorHeight = nativeElement.childNodes[0].childNodes[0].offsetHeight;
+      
+      this.renderer.setStyle( nativeElement.childNodes[0].childNodes[0].childNodes[2].childNodes[0], "height",  (initialContentHeight + componentHeight - initialEditorHeight) +'px');
   }
 }
