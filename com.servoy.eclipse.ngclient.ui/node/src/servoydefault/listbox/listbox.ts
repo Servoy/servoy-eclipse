ff15d9ec0a78c2e695f3d20d55658db756a508e7
Diana Bunaciu <dtimut@servoy.com>
2020-01-09 13:28:12 +0200

    SVY-14515 [NG2] Listbox issues refactor listbox.ts + use ngModel value

--- com.servoy.eclipse.ngclient.ui/node/src/servoydefault/listbox/listbox.ts
+++ com.servoy.eclipse.ngclient.ui/node/src/servoydefault/listbox/listbox.ts
@@ -1,36 +1,36 @@
-import {Component, Renderer2, ViewChild, Input, ElementRef, ChangeDetectorRef, AfterViewInit, SimpleChanges} from '@angular/core';
+import { Component, Renderer2, ViewChild, Input, ElementRef, ChangeDetectorRef, AfterViewInit, SimpleChanges } from '@angular/core';
 
-import {FormattingService} from '../../ngclient/servoy_public'
+import { FormattingService } from '../../ngclient/servoy_public'
 
-import {ServoyDefaultBaseField} from '../basefield'
+import { ServoyDefaultBaseField } from '../basefield'
 
 @Component( {
     selector: 'servoydefault-listbox',
     templateUrl: './listbox.html'
 } )
-export class ServoyDefaultListBox extends ServoyDefaultBaseField implements AfterViewInit{
+export class ServoyDefaultListBox extends ServoyDefaultBaseField implements AfterViewInit {
     @Input() multiselectListbox;
-    
+
     selectedValues: any[];
-        
-    @ViewChild('element', {static: false}) elementRef:ElementRef;
-    
+
+    @ViewChild( 'element', { static: false } ) elementRef: ElementRef;
+
     private changes: SimpleChanges;
-    
-    constructor(private changeDetectorRef : ChangeDetectorRef,  renderer: Renderer2, formattingService : FormattingService) { 
-        super(renderer,formattingService);
+
+    constructor( private changeDetectorRef: ChangeDetectorRef, renderer: Renderer2, formattingService: FormattingService ) {
+        super( renderer, formattingService );
     }
-    
+
     ngOnInit() {
         //this method should do nothing
     }
-    
+
     ngAfterViewInit() {
         this.ngOnChanges( this.changes );
         this.changeDetectorRef.detectChanges();
-        this.addAttributes(); 
-        this.attachFocusListeners(this.getFocusElement());
-        this.attachHandlers(); 
+        this.addAttributes();
+        this.attachFocusListeners( this.getFocusElement() );
+        this.attachHandlers();
     }
 
     ngOnChanges( changes: SimpleChanges ) {
@@ -49,36 +49,23 @@ export class ServoyDefaultListBox extends ServoyDefaultBaseField implements Afte
                 let change = changes[property];
                 switch ( property ) {
                     case "dataProviderID":
+                        if ( this.selectedValues ) {
+                            break;
+                        }
                         this.dataProviderID += '';
-                        this.selectedValues = this.dataProviderID.split('\n');
+                        this.selectedValues = this.dataProviderID.split( '\n' );
                         break;
 
                 }
             }
-            super.ngOnChanges(changes);
+            super.ngOnChanges( changes );
         }
     }
-    
+
     multiUpdate( isMultiSelect: boolean ) {
-        var select = this.elementRef.nativeElement;
-        var newValue = select.value;
-        if (isMultiSelect && newValue)
-        {
-            var result = [];
-            var options = select && select.options;
-            var opt;
-
-            for (var i=0, iLen=options.length; i<iLen; i++) {
-              opt = options[i];
-
-              if (opt.selected) {
-                result.push(opt.text);
-              }
-            }
-            
-            newValue = result.join('\n');
-            this.selectedValues = result;
-        }   
-        this.update(newValue);
+        for ( var i = 0; i < this.selectedValues.length; i += 1 ) {
+            this.selectedValues[i] = '' + this.selectedValues[i];
+        }
+        this.update( this.selectedValues.join( '\n' ) );
     }
 }
