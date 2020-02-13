8607cad77d76ccbd582d75ccac95afee2d70bec1
Johan Compagner <jcompagner@servoy.com>
2020-02-13 11:33:18 +0100

    SVY-14701 upgrade and improve NG2 to the latest angular 9
    refactor the start edit directive to do the same as image media field
    directive. For thos the "[hostComponent]" property has to be given into
    the template which should be injected as our ServoyBaseComponent
    Adjusted all templates (textfield,textarea,calender) to use that

--- com.servoy.eclipse.ngclient.ui/node/src/ngclient/utils/startedit.directive.ts
+++ com.servoy.eclipse.ngclient.ui/node/src/ngclient/utils/startedit.directive.ts
@@ -1,32 +1,28 @@
 import { Directive , Input , HostListener, ViewContainerRef} from '@angular/core';
 import {LoggerService, LoggerFactory} from '../../sablo/logger.service'
+import { ServoyBaseComponent } from '../servoy_public';
 
 @Directive({
   selector: '[svyStartEdit]'
 })
 export class StartEditDirective {
 
-  @Input('svyStartEdit') dataProviderID : string;
+  @Input('svyStartEdit') dataProviderID: string;
+  @Input() hostComponent: ServoyBaseComponent;
 
   private log: LoggerService;
-  private field: any;
-  
-  public constructor(private logFactory:LoggerFactory, private viewContainer: ViewContainerRef) {
-    this.log = logFactory.getLogger("StartEditDirective");
-    if(this.viewContainer['_view'] != undefined && this.viewContainer['_view']['component'] != undefined) {
-      this.field = this.viewContainer['_view']['component'];
-    }
-    else {
-      this.log.error("Can't find field for startEdit");
-    }
+
+  public constructor(private logFactory: LoggerFactory, private viewContainer: ViewContainerRef) {
+    this.log = logFactory.getLogger('StartEditDirective');
   }
 
-  @HostListener('focus',['$event']) onFocus(e:FocusEvent) {
-    if(this.field != undefined && this.field.servoyApi && this.dataProviderID != undefined) {
-      this.field.servoyApi.startEdit(this.dataProviderID);
-    }
-    else {
-      this.log.error("Can't call startEdit, missing servoyApi and dataProviderID for field " + this.field);
+  @HostListener('focus', ['$event']) onFocus(e: FocusEvent) {
+    if (!this.hostComponent) {
+      this.log.error('host component not found for the start edit directive use [hostComponent]="self" besides this in the template (component must be extending ServoyBaseComponent)');
+    } else if (this.hostComponent.servoyApi && this.dataProviderID !== undefined) {
+      this.hostComponent.servoyApi.startEdit(this.dataProviderID);
+    } else {
+      this.log.error('Can\'t call startEdit, missing servoyApi and dataProviderID for field ' + this.hostComponent);
     }
   }
 }
