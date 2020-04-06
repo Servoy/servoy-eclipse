c4509904472c02fa1a2b9a931d5be8756bf34d7a
Johan Compagner <jcompagner@servoy.com>
2020-04-06 13:30:07 +0200

    SVY-14833 update to eclipse 4.15
    patch on top of jface 4.15 version
    1> pom parent, pointing to our parent
    2> pom version fixed to the jface that is shipped with 4.15
    3> manfiest version fixed to jface that is shpiped with 4.15
    4> patched ImageDescriptor to use the new interface

--- org.eclipse.jface/src/org/eclipse/jface/resource/ImageDescriptor.java
+++ org.eclipse.jface/src/org/eclipse/jface/resource/ImageDescriptor.java
@@ -61,6 +61,11 @@ import org.eclipse.swt.widgets.Display;
 public abstract class ImageDescriptor extends DeviceResourceDescriptor {
 
 	/**
+	 *
+	 */
+	public volatile static ImageDescriptorRelay loaderRelay = null;
+
+	/**
 	 * A small red square used to warn that an image cannot be created.
 	 */
 	protected static final ImageData DEFAULT_IMAGE_DATA = new ImageData(6, 6, 1, new PaletteData(new RGB(255, 0, 0)));
@@ -80,6 +85,11 @@ public abstract class ImageDescriptor extends DeviceResourceDescriptor {
 	 * @return a new image descriptor
 	 */
 	public static ImageDescriptor createFromFile(Class<?> location, String filename) {
+		if (loaderRelay != null) {
+			URL url = loaderRelay.getReplacementFromFile(location, filename);
+			if (url != null)
+				return new URLImageDescriptor(url);
+		}
 		return new FileImageDescriptor(location, filename);
 	}
 
@@ -174,6 +184,11 @@ public abstract class ImageDescriptor extends DeviceResourceDescriptor {
 	public static ImageDescriptor createFromURL(URL url) {
 		if (url == null) {
 			return getMissingImageDescriptor();
+		}
+		if (loaderRelay != null) {
+			URL replacement = loaderRelay.getReplacementFromURL(url);
+			if (replacement != null)
+				return new URLImageDescriptor(replacement);
 		}
 		return new URLImageDescriptor(url);
 	}
