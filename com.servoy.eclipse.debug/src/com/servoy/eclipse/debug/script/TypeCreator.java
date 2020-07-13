576a4cf0a9d6957f609ab55f1748e0d3e328be2c
Johan Compagner <jcompagner@servoy.com>
2020-07-13 15:23:09 +0200

    SVY-15152 JSFoundSet not seen as an object in method parameters
    revert of prevous commit, needs to solve it differently that all servoy
    objects are java (because of overloads) and that assignable check will
    look at a marker interface "IJavaScriptAlike"

--- com.servoy.eclipse.debug/src/com/servoy/eclipse/debug/script/TypeCreator.java
+++ com.servoy.eclipse.debug/src/com/servoy/eclipse/debug/script/TypeCreator.java
@@ -1386,7 +1386,7 @@ public class TypeCreator extends TypeCache
 	{
 		Type type = TypeInfoModelFactory.eINSTANCE.createType();
 		type.setName(typeName);
-		type.setKind(Scriptable.class.isAssignableFrom(cls) ? TypeKind.JAVASCRIPT : TypeKind.JAVA);
+		type.setKind(TypeKind.JAVA);
 		EList<Member> members = type.getMembers();
 		fill(context, members, cls, typeName);
 
@@ -4977,7 +4977,7 @@ public class TypeCreator extends TypeCache
 		{
 			Type type = TypeInfoModelFactory.eINSTANCE.createType();
 			type.setName(fullTypeName);
-			type.setKind(TypeKind.JAVASCRIPT); // this is a record,foundset or forms that are Scriptable so javascript objects.
+			type.setKind(TypeKind.JAVA);
 			type.setAttribute(IMAGE_DESCRIPTOR, imageDescriptor);
 			type.setSuperType(superType);
 			type.getMembers().addAll(members);
@@ -5049,7 +5049,7 @@ public class TypeCreator extends TypeCache
 		type.setAttribute(IMAGE_DESCRIPTOR, imageDescriptor);
 		type.setSuperType(superType);
 		type.setName(fullTypeName);
-		type.setKind(TypeKind.JAVASCRIPT); // this is a record,foundset or forms that are Scriptable so javascript objects.
+		type.setKind(TypeKind.JAVA);
 
 		if (table != null)
 		{
