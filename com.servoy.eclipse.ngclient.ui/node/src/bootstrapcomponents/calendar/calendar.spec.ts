46448558c80c29011ec682eb06313b99aed7e289
lvostinar <lvostinar@servoy.com>
2020-07-28 11:17:38 +0300

    SVY-15216 [ng2] implement calendar and calendarinline bootstrap components - fix test

--- com.servoy.eclipse.ngclient.ui/node/src/bootstrapcomponents/calendar/calendar.spec.ts
+++ com.servoy.eclipse.ngclient.ui/node/src/bootstrapcomponents/calendar/calendar.spec.ts
@@ -2,13 +2,17 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ServoyBootstrapCalendar } from './calendar';
 
+import { ServoyPublicModule } from '../../ngclient/servoy_public.module'
+import { SabloModule } from '../../sablo/sablo.module'
+
 describe('CalendarComponent', () => {
   let component: ServoyBootstrapCalendar;
   let fixture: ComponentFixture<ServoyBootstrapCalendar>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ServoyBootstrapCalendar ]
+      declarations: [ ServoyBootstrapCalendar ],
+      imports: [ServoyPublicModule,SabloModule],
     })
     .compileComponents();
   }));
