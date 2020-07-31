384b557fddf323650f18a56eb1ed73e1102d6ca4
emera <emera@servoy.com>
2020-07-31 21:39:24 +0300

    SVY-14333 [NG2] add support for the extra-table component (scroll to selection and key navigation support)

--- com.servoy.eclipse.ngclient.ui/node/src/servoyextra/table/table.ts
+++ com.servoy.eclipse.ngclient.ui/node/src/servoyextra/table/table.ts
@@ -37,6 +37,8 @@ export class ServoyExtraTable extends ServoyBaseComponent implements AfterViewIn
     @Input() responsiveHeight;
     @Input() responsiveDynamicHeight;
     @Input() absoluteLayout : boolean;
+    @Input() lastSelectionFirstElement: number;
+    @Input() keyCodeSettings : {arrowUp: boolean , arrowDown: boolean, end:boolean, enter:boolean, home: boolean, pageDown: boolean, pageUp: boolean};
 
     @Input() onViewPortChanged;
     @Input() onCellClick;
@@ -94,24 +96,58 @@ export class ServoyExtraTable extends ServoyBaseComponent implements AfterViewIn
 
         this.changeListener = this.foundset.addChangeListener((event: FoundsetChangeEvent) => {
             if (event.sortColumnsChanged) {
-                let sortSet = false;
-                let sortColumnsA = this.foundset.sortColumns.split(/[\s,]+/);
-                if (sortColumnsA.length >= 2) {
-                    for (let i = 0; i < this.columns.length; i++) {
-                        if (this.columns[i].dataprovider && sortColumnsA[0] == this.columns[i].dataprovider.idForFoundset) {
-                            this.sortColumnIndex = i;
-                            this.sortDirection = sortColumnsA[1].toLowerCase() == 'asc' ? 'up' : 'down';
-                            sortSet = true;
-                            break;
-                        }
-                    }
-                }
-                if(!sortSet) {
-                    this.sortColumnIndex = -1;
-                    this.sortDirection = null;
-                }
+                this.sortColumnsChanged(event);
+            }
+
+            if (event.selectedRowIndexesChanged) {
+                this.selectedRowIndexesChanged(event.selectedRowIndexesChanged.oldValue);
             }
         });
+
+        window.setTimeout(() => {
+			//first time we need to wait a bit before we scroll
+            this.scrollToSelection();
+        }, 100);
+    }
+
+    private selectedRowIndexesChanged(oldValue: number[]) {
+        if (this.foundset.selectedRowIndexes.length > 0) {
+            if (this.foundset.selectedRowIndexes != oldValue || this.lastSelectionFirstElement != this.foundset.selectedRowIndexes[0]) {
+                if (this.lastSelectionFirstElement != this.foundset.selectedRowIndexes[0]) {
+                    this.log.spam("svy extra table * selectedRowIndexes changed; scrollToSelectionNeeded = true");
+                    this.lastSelectionFirstElement = this.foundset.selectedRowIndexes[0];
+                    this.scrollToSelection();
+                }
+            }
+        }
+        else {
+            this.lastSelectionFirstElement = -1;
+        }
+    }
+
+    private sortColumnsChanged(event: FoundsetChangeEvent) {
+        let sortSet = false;
+        let sortColumnsA = this.foundset.sortColumns.split(/[\s,]+/);
+        if (sortColumnsA.length >= 2) {
+            for (let i = 0; i < this.columns.length; i++) {
+                if (this.columns[i].dataprovider && sortColumnsA[0] == this.columns[i].dataprovider.idForFoundset) {
+                    this.sortColumnIndex = i;
+                    this.sortDirection = sortColumnsA[1].toLowerCase() == 'asc' ? 'up' : 'down';
+                    sortSet = true;
+                    break;
+                }
+            }
+        }
+        if (!sortSet) {
+            this.sortColumnIndex = -1;
+            this.sortDirection = null;
+        }
+    }
+
+    private scrollToSelection() {
+        if (this.lastSelectionFirstElement !== -1) {
+            this.viewPort.scrollToIndex(this.lastSelectionFirstElement);
+        }
     }
 
     private computeTableHeight() {
@@ -178,13 +214,31 @@ export class ServoyExtraTable extends ServoyBaseComponent implements AfterViewIn
 
         if (this.onFocusGainedMethodID)
         {
-            this.renderer.listen(this.getNativeElement().getElementsByTagName("table")[0], 'focus', e => this.onFocusGainedMethodID(e));
+            this.renderer.listen(this.getNativeElement().getElementsByTagName("table")[0], 'focus', e => {
+                this.callFocusGained(e);
+            });
         }
 
         if (this.onFocusLostMethodID)
         {
-            this.renderer.listen(this.getNativeElement().getElementsByTagName("table")[0], 'blur', e=> this.onFocusLostMethodID(e));
+            this.renderer.listen(this.getNativeElement().getElementsByTagName("table")[0], 'blur', e => {
+                this.callFocusLost(e);   
+            });
         }
+    }
+
+    private callFocusLost(e: any) {
+        if (!this.skipOnce) {
+            this.onFocusLostMethodID(e);
+        }
+        this.skipOnce = false;
+    }
+
+    private callFocusGained(e: any) {
+        if (!this.skipOnce) {
+            this.onFocusGainedMethodID(e);
+        }
+        this.skipOnce = false;
     }
 
     private headerClicked(i: number, event?: Event):  void {
@@ -291,7 +345,7 @@ export class ServoyExtraTable extends ServoyBaseComponent implements AfterViewIn
         if (!this.autoColumns) return null;
 
         for (let autoColumnIdx in this.autoColumns["columns"]) {
-            var w = this.columns[autoColumnIdx].width;
+            let w = this.columns[autoColumnIdx].width;
             if (w) {
                 w = w.trim();
                 if (w.indexOf("%") == w.length - 1) {
@@ -326,7 +380,7 @@ export class ServoyExtraTable extends ServoyBaseComponent implements AfterViewIn
         if (this.columns) {
             for (let i = 0; i < this.columns.length; i++) {
                 if (!this.isAutoResizeColumn(i) && this.getNumberFromPxString(this.columns[i].initialWidth) > 0) {
-                    let w = this.getNumberFromPxString(this.columns[i].width);
+                    const w = this.getNumberFromPxString(this.columns[i].width);
                     if (w > -1) {
                         this.tableWidth += w;
                     }
@@ -342,14 +396,14 @@ export class ServoyExtraTable extends ServoyBaseComponent implements AfterViewIn
     setColumnsToInitalWidthAndInitAutoColumns() {
         let newAutoColumns = { columns: { }, minWidth: { }, autoResize: {}, count: 0 };
         if (this.columns) {
-            for (var i = 0; i < this.columns.length; i++) {
+            for (let i = 0; i < this.columns.length; i++) {
                 if (this.columns[i].initialWidth == undefined) {
                     this.columns[i].initialWidth = this.columns[i].width == undefined ? "" : this.columns[i].width;
                 } else {
                     this.columns[i].width = this.columns[i].initialWidth;
                 }
 
-                var minWidth = this.getNumberFromPxString(this.columns[i].width);
+                let minWidth = this.getNumberFromPxString(this.columns[i].width);
                 if (this.isAutoResizeColumn(i) || minWidth < 0) {
                     newAutoColumns.columns[i] = true;
                     newAutoColumns.minWidth[i] = minWidth;
@@ -384,8 +438,8 @@ export class ServoyExtraTable extends ServoyBaseComponent implements AfterViewIn
         for (let i = 0; i < this.columns.length; i++) {
             if (this.autoColumns.autoResize[i]) {
                 if (this.autoColumns.minWidth[i] > 0) {
-                    var oldW = this.getNumberFromPxString(this.columns[i].width);
-                    var w = Math.floor(oldW * newWidth / oldWidth);
+                    const oldW = this.getNumberFromPxString(this.columns[i].width);
+                    let w = Math.floor(oldW * newWidth / oldWidth);
                     
                     if (w < this.autoColumns.minWidth[i]) {
                         w = this.autoColumns.minWidth[i];
@@ -481,7 +535,7 @@ export class ServoyExtraTable extends ServoyBaseComponent implements AfterViewIn
             tBodyStyle['top'] = tblHead.offsetHeight + "px";
         }
        if (this.showPagination()) {
-            var pagination = this.getNativeElement().getElementsByTagName('ngb-pagination');
+            const pagination = this.getNativeElement().getElementsByTagName('ngb-pagination');
             if (pagination[0] && pagination[0].children[0]) {
                 tBodyStyle['margin-bottom'] = (pagination[0].children[0].clientHeight + 2) + "px";
                 this.renderer.setStyle(pagination[0].children[0], "margin-bottom", "0");
@@ -524,7 +578,7 @@ export class ServoyExtraTable extends ServoyBaseComponent implements AfterViewIn
             }
         }
 
-        for (var p in style) {
+        for (let p in style) {
             this.columnCSSRules[columnIndex].style[p] = style[p];
         }
     }
@@ -611,10 +665,13 @@ export class ServoyExtraTable extends ServoyBaseComponent implements AfterViewIn
 
     showPagination() {
         return this.pageSize && this.foundset && (this.foundset.serverSize > this.pageSize || this.foundset.hasMoreRows);
-
     }
 
-    modifyPage() {
+    modifyPage(count?:number) {
+        if (count)
+        {
+            this.currentPage += count;
+        }
         this.viewPort.setRenderedRange({start: this.pageSize * (this.currentPage - 1), end: this.pageSize * this.currentPage - 1});
     }
 
@@ -628,4 +685,184 @@ export class ServoyExtraTable extends ServoyBaseComponent implements AfterViewIn
         }
         return rowClass;
     }
+
+    keypressed(event: KeyboardEvent) {
+        let fs = this.foundset;
+        if (fs.selectedRowIndexes && fs.selectedRowIndexes.length > 0) {
+            let selectionChanged = false;
+            let oldSelectedIdxs = fs.selectedRowIndexes.slice();
+            let selection = fs.selectedRowIndexes[0];
+            if (event.keyCode == 33) { // PAGE UP KEY
+                if (this.keyCodeSettings && !this.keyCodeSettings.pageUp) return;
+                
+                const firstVisibleIndex = this.showPagination() ? this.pageSize * (this.currentPage - 1) : 1;
+                fs.selectedRowIndexes = [firstVisibleIndex];
+                selectionChanged = (selection != firstVisibleIndex);
+                this.log.spam("svy extra table * keyPressed; scroll on PG UP");
+                this.viewPort.scrollToIndex(firstVisibleIndex);
+            }
+            else if (event.keyCode == 34) { // PAGE DOWN KEY
+                if (this.keyCodeSettings && !this.keyCodeSettings.pageDown) return;
+
+                const lastVisibleIndex = this.showPagination() ? this.pageSize * this.currentPage - 1 : (this.foundset.viewPort.rows.length - 1);
+                fs.selectedRowIndexes = [lastVisibleIndex];
+                selectionChanged = (selection != lastVisibleIndex);
+                this.log.spam("svy extra table * keyPressed; scroll on PG DOWN");
+                this.viewPort.scrollToIndex(lastVisibleIndex);
+            } else if (event.keyCode == 38) { // ARROW UP KEY
+                if (this.keyCodeSettings && !this.keyCodeSettings.arrowUp) return;
+                
+                if (selection > 0) {
+                    fs.selectedRowIndexes = [selection - 1];
+                    selectionChanged = true;
+                    this.viewPort.scrollToIndex(selection - 1);
+                }
+                event.preventDefault();
+            } else if (event.keyCode == 40) { // ARROW DOWN KEY
+                if (this.keyCodeSettings && !this.keyCodeSettings.arrowDown) return;
+                
+                if (selection + 1 < (fs.viewPort.startIndex + fs.viewPort.size)) {
+                    fs.selectedRowIndexes = [selection + 1];
+                    this.viewPort.scrollToIndex(selection + 1);
+                    selectionChanged = true;
+                }
+                event.preventDefault();
+            } else if (event.keyCode == 13) { // ENTER KEY
+                if (!this.keyCodeSettings.enter) return;
+                if (this.onCellClick) {
+                    this.onCellClick(selection + 1, null, fs.viewPort.rows[selection])
+                }
+            } else if (event.keyCode == 36) { // HOME
+                if (this.keyCodeSettings && !this.keyCodeSettings.home) return;
+
+                let allowedBounds = this.calculateAllowedLoadedDataBounds();
+                if (fs.viewPort.startIndex > allowedBounds.startIdx) { // see if we have the first record loaded
+                    this.loadFirstRecordsIfNeeded(allowedBounds);
+                }
+                else if (allowedBounds.size > 0) {
+                    if (selection != allowedBounds.startIdx)
+                    {
+                        this.foundset.requestSelectionUpdate([allowedBounds.startIdx]);
+                        this.foundsetChange.emit(this.foundset);
+                    } 
+                    else {
+                        this.viewPort.scrollToIndex(fs.viewPort.startIndex);
+                    }
+                }
+                event.preventDefault()
+                event.stopPropagation();
+
+            } else if (event.keyCode == 35) { // END
+                if (this.keyCodeSettings && !this.keyCodeSettings.end) return;
+
+                let allowedBounds = this.calculateAllowedLoadedDataBounds();
+                if (fs.viewPort.startIndex + fs.viewPort.size < allowedBounds.startIdx + allowedBounds.size) { // see if we already have the last record loaded or not
+                    this.loadLastRecordsIfNeeded(allowedBounds);
+                } 
+                else if (allowedBounds.size > 0) {
+                    if (selection != allowedBounds.startIdx + allowedBounds.size - 1) {
+                        this.foundset.requestSelectionUpdate([allowedBounds.startIdx + allowedBounds.size - 1]);
+                        this.foundsetChange.emit(this.foundset);
+                    }
+                    else {
+                        this.viewPort.scrollToIndex(fs.viewPort.size - 1);
+                    }
+                }
+
+                event.preventDefault();
+                event.stopPropagation();
+            }
+
+            if (selectionChanged) {
+                this.selectedRowIndexesChanged(oldSelectedIdxs);
+            }
+        }
+    }
+
+    loadFirstRecordsIfNeeded(allowedBounds: { startIdx: any; size: any; }) {
+        // this can be executed delayed, after pending loads finish, so do check again if we still need to load bottom of foundset
+        if (this.foundset.viewPort.startIndex > allowedBounds.startIdx) {
+            let newLoadPromise = this.foundset.loadRecordsAsync(allowedBounds.startIdx, Math.min(allowedBounds.size, this.getInitialPreferredLoadedSize()));
+            newLoadPromise.then(() => {
+                //runWhenThereIsNoPendingLoadRequest(this.loadFirstRecordsIfNeeded);
+                this.loadFirstRecordsIfNeeded(allowedBounds);
+            });
+            this.foundsetChange.emit(this.foundset);
+            return newLoadPromise;
+        } else if (allowedBounds.size > 0) {
+            this.foundset.requestSelectionUpdate([allowedBounds.startIdx]).then(() => {
+                this.scrollToSelection();
+            });
+        }
+    }
+
+    loadLastRecordsIfNeeded(allowedBounds: { startIdx: any; size: any; }) {
+        // this can be executed delayed, after pending loads finish, so do check again if we still need to load bottom of foundset
+        if (this.foundset.viewPort.startIndex + this.foundset.viewPort.size < allowedBounds.startIdx + allowedBounds.size) {
+            const firstIndexToLoad = Math.max(allowedBounds.startIdx, allowedBounds.startIdx + allowedBounds.size - this.getInitialPreferredLoadedSize());
+            const newLoadPromise = this.foundset.loadRecordsAsync(firstIndexToLoad, allowedBounds.startIdx + allowedBounds.size - firstIndexToLoad);
+            newLoadPromise.then(() => {
+                // just in case server side foundset was not fully loaded and now that we accessed last part of it it already loaded more records
+                //runWhenThereIsNoPendingLoadRequest(this.loadLastRecordsIfNeeded);
+                this.loadLastRecordsIfNeeded(allowedBounds);
+            });
+            this.foundsetChange.emit(this.foundset);
+            return newLoadPromise;
+        } else {
+            this.foundset.requestSelectionUpdate([allowedBounds.startIdx + allowedBounds.size - 1]).then(() => {
+                this.scrollToSelection();
+            });
+        }
+    }
+
+    // this function also adjusts current page if needed (if it's after the foundset size for example)
+    calculateAllowedLoadedDataBounds() {
+        let allowedStart: number;
+        let allowedSize: number;
+
+        let fs = this.foundset;
+        let serverSize = fs.serverSize;
+        if (this.showPagination()) {
+            // paging mode only keeps data for the showing page - at maximum
+            allowedStart = this.pageSize * (this.currentPage - 1);
+            if (!this.foundset.hasMoreRows && allowedStart >= serverSize) {
+                // this page no longer exists; it is after serverSize; adjust current page and that watch on that will request the correct viewport
+                this.setCurrentPage(this.getPageForIndex(serverSize - 1));
+                allowedStart = this.pageSize * (this.currentPage - 1);
+            }
+
+            let newAllowedSize: number;
+            if (allowedStart >= serverSize && this.foundset.hasMoreRows) {
+                newAllowedSize = this.pageSize;
+            }
+            else {
+                newAllowedSize = serverSize - allowedStart;
+            }
+
+            allowedSize = Math.min(this.pageSize, newAllowedSize);
+        } else {
+            // table is not going to show/use pages; so we can think of it as one big page
+            this.setCurrentPage(1); // just to be sure - we are not paging so we are on first "page"
+
+            allowedStart = 0;
+            allowedSize = serverSize;
+        }
+        return { startIdx: allowedStart, size: allowedSize };
+    }
+        
+    setCurrentPage(pageNumber:number) {
+        this.currentPage = pageNumber;
+        this.viewPort.setRenderedRange({start: this.pageSize * (this.currentPage - 1), end: this.pageSize * this.currentPage - 1});
+    }
+
+    getPageForIndex(idx:number) {
+        return Math.floor(Math.max(idx, 0) / this.pageSize) + 1;
+    }
+
+    // this is actually the preferred viewport size that the server will send automatically when foundset data completely changes
+	// it should be maximum pageSize if that is > 0 or (when we implement it) -1 (so auto paging)
+	getInitialPreferredLoadedSize() {
+		let potentialInitialViewportSize = Math.floor(52 * 2.5); //TODo performance settings batchSizeForLoadingMoreRows
+		return (this.pageSize > 0 && this.pageSize < potentialInitialViewportSize) ? this.pageSize : potentialInitialViewportSize;
+	}
 }
