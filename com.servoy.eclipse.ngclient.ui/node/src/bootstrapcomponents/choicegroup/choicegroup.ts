bf3d95d39513b939084741bfdd5f4db876e3235d
fbandrei <afara@servoy.com>
2020-08-03 09:40:45 +0300

    SVY-15110 [ng2] implement bootstrap components package
    choicegroup comp: added 'showAs' property and replaced 'ngIf' with
    'hiddden'

--- com.servoy.eclipse.ngclient.ui/node/src/bootstrapcomponents/choicegroup/choicegroup.ts
+++ com.servoy.eclipse.ngclient.ui/node/src/bootstrapcomponents/choicegroup/choicegroup.ts
@@ -12,6 +12,7 @@ export class ServoyBootstrapChoicegroup extends ServoyBootstrapBasefield impleme
   @Input() inputType;
   @Input() findmode;
   @Input() valuelistID : IValuelist;
+  @Input() showAs;
   selection: any[] = [];
   allowNullinc = 0;
 
