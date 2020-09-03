d7859500b83956534c6b0e1f988dd7f0c93da7f0
andreif17 <fbandrei538@gmail.com>
2020-09-03 15:37:33 +0300

    SVY-15252 [ng2] implement bs calendar/inlinecalendar API
    implemented api methods

--- com.servoy.eclipse.ngclient.ui/node/src/bootstrapcomponents/calendar/calendar.ts
+++ com.servoy.eclipse.ngclient.ui/node/src/bootstrapcomponents/calendar/calendar.ts
@@ -1,6 +1,7 @@
-import { Component, Renderer2, ElementRef, ViewChild, Input  } from '@angular/core';
+import { Component, Renderer2, ElementRef, ViewChild, Input} from '@angular/core';
 import { ServoyBootstrapBasefield } from '../bts_basefield';
-import { DateTimeAdapter, OwlDateTimeIntl } from '@danielmoncada/angular-datetime-picker';
+import { OwlDateTimeIntl } from '@danielmoncada/angular-datetime-picker';
+import * as moment from 'moment';
 
 @Component({
   selector: 'servoybootstrap-calendar',
@@ -12,6 +13,10 @@ export class ServoyBootstrapCalendar extends ServoyBootstrapBasefield {
 
     @ViewChild( 'inputElement' , {static: true} ) inputElementRef: ElementRef;
     @Input() format;
+
+    public filter: any;
+    min: Date;
+    max: Date;
     
     constructor(renderer: Renderer2) { 
         super(renderer);
@@ -25,4 +30,26 @@ export class ServoyBootstrapCalendar extends ServoyBootstrapBasefield {
         return this.inputElementRef.nativeElement;
     }
 
+    public disableDays(dateArray : Number[]) {
+        this.filter = (d: moment.Moment): boolean => {
+            return !dateArray.includes(d.day());
+        }
+    }
+ 
+    public disableDates(dateArray: Date[]) {
+        this.filter = (d: moment.Moment): boolean => {
+          let result = true;
+          dateArray.forEach(el => {
+            if (el.toString() === d.toDate().toString()) { 
+                result = false;
+            }
+          });
+          return result;
+        }      
+    }
+
+    public setMinMaxDate(minDate: Date, maxDate: Date) {
+        if (minDate) this.min = minDate;
+        if (maxDate) this.max = maxDate;
+    }
 }
--- com.servoy.eclipse.ngclient.ui/node/src/bootstrapcomponents/calendarinline/calendarinline.ts
+++ com.servoy.eclipse.ngclient.ui/node/src/bootstrapcomponents/calendarinline/calendarinline.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Component, Renderer2} from '@angular/core';
 import { ServoyBootstrapBasefield } from '../bts_basefield';
 
 @Component({
@@ -8,8 +8,35 @@ import { ServoyBootstrapBasefield } from '../bts_basefield';
 })
 export class ServoyBootstrapCalendarinline extends ServoyBootstrapBasefield {
 
+    public filter: any;
+    min: Date;
+    max: Date;
+
     constructor(renderer: Renderer2) { 
         super(renderer);
+    }
+
+    public disableDays(dateArray : Number[]) {
+      this.filter = (d: moment.Moment): boolean => {
+          return dateArray.includes(d.day());
+      }
+    }
+
+    public disableDates(dateArray: Date[]) {
+      this.filter = (d: moment.Moment): boolean => {
+        let result = true;
+        dateArray.forEach(el => {
+          if (el.toString() === d.toDate().toString()) { 
+              result = false;
+          }
+        });
+        return result;
+      }      
+    }
+
+    public setMinMaxDate(minDate: Date, maxDate: Date) {
+        if (minDate) this.min = minDate;
+        if (maxDate) this.max = maxDate;
     }
 
 }
