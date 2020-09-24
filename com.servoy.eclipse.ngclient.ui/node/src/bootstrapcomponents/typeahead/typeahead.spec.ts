95d75b15ac4043e390d7bfc794e7e06fb721afb5
lvostinar <lvostinar@servoy.com>
2020-09-24 13:00:15 +0300

    SVY-15219 [ng2] implement the typeahead bootstrap component

--- com.servoy.eclipse.ngclient.ui/node/src/bootstrapcomponents/typeahead/typeahead.spec.ts
+++ com.servoy.eclipse.ngclient.ui/node/src/bootstrapcomponents/typeahead/typeahead.spec.ts
@@ -2,13 +2,20 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ServoyBootstrapTypeahead } from './typeahead';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormattingService, TooltipService } from "../../ngclient/servoy_public";
+import { ServoyPublicModule } from '../../ngclient/servoy_public.module'
+import { SabloModule } from '../../sablo/sablo.module';
+
 describe('TypeaheadComponent', () => {
   let component: ServoyBootstrapTypeahead;
   let fixture: ComponentFixture<ServoyBootstrapTypeahead>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ServoyBootstrapTypeahead ]
+      declarations: [ ServoyBootstrapTypeahead ],
+      providers: [ FormattingService, TooltipService],
+      imports: [ServoyPublicModule,SabloModule, NgbModule]
     })
     .compileComponents();
   }));
