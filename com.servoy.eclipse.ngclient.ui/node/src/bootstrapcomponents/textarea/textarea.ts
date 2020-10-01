1147aa0b491c4f6bbc2097a08915df375b0b84fe
fbandrei <afara@servoy.com>
2020-10-01 17:05:31 +0300

    [ng2] Set maxlength of textarea component
    added maxLength property to text area comp (bts)

--- com.servoy.eclipse.ngclient.ui/node/src/bootstrapcomponents/textarea/textarea.ts
+++ com.servoy.eclipse.ngclient.ui/node/src/bootstrapcomponents/textarea/textarea.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Component, Input, OnInit, Renderer2 } from '@angular/core';
 import { ServoyBootstrapBasefield } from '../bts_basefield';
 
 @Component({
@@ -6,7 +6,16 @@ import { ServoyBootstrapBasefield } from '../bts_basefield';
   templateUrl: './textarea.html',
   styleUrls: ['./textarea.scss']
 })
-export class ServoyBootstrapTextarea extends ServoyBootstrapBasefield {
+export class ServoyBootstrapTextarea extends ServoyBootstrapBasefield implements OnInit {
+
+  @Input() maxLength: number;
+
+  ngOnInit() {
+    super.ngOnInit();
+    if (!this.maxLength || this.maxLength == 0) {
+      this.maxLength = 524288;
+    }
+  }
 
   constructor(renderer: Renderer2) {
     super(renderer);
