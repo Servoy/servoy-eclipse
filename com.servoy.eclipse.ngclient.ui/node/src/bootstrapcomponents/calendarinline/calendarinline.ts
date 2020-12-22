30d89486f3df975e47874e1ff76eb6441fe375b2
Johan Compagner <jcompagner@servoy.com>
2020-12-22 16:31:02 +0100

    SVY-15556 [NG2] make a test solution that test all components of the default and bootstrap handlers
    fixed datachange (use (ngModelChange) to get the event to push the value

--- com.servoy.eclipse.ngclient.ui/node/src/bootstrapcomponents/calendarinline/calendarinline.ts
+++ com.servoy.eclipse.ngclient.ui/node/src/bootstrapcomponents/calendarinline/calendarinline.ts
@@ -1,10 +1,11 @@
-import { Component, Renderer2, ChangeDetectorRef} from '@angular/core';
+import { Component, Renderer2, ChangeDetectorRef } from '@angular/core';
+import { Moment } from 'moment';
 import { ServoyBootstrapBasefield } from '../bts_basefield';
 
 @Component({
-  selector: 'bootstrapcomponents-calendarinline',
-  templateUrl: './calendarinline.html',
-  styleUrls: ['./calendarinline.scss']
+    selector: 'bootstrapcomponents-calendarinline',
+    templateUrl: './calendarinline.html',
+    styleUrls: ['./calendarinline.scss']
 })
 export class ServoyBootstrapCalendarinline extends ServoyBootstrapBasefield {
 
@@ -12,24 +13,31 @@ export class ServoyBootstrapCalendarinline extends ServoyBootstrapBasefield {
     min: Date;
     max: Date;
 
-    constructor(renderer: Renderer2,protected cdRef: ChangeDetectorRef) {
+    constructor(renderer: Renderer2, protected cdRef: ChangeDetectorRef) {
         super(renderer, cdRef);
     }
 
-    public disableDays(dateArray: Number[]) {
-      this.filter = (d: moment.Moment): boolean => dateArray.includes(d.day());
+    public dateChanged(event: Moment) {
+        if (event) {
+            this.dataProviderID = event.toDate();
+        } else this.dataProviderID = null;
+        super.pushUpdate();
+    }
+
+    public disableDays(dateArray: number[]) {
+        this.filter = (d: Moment): boolean => dateArray.includes(d.day());
     }
 
     public disableDates(dateArray: Date[]) {
-      this.filter = (d: moment.Moment): boolean => {
-        let result = true;
-        dateArray.forEach(el => {
-          if (el.toString() === d.toDate().toString()) {
-              result = false;
-          }
-        });
-        return result;
-      };
+        this.filter = (d: Moment): boolean => {
+            let result = true;
+            dateArray.forEach(el => {
+                if (el.toString() === d.toDate().toString()) {
+                    result = false;
+                }
+            });
+            return result;
+        };
     }
 
     public setMinMaxDate(minDate: Date, maxDate: Date) {
