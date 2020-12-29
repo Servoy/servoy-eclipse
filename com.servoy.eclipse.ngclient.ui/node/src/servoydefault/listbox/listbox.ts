c680bdb739729355f3b958b5ea1665dc00e0c635
Diana Bunaciu <dtimut@servoy.com>
2020-12-29 17:22:55 +0200

    SVY-15557 [NG2] make a test solution that test all components of the default and bootstrap api calls add getSelectedElements to listbox

--- com.servoy.eclipse.ngclient.ui/node/src/servoydefault/listbox/listbox.ts
+++ com.servoy.eclipse.ngclient.ui/node/src/servoydefault/listbox/listbox.ts
@@ -13,6 +13,9 @@ export class ServoyDefaultListBox extends ServoyDefaultBaseField<HTMLSelectEleme
 
     selectedValues: any[];
 
+    selection: any[] = [];
+    allowNullinc = 0;
+
     constructor( changeDetectorRef: ChangeDetectorRef, renderer: Renderer2, formattingService: FormattingService ) {
         super( renderer, changeDetectorRef, formattingService );
     }
@@ -24,14 +27,30 @@ export class ServoyDefaultListBox extends ServoyDefaultBaseField<HTMLSelectEleme
                 this.selectedValues = [];
                 if (this.multiselectListbox && this.dataProviderID) {
                     this.selectedValues = ('' + this.dataProviderID).split( '\n' );
+                } else if (!this.multiselectListbox && this.dataProviderID) {
+                    this.selectedValues = [this.dataProviderID];
                 }
+                this.setSelectionFromDataprovider();
                 break;
 
             }
         }
         super.svyOnChanges( changes );
     }
+    
+    svyOnInit() {
+        super.svyOnInit();
+        this.onValuelistChange();
+    }
+    
+    onValuelistChange() {
+        if (this.valuelistID)
+            if (this.valuelistID.length > 0 && this.isValueListNull(this.valuelistID[0])) this.allowNullinc = 1;
+    }
+    
+    isValueListNull = (item) => (item.realValue == null || item.realValue === '') && item.displayValue === '';
 
+    
     attachHandlers() {
         if (this.onActionMethodID) this.renderer.listen( this.getNativeElement(), 'click', e => this.onActionMethodID( e ));
         super.attachHandlers();
@@ -44,4 +63,25 @@ export class ServoyDefaultListBox extends ServoyDefaultBaseField<HTMLSelectEleme
         this.dataProviderID = this.selectedValues.join( '\n' );
         this.pushUpdate();
     }
+    
+    setSelectionFromDataprovider() {
+        this.selection = [];
+        if (this.dataProviderID === null || this.dataProviderID === undefined) return;
+        const arr = (typeof this.dataProviderID === 'string') ? this.dataProviderID.split('\n') : [this.dataProviderID];
+        arr.forEach((element, index, array) => {
+            for (let i = 0; i < this.valuelistID.length; i++) {
+                const item = this.valuelistID[i];
+                if (item.realValue + '' === element + '' && !this.isValueListNull(item)) this.selection[i - this.allowNullinc] = true;
+            }
+        });
+    }
+    
+    getSelectedElements() {
+        return this.selection
+            .map((item, index) => {
+                if (item == true) return this.valuelistID[index + this.allowNullinc].realValue;
+            })
+            .filter(item => item !== null);
+    }
+
 }
