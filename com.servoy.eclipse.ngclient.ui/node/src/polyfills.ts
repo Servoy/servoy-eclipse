101c074616821ed361dd7205c284150596101cbd
Johan Compagner <jcompagner@servoy.com>
2021-01-08 17:25:59 +0100

    SVY-15707 [NG2] implement LFC.onSelectionChanged
    pollyfill custom event for IE

--- com.servoy.eclipse.ngclient.ui/node/src/polyfills.ts
+++ com.servoy.eclipse.ngclient.ui/node/src/polyfills.ts
@@ -68,3 +68,6 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+ import 'custom-event-polyfill';
+
