7d0d7d8c4b3a0b085b1a16a1bdddf85ce7f5939e
lvostinar <lvostinar@servoy.com>
2021-01-20 15:15:01 +0200

    SVY-15747 [NG2] window plugin: implement shortcuts

--- com.servoy.eclipse.ngclient.ui/node/src/window_service/window.service.spec.ts
+++ com.servoy.eclipse.ngclient.ui/node/src/window_service/window.service.spec.ts
@@ -1,12 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 
 import { WindowService } from './window.service';
+import { ShortcutService } from './shortcut.service';
+import { SvyUtilsService, LocaleService } from '../ngclient/servoy_public';
+import { SabloModule } from '../sablo/sablo.module';
+import { ServoyPublicModule } from '../ngclient/servoy_public.module';
+import { FormService } from '../ngclient/form.service';
+import { ServoyService } from '../ngclient/servoy.service';
+import { ViewportService } from '../ngclient/services/viewport.service';
 
 describe('WindowService', () => {
   let service: WindowService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+        imports: [SabloModule, ServoyPublicModule],
+        providers:[WindowService, ShortcutService, SvyUtilsService,ViewportService, FormService, ServoyService, { provide: LocaleService, useValue: {getLocale: () => 'en' }}]
+    });
     service = TestBed.inject(WindowService);
   });
 
