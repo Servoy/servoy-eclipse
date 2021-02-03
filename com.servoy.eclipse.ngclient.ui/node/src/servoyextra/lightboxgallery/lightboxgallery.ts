4ab01df1007d80eec2a5d4e25414354273ab0f0c
Diana Bunaciu <dtimut@servoy.com>
2021-02-03 18:37:01 +0200

    SVY-15563 [NG2] [servoy-extra] create lightboxgallery component 2nd draft

--- com.servoy.eclipse.ngclient.ui/node/src/servoyextra/lightboxgallery/lightboxgallery.ts
+++ com.servoy.eclipse.ngclient.ui/node/src/servoyextra/lightboxgallery/lightboxgallery.ts
@@ -1,6 +1,7 @@
-import { Component, Output, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Output, ChangeDetectorRef, SimpleChanges, Renderer2, Input, ChangeDetectionStrategy } from '@angular/core';
 import { ServoyBaseComponent, SvyUtilsService } from '../../ngclient/servoy_public';
-import { LightboxModule } from 'ngx-lightbox';
+import { IFoundset } from '../../sablo/spectypes.service';
+import { LightboxModule, Lightbox } from 'ngx-lightbox';
 
 @Component( {
     selector: 'servoyextra-lightboxgallery',
@@ -10,5 +11,78 @@ import { LightboxModule } from 'ngx-lightbox';
 } )
 export class ServoyExtraLightboxGallery extends ServoyBaseComponent<HTMLDivElement> {
 
+    @Input() onHoverButtonClicked: ( e: Event, imageId: string ) => void;
+
+    @Input() imagesFoundset: IFoundset;
+    @Input() maxImageWidth: number;
+    @Input() maxImageHeight: number;
+    @Input() albumLabel: string;
+    @Input() fadeDuration: number;
+    @Input() fitImagesInViewport: boolean;
+    @Input() imageFadeDuration: number;
+    @Input() positionFromTop: number;
+    @Input() resizeDuration: number;
+    @Input() wrapAround: boolean;
+    @Input() galleryVisible: boolean;
+    @Input() showCaptionInGallery: boolean;
+    @Input() showImageNumberLabel: boolean;
+    @Input() hoverButtonIcon: string;
+    @Input() buttonText: string;
+    @Input() buttonStyleClass: string;
+    @Input() enabled: boolean;
+
+    public images: Array<any> = [];
+
+    constructor( renderer: Renderer2, cdRef: ChangeDetectorRef, private _lightbox: Lightbox ) {
+        super( renderer, cdRef );
+    }
+
+
+    svyOnInit() {
+        super.svyOnInit();
+        for ( let i = 0; i <= this.imagesFoundset.viewPort.rows.length; i++ ) {
+            const row = this.imagesFoundset.viewPort.rows[i];
+            const image = {
+                src: row.image && row.image.url ? row.image.url : null,
+                caption: row.caption ? row.caption : null,
+                thumb: row.thumbnail && row.thumbnail.url ? row.thumbnail.url : null
+            };
+
+            //check if using url strings instead of media/blob
+            image.src = typeof row.image == 'string' ? row.image : image.src;
+            image.thumb = typeof row.thumbnail == 'string' ? row.thumbnail : image.thumb;
+
+            if ( !image.src ) continue;
+            this.images.push( image );
+        }
+
+    }
+
+    svyOnChanges( changes: SimpleChanges ) {
+        if ( changes ) {
+            for ( const property of Object.keys( changes ) ) {
+                const change = changes[property];
+                switch ( property ) {
+                    case 'styleClass':
+                        if ( change.previousValue )
+                            this.renderer.removeClass( this.getNativeElement(), change.previousValue );
+                        if ( change.currentValue )
+                            this.renderer.addClass( this.getNativeElement(), change.currentValue );
+                        break;
+                }
+            }
+        }
+        super.svyOnChanges( changes );
+    }
+
+    open( index: number ): void {
+        // open lightbox
+        this._lightbox.open( this.images, index );
+    }
+
+    close(): void {
+        // close lightbox programmatically
+        this._lightbox.close();
+    }
 }
 
