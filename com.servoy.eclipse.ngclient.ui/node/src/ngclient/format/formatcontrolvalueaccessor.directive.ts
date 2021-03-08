68dc09f5cb42bd9bfceea3762b952c15cd949339
lvostinar <lvostinar@servoy.com>
2021-03-08 15:09:55 +0200

    SVY-15878 ng2 (default) textfield is cleared after foundset.search

--- com.servoy.eclipse.ngclient.ui/node/src/ngclient/format/formatcontrolvalueaccessor.directive.ts
+++ com.servoy.eclipse.ngclient.ui/node/src/ngclient/format/formatcontrolvalueaccessor.directive.ts
@@ -1,5 +1,5 @@
 
-import {Directive, Renderer2, ElementRef, Input, HostListener, forwardRef, AfterViewInit, OnChanges, Inject } from '@angular/core';
+import { Directive, Renderer2, ElementRef, Input, HostListener, forwardRef, AfterViewInit, OnChanges, Inject } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { MaskFormat } from './maskformat';
 import { Format, FormattingService } from './formatting.service';
@@ -23,15 +23,15 @@ export class FormatDirective implements ControlValueAccessor, AfterViewInit, OnC
     @Input() findmode: boolean;
 
     private hasFocus = false;
-	private realValue = null;
+    private realValue = null;
 
-	private isKeyPressEventFired = false;
-	private oldInputValue = null;
+    private isKeyPressEventFired = false;
+    private oldInputValue = null;
     private listeners = [];
     private readonly log: LoggerService;
 
     constructor(private _renderer: Renderer2, private _elementRef: ElementRef, private formatService: FormattingService,
-                        @Inject(DOCUMENT) private doc: Document, logFactory: LoggerFactory ) {
+        @Inject(DOCUMENT) private doc: Document, logFactory: LoggerFactory) {
         this.log = logFactory.getLogger('formatdirective');
     }
 
@@ -55,15 +55,15 @@ export class FormatDirective implements ControlValueAccessor, AfterViewInit, OnC
         if (!this.findmode && this.format) {
             const type = this.format.type;
             let format = this.format.display ? this.format.display : this.format.edit;
-            if (this.hasFocus&& this.format.edit && !this.format.isMask) format = this.format.edit;
+            if (this.hasFocus && this.format.edit && !this.format.isMask) format = this.format.edit;
             try {
                 data = this.formatService.unformat(data, format, type, this.realValue);
             } catch (e) {
                 this.log.error(e);
-                    //TODO set error state
+                //TODO set error state
             }
             if (this.format.type === 'TEXT' && this.format.isRaw && this.format.isMask) {
-                if (data && format && data.length === format.length){
+                if (data && format && data.length === format.length) {
                     let ret = '';
                     for (let i = 0; i < format.length; i++) {
                         switch (format[i]) {
@@ -81,26 +81,26 @@ export class FormatDirective implements ControlValueAccessor, AfterViewInit, OnC
                                 break;
                         }
                     }
-                   data = ret;
+                    data = ret;
                 }
             }
         }
-
+        this.realValue = data;
         this.onChangeCallback(data);
     }
 
-    onChangeCallback = (_: any) => {};
-    onTouchedCallback = () => {};
+    onChangeCallback = (_: any) => { };
+    onTouchedCallback = () => { };
 
     ngAfterViewInit(): void {
-		this.setFormat();
+        this.setFormat();
     }
 
     ngOnChanges(): void {
         this.setFormat();
     }
 
-	registerOnChange(fn: any) {
+    registerOnChange(fn: any) {
         this.onChangeCallback = fn;
     }
 
@@ -111,27 +111,27 @@ export class FormatDirective implements ControlValueAccessor, AfterViewInit, OnC
 
     writeValue(value: any): void {
         this.realValue = value;
-         if (value && this.format) {
-             let data = value;
-             if (!this.findmode) {
-                data = this.type === 'number' && data.toString().length >= this.format.maxLength ? data.toString().substring(0, this.format.maxLength):data;
-                 let useEdit = !this.format.display;
-                 if (this.format.edit && !this.format.isMask && this.hasFocus) useEdit = true;
-                 try {
-                     data = this.formatService.format(data, this.format, useEdit);
-                 } catch (e) {
-                     this.log.error(e);
-                 }
-                 if (data && this.format.type === 'TEXT') {
-                     if (this.format.uppercase) data = data.toUpperCase();
-                     else if (this.format.lowercase) data = data.toLowerCase();
-                 }
-             }
-             this._renderer.setProperty(this._elementRef.nativeElement, 'value', data);
-         } else {
-             this._renderer.setProperty(this._elementRef.nativeElement, 'value', value?value:'');
-         }
-	 }
+        if (value && this.format) {
+            let data = value;
+            if (!this.findmode) {
+                data = this.type === 'number' && data.toString().length >= this.format.maxLength ? data.toString().substring(0, this.format.maxLength) : data;
+                let useEdit = !this.format.display;
+                if (this.format.edit && !this.format.isMask && this.hasFocus) useEdit = true;
+                try {
+                    data = this.formatService.format(data, this.format, useEdit);
+                } catch (e) {
+                    this.log.error(e);
+                }
+                if (data && this.format.type === 'TEXT') {
+                    if (this.format.uppercase) data = data.toUpperCase();
+                    else if (this.format.lowercase) data = data.toLowerCase();
+                }
+            }
+            this._renderer.setProperty(this._elementRef.nativeElement, 'value', data);
+        } else {
+            this._renderer.setProperty(this._elementRef.nativeElement, 'value', value ? value : '');
+        }
+    }
 
 
     private setFormat() {
@@ -162,85 +162,85 @@ export class FormatDirective implements ControlValueAccessor, AfterViewInit, OnC
         }
     }
 
-	 // lower and upper case handling
+    // lower and upper case handling
 
-	 private upperOrLowerCase() {
+    private upperOrLowerCase() {
         const element = this._elementRef.nativeElement;
         const caretPos = element.selectionStart;
-        element.value = this.format.uppercase?element.value.toUpperCase():element.value.toLowerCase();
+        element.value = this.format.uppercase ? element.value.toUpperCase() : element.value.toLowerCase();
         element.setSelectionRange(caretPos, caretPos);
-	}
+    }
 
-	private inputFiredForNumbersCheck(event: Event) {
-		let currentValue = this._elementRef.nativeElement.value;
+    private inputFiredForNumbersCheck(event: Event) {
+        let currentValue = this._elementRef.nativeElement.value;
 
-		if(!this.isKeyPressEventFired && (event.target as HTMLElement).tagName.toUpperCase() === 'INPUT') {
-			// get inserted chars
-			const inserted = this.findDelta(currentValue, this.oldInputValue);
-			// get removed chars
-			const removed = this.findDelta(this.oldInputValue, currentValue);
-			// determine if user pasted content
-			const pasted = inserted.length > 1 || (!inserted && !removed);
+        if (!this.isKeyPressEventFired && (event.target as HTMLElement).tagName.toUpperCase() === 'INPUT') {
+            // get inserted chars
+            const inserted = this.findDelta(currentValue, this.oldInputValue);
+            // get removed chars
+            const removed = this.findDelta(this.oldInputValue, currentValue);
+            // determine if user pasted content
+            const pasted = inserted.length > 1 || (!inserted && !removed);
 
-			if(!pasted && !removed) {
-				if(!this.formatService.testForNumbersOnly(event, inserted, this._elementRef.nativeElement, this.findmode, true, this.format, true)) {
-					currentValue = this.oldInputValue;
-				}
-			}
+            if (!pasted && !removed) {
+                if (!this.formatService.testForNumbersOnly(event, inserted, this._elementRef.nativeElement, this.findmode, true, this.format, true)) {
+                    currentValue = this.oldInputValue;
+                }
+            }
 
-			//If number validator, check all chars in string and extract only the valid chars.
-			if((event.target as HTMLInputElement).type.toUpperCase() === 'NUMBER' || this.format.type ==='NUMBER' || this.format.type === 'INTEGER' || this.format.isNumberValidator){
-				currentValue = this.getNumbersFromString(event,currentValue, this.oldInputValue);
-			}
+            //If number validator, check all chars in string and extract only the valid chars.
+            if ((event.target as HTMLInputElement).type.toUpperCase() === 'NUMBER' || this.format.type === 'NUMBER' || this.format.type === 'INTEGER' || this.format.isNumberValidator) {
+                currentValue = this.getNumbersFromString(event, currentValue, this.oldInputValue);
+            }
 
-			if (currentValue !== this._elementRef.nativeElement.value) {
-				this._elementRef.nativeElement.value = currentValue;
+            if (currentValue !== this._elementRef.nativeElement.value) {
+                this._elementRef.nativeElement.value = currentValue;
 
-				// // detect IE8 and above, and Edge; call on change manually because of https://bugs.jquery.com/ticket/10818
-				// if (/MSIE/.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent) || /Edge/.test(navigator.userAgent)) {
-				// 	var changeOnBlurForIE = function() {
-				// 		 element.change();
-				// 		 element.off("blur", callChangeOnBlur);
-				// 	 }
-				// 	 element.on("blur", changeOnBlurForIE);
-				// }
-			}
+                // // detect IE8 and above, and Edge; call on change manually because of https://bugs.jquery.com/ticket/10818
+                // if (/MSIE/.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent) || /Edge/.test(navigator.userAgent)) {
+                // 	var changeOnBlurForIE = function() {
+                // 		 element.change();
+                // 		 element.off("blur", callChangeOnBlur);
+                // 	 }
+                // 	 element.on("blur", changeOnBlurForIE);
+                // }
+            }
 
-			this.oldInputValue = currentValue;
-			this.isKeyPressEventFired = false;
-		}
+            this.oldInputValue = currentValue;
+            this.isKeyPressEventFired = false;
+        }
 
-		if(this.isKeyPressEventFired){
-			this.oldInputValue = currentValue;
-			this.isKeyPressEventFired = false;
-		}
-	}
+        if (this.isKeyPressEventFired) {
+            this.oldInputValue = currentValue;
+            this.isKeyPressEventFired = false;
+        }
+    }
 
-	private findDelta(value: string, prevValue: string): string {
-		let delta = '';
-		if(typeof value === 'string' && typeof prevValue === 'string' && value.length >= prevValue.length) {
-			for (let i = 0; i < value.length; i++) {
-				const str = value.substr(0, i) + value.substr(i + value.length - prevValue.length);
-				if (str === prevValue) {
-					delta = value.substr(i, value.length - prevValue.length);
-					break;
-				}
-			}
-		}
-		return delta;
-	}
+    private findDelta(value: string, prevValue: string): string {
+        let delta = '';
+        if (typeof value === 'string' && typeof prevValue === 'string' && value.length >= prevValue.length) {
+            for (let i = 0; i < value.length; i++) {
+                const str = value.substr(0, i) + value.substr(i + value.length - prevValue.length);
+                if (str === prevValue) {
+                    delta = value.substr(i, value.length - prevValue.length);
+                    break;
+                }
+            }
+        }
+        return delta;
+    }
 
-	private getNumbersFromString(e: Event, currentValue: string, oldInputValue: string){
-		if(oldInputValue === currentValue){
-			return currentValue;
-		}
-		let stripped = '';
-		for (let i = 0; i < currentValue.length; i++) {
-			if(this.formatService.testForNumbersOnly(e, currentValue.charAt(i), this._elementRef.nativeElement, this.findmode, true, this.format, true)){
-				stripped = stripped + currentValue.charAt(i);
-				if(stripped.length === this.format.maxLength) break;
-			}
-		}
-		return stripped;
-	}
+    private getNumbersFromString(e: Event, currentValue: string, oldInputValue: string) {
+        if (oldInputValue === currentValue) {
+            return currentValue;
+        }
+        let stripped = '';
+        for (let i = 0; i < currentValue.length; i++) {
+            if (this.formatService.testForNumbersOnly(e, currentValue.charAt(i), this._elementRef.nativeElement, this.findmode, true, this.format, true)) {
+                stripped = stripped + currentValue.charAt(i);
+                if (stripped.length === this.format.maxLength) break;
+            }
+        }
+        return stripped;
+    }
 }
