f86aeeb08143b82913ce8accbdbcd47f253d0e06
fbandrei <afara@servoy.com>
2021-03-09 12:54:01 +0100

    SVY-15559 [NG2] [servoy-extra] create dbtreeview component
    added server side script option between ng1 & ng2

--- com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/views/solutionexplorer/SolutionExplorerListContentProvider.java
+++ com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/views/solutionexplorer/SolutionExplorerListContentProvider.java
@@ -1787,7 +1787,7 @@ public class SolutionExplorerListContentProvider implements IStructuredContentPr
 		if (spec.getApiFunctions().size() > 0)
 		{
 			extractDocsFromJsFile(spec, spec.getDefinitionURL());
-			extractDocsFromJsFile(spec, spec.getServerScript());
+			extractDocsFromJsFile(spec, spec.getServerScript(false));
 		}
 	}
 
