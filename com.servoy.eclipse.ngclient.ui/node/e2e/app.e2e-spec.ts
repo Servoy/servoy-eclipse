14f03437e52ca5d99d81e583770f0a379c7275b2
Johan Compagner <jcompagner@servoy.com>
2021-03-15 10:43:08 +0100

    SVY-15741 [NG2] create a first E2E test that runs and test NG2
    first test to see how an angular e2e works
    change a simple test to work on one of our solutions
    added varoius testing scripts to start webdriver and start a e2e test
    (based on edge)

--- com.servoy.eclipse.ngclient.ui/node/e2e/app.e2e-spec.ts
+++ com.servoy.eclipse.ngclient.ui/node/e2e/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('ngclient2 App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display the username label', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Username');
   });
 });
