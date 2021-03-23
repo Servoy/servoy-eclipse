81c3ed0ce709c1b3a58d71759cf5372882b982d5
vidmarian <mvid@servoy.com>
2021-03-23 11:42:10 +0200

    SVY-15925 NG2 dropdown does not allow you to type a value

--- com.servoy.eclipse.ngclient.ui/node/src/bootstrapcomponents/combobox/combobox.ts
+++ com.servoy.eclipse.ngclient.ui/node/src/bootstrapcomponents/combobox/combobox.ts
@@ -2,7 +2,7 @@ import { Component, Renderer2, Input, SimpleChanges, ChangeDetectorRef, ViewChil
 import { ServoyBootstrapBasefield } from '../bts_basefield';
 import { IValuelist } from '../../sablo/spectypes.service';
 import { Format, FormattingService } from '../../ngclient/servoy_public';
-import { NgbDropdownItem } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDropdownItem, NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
 import { DOCUMENT } from '@angular/common';
 
 @Component({
@@ -20,10 +20,15 @@ export class ServoyBootstrapCombobox extends ServoyBootstrapBasefield<HTMLDivEle
 
     @ViewChildren(NgbDropdownItem) menuItems: QueryList<NgbDropdownItem>;
     @ViewChild('input') input: ElementRef<HTMLButtonElement>;
+    @ViewChild('tooltip') tooltip: NgbTooltip;
 
     formattedValue: any;
-    valueComparator: (value:{ displayValue: any; realValue: any }) => boolean;
+    valueComparator: (value: { displayValue: any; realValue: any }) => boolean;
     openState = false;
+    keyboardSelectValue: string = null;
+    lastSelectValue: string = null;
+    firstItemFound = false;
+    private kbSelection = null;
     private skipFocus = false;
 
     constructor(renderer: Renderer2, protected cdRef: ChangeDetectorRef, private formatService: FormattingService, @Inject(DOCUMENT) doc: Document) {
@@ -36,6 +41,53 @@ export class ServoyBootstrapCombobox extends ServoyBootstrapBasefield<HTMLDivEle
             // stop propagation when using list form component (to not break the selection)
             event.stopPropagation();
         }
+        this.lastSelectValue = null;
+        this.firstItemFound = false;
+        if (this.isPrintableChar(event.key)) {
+            clearTimeout(this.kbSelection);
+            this.keyboardSelectValue = (this.keyboardSelectValue ? this.keyboardSelectValue : '') + event.key;
+            this.lastSelectValue = this.keyboardSelectValue.slice();
+            this.refreshTooltip();
+            this.cdRef.detectChanges();
+            this.kbSelection = setTimeout(() => {
+                this.scrollToFirstMatchingItem();
+                this.keyboardSelectValue = null;
+                this.lastSelectValue = null;
+                this.tooltip.close();
+            }, 300);
+        }
+    }
+
+    svyOnInit() {
+        super.svyOnInit();
+        this.tooltip.autoClose = false;
+    }
+
+    refreshTooltip() {
+        if (!this.tooltip.isOpen()) {
+            this.tooltip.open();
+        }
+    }
+
+    handleTooltip(event: KeyboardEvent) {
+        this.tooltip.autoClose = false;
+        this.tooltip.ngbTooltip = 'This is the CHANGED text';
+        if (this.tooltip.isOpen()) {
+            this.tooltip.close();
+        } else {
+            this.tooltip.open();
+        }
+    }
+
+    isPrintableChar(key: string): boolean {
+        const nonPrintableValue = [
+            'Alt', 'AltGraph', 'CapsLock', 'Fn', 'Meta', 'NumLock', 'ScrollLock', 'Shift',
+            'Enter', 'Tab', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowUp', 'End', 'Home',
+            'PageUp', 'PageDown', 'Backspace', 'Delete', 'Insert', 'Del', 'Escape'
+        ];
+        if (nonPrintableValue.includes(key))
+            return false;
+        return true;
     }
 
     getDropDownWidth() {
@@ -70,6 +122,7 @@ export class ServoyBootstrapCombobox extends ServoyBootstrapBasefield<HTMLDivEle
                 }
             });
         } else {
+            this.lastSelectValue = null;
             this.requestFocus(this.mustExecuteOnFocus);
         }
     }
@@ -93,6 +146,33 @@ export class ServoyBootstrapCombobox extends ServoyBootstrapBasefield<HTMLDivEle
     updateValue(realValue: any) {
         this.dataProviderID = realValue;
         this.dataProviderIDChange.emit(this.dataProviderID);
+    }
+
+    getStrongValue(value: any): any {
+        let retValue = '';
+        if (this.openState && this.lastSelectValue && value && value.startsWith(this.lastSelectValue)) {
+            retValue = this.lastSelectValue;
+        }
+        return retValue;
+    }
+
+    getRemainingValue(value: any): any {
+        let retValue = value;
+        if (this.openState && this.lastSelectValue && value && value.startsWith(this.lastSelectValue)) {
+            retValue = value.substring(this.lastSelectValue.length);
+        }
+        return retValue;
+    }
+
+    scrollToFirstMatchingItem() {
+        if (this.openState && this.lastSelectValue) {
+            for (const item of this.menuItems) {
+                if (item.elementRef.nativeElement.innerText.startsWith(this.lastSelectValue) && !this.firstItemFound) {
+                    this.firstItemFound = true;
+                    item.elementRef.nativeElement.focus();
+                }
+            }
+       }
     }
 
     // eslint-disable-next-line eqeqeq
