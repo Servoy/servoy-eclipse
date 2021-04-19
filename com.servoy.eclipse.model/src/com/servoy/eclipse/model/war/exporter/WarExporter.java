3f2aca4035d8270a9b077e1c4aa723a65141e556
Andrei Costescu <costescuandrei@gmail.com>
2021-04-19 12:12:30 +0300

    Do not include .git dir in .war export - in case of component/service package projects that are linked to git.

--- com.servoy.eclipse.model/src/com/servoy/eclipse/model/war/exporter/WarExporter.java
+++ com.servoy.eclipse.model/src/com/servoy/eclipse/model/war/exporter/WarExporter.java
@@ -147,6 +147,13 @@ public class WarExporter
 			".jar", "org.apache.commons.lang3_*.jar", "org.apache.commons.commons-text_*.jar", "de.inetsoftware.jlessc_*.jar", "com.github.ua-parser.uap-java_*.jar", "org.yaml.snakeyaml_*.jar" };
 
 	private static final String WRO4J_RUNNER = "wro4j-runner-1.8.0";
+	private static final Set<String> EXCLUDED_RESOURCES_BY_NAME;
+
+	static
+	{
+		EXCLUDED_RESOURCES_BY_NAME = new HashSet<>();
+		EXCLUDED_RESOURCES_BY_NAME.add(".git");
+	}
 
 	private final IWarExportModel exportModel;
 	private SpecProviderState componentsSpecProviderState;
@@ -678,7 +685,7 @@ public class WarExporter
 					{
 						if (resource.isDirectory())
 						{
-							copyDir(resource, new File(tmpWarDir, name), true, allTemplates);
+							copyDir(resource, new File(tmpWarDir, name), true, allTemplates, EXCLUDED_RESOURCES_BY_NAME);
 						}
 						else
 						{
@@ -1816,28 +1823,32 @@ public class WarExporter
 		path.delete();
 	}
 
-	private static Set<File> copyDir(File sourceDir, File destDir, boolean recusive, Map<String, File> allTemplates) throws ExportException
+	private static Set<File> copyDir(File sourceDir, File destDir, boolean recusive, Map<String, File> allTemplates, Set<String> excludedResourcesByName)
+		throws ExportException
 	{
 		Set<File> writtenFiles = new HashSet<File>();
-		copyDir(sourceDir, destDir, recusive, writtenFiles, allTemplates);
+		copyDir(sourceDir, destDir, recusive, writtenFiles, allTemplates, excludedResourcesByName);
 		return writtenFiles;
 	}
 
 	private static Set<File> copyDir(File sourceDir, File destDir, boolean recusive) throws ExportException
 	{
-		return copyDir(sourceDir, destDir, recusive, null);
+		return copyDir(sourceDir, destDir, recusive, null, null);
 	}
 
-	private static void copyDir(File sourceDir, File destDir, boolean recusive, Set<File> writtenFiles, Map<String, File> allTemplates) throws ExportException
+	private static void copyDir(File sourceDir, File destDir, boolean recusive, Set<File> writtenFiles, Map<String, File> allTemplates,
+		Set<String> excludedResourcesByName) throws ExportException
 	{
 		if (!destDir.exists() && !destDir.mkdirs()) throw new ExportException("Can't create destination dir: " + destDir);
 		File[] listFiles = sourceDir.listFiles();
 		if (listFiles == null) return;
 		for (File file : listFiles)
 		{
+			if (excludedResourcesByName != null && excludedResourcesByName.contains(file.getName())) continue; // skip it; for example it could be a .git dir that should not get inside .war contents as it is useless there and can have a large size
+
 			if (file.isDirectory())
 			{
-				if (recusive) copyDir(file, new File(destDir, file.getName()), recusive, writtenFiles, allTemplates);
+				if (recusive) copyDir(file, new File(destDir, file.getName()), recusive, writtenFiles, allTemplates, excludedResourcesByName);
 			}
 			else
 			{
