3c1f63f59830616fd41fdf9839f3abc20b23920d
emera <emera@servoy.com>
2021-06-09 14:57:03 +0300

    SVY-16173 Tiny Tutorial doesn't renders well <b>,<img> tags (img regex should support other kind of whitespace characters as well, otherwise it doesn't match all images; the not matched img tags are completely removed from the text, so the text becomes shorter and this causes the indexes to be out of sync)

--- com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/views/TutorialView.java
+++ com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/views/TutorialView.java
@@ -89,7 +89,7 @@ public class TutorialView extends ViewPart
 	private static final String URL_DEFAULT_TUTORIALS_LIST = REST_TUTORIALS_URL + "/listtutorials?servoyVersion=" + ClientVersion.getPureVersion();
 	private static final String SWT_CSS_ID_KEY = "org.eclipse.e4.ui.css.id";//did not import it to avoid adding dependencies for using one constant from CSSSWTConstants
 	private static final String SVY_BACKGROUND = "svybackground";
-	private static final Pattern imgPattern = Pattern.compile("<img src=(.+?) (alt=(.+?))?\\s*/>");
+	private static final Pattern imgPattern = Pattern.compile("<img\\s+src=(.+?)(\\s+alt=(.+?))?/>");
 
 	private JSONObject dataModel;
 	private JSONArray dataTutorialsList;
@@ -601,7 +601,7 @@ public class TutorialView extends ViewPart
 								{
 									//inline image
 									imgStartIndex.add(Integer.valueOf(i - numberOfHtmlChars));
-									numberOfHtmlChars += text.substring(i, text.indexOf("/>", i + 3)).length() - 1;
+									numberOfHtmlChars += text.substring(i, text.indexOf("/>", i + 3)).length() - 2;
 									int imgStart = text.indexOf("base64,", i + 3) + 7;
 									String imgBase64 = text.substring(imgStart, text.indexOf("\"", imgStart)).trim();
 									images.add(getImageFromEncodedString(imgBase64));
@@ -841,7 +841,7 @@ public class TutorialView extends ViewPart
 		{
 			if (images.get(count) == null)
 			{
-				copyText = copyText.replace(matcher.group(0), matcher.groupCount() == 2 ? matcher.group(2).replaceAll("\"", "") : "");
+				copyText = copyText.replace(matcher.group(0), matcher.groupCount() == 3 ? matcher.group(3).replaceAll("\"", "") : "");
 			}
 			else
 			{
