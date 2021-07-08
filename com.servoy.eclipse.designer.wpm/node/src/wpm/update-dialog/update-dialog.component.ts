800820f7b193243b8c24ac9f18f133951c91198f
fbandrei <afara@servoy.com>
2021-07-08 11:31:15 +0300

    SVY-16211 add an update all button in the Servoy Package Manager
    close the dialog properly

--- com.servoy.eclipse.designer.wpm/node/src/wpm/update-dialog/update-dialog.component.ts
+++ com.servoy.eclipse.designer.wpm/node/src/wpm/update-dialog/update-dialog.component.ts
@@ -1,5 +1,5 @@
 import {Component, Inject} from '@angular/core';
-import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { Package, WpmService } from '../wpm.service';
 
 @Component({
@@ -12,7 +12,8 @@ export class UpdatePackagesDialog {
     extendedData: ExtendedPackage[] = [];
     installingOrRemoving = false;
 
-    constructor(@Inject(MAT_DIALOG_DATA) public data: Package[], public wpmService: WpmService) {
+    constructor(public dialogRef: MatDialogRef<UpdatePackagesDialog>, 
+        @Inject(MAT_DIALOG_DATA) public data: Package[], public wpmService: WpmService) {
         data.forEach(p => {
             if (this.wpmService.versionCompare(p.installed, p.releases[0].version) < 0) {
                 this.extendedData.push({package: p, isSelected: p.packageType != 'Solution-Main' ? true : false});
@@ -20,20 +21,26 @@ export class UpdatePackagesDialog {
         });
      }  
 
-     updateSelected() {
+     updateSelected(): void {
         this.extendedData.forEach(data => {
             if (data.isSelected) {
                 data.package.selected = data.package.releases[0].version;
                 this.wpmService.install(data.package);
             }
-        })
+        });
+        this.dialogRef.close();
      }
 
-     updateAll() {
+     updateAll(): void {
         this.extendedData.forEach(data => {
             data.package.selected = data.package.releases[0].version;
             this.wpmService.install(data.package);
-        })
+        });
+        this.dialogRef.close();
+     }
+
+     closeDialog(): void {
+         this.dialogRef.close();
      }
 }
 
