63767a93896f2790d4c1044ecd6d6e18ded9b8f1
lvostinar <lvostinar@servoy.com>
2021-08-25 10:28:13 +0300

    SVY-16370 [designer2] Errors when components are added to the form

--- com.servoy.eclipse.ngclient.ui/node/src/designer/servoydesigner.module.ts
+++ com.servoy.eclipse.ngclient.ui/node/src/designer/servoydesigner.module.ts
@@ -6,9 +6,11 @@ import { AllServicesModules } from '../ngclient/allservices.service';
 import { AllComponentsModule } from '../ngclient/allcomponents.module';
 import { ServoyPublicModule, ServoyPublicService } from '@servoy/public';
 import {DesignFormComponent, AddAttributeDirective} from './designform_component.component';
-import { ServoyPublicServiceImpl } from '../ngclient/services/servoy_public_impl.service';
+import { ServoyPublicServiceDesignerImpl } from './servoy_public_designer_impl.service';
 import { ServerDataService } from '../ngclient/services/serverdata.service';
 import {EditorContentService} from './editorcontent.service';
+import { BSWindow } from '../ngclient/services/bootstrap-window/bswindow.service';
+import { BSWindowManager } from '../ngclient/services/bootstrap-window/bswindow_manager.service';
 
 @NgModule({
   imports: [
@@ -19,7 +21,7 @@ import {EditorContentService} from './editorcontent.service';
     ServoyPublicModule
   ],
   declarations: [ServoyDesignerComponent, DesignFormComponent, AddAttributeDirective],
-  providers: [EditorContentService, ServerDataService, ServoyPublicServiceImpl, { provide: ServoyPublicService, useExisting: ServoyPublicServiceImpl }],
+  providers: [EditorContentService, BSWindow, BSWindowManager, ServerDataService, ServoyPublicServiceDesignerImpl, { provide: ServoyPublicService, useExisting: ServoyPublicServiceDesignerImpl }],
   schemas: [
               CUSTOM_ELEMENTS_SCHEMA
     ]
