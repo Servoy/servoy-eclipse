d57469a0b5f70ff705c0ebbbe58ff618bfba4ef9
Diana Bunaciu <dtimut@servoy.com>
2021-12-22 10:51:39 +0200

    SVY-16720 [ng2] Combobox - filtering not working do not allow filtering in combobox. just point to the first value that matches. Also allow backspace

--- com.servoy.eclipse.ngclient.ui/node/projects/servoydefault/src/lib/combobox/combobox.ts
+++ com.servoy.eclipse.ngclient.ui/node/projects/servoydefault/src/lib/combobox/combobox.ts
@@ -1,6 +1,6 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, Renderer2, SimpleChanges, ChangeDetectorRef, ViewChild, HostListener, QueryList, ElementRef, ViewChildren, ChangeDetectionStrategy, Inject } from '@angular/core';
-import { NgbDropdownItem } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDropdownItem, NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
 import { FormattingService } from '@servoy/public';
 import { ServoyDefaultBaseField } from '../basefield';
 
@@ -13,10 +13,15 @@ export class ServoyDefaultCombobox extends ServoyDefaultBaseField<HTMLInputEleme
 
     @ViewChildren(NgbDropdownItem) menuItems: QueryList<NgbDropdownItem>;
     @ViewChild('input') input: ElementRef<HTMLButtonElement>;
-
+    @ViewChild('tooltip') tooltip: NgbTooltip;
+    
     formattedValue: any;
     valueComparator: (value: { displayValue: any; realValue: any }) => boolean;
     openState = false;
+    keyboardSelectValue: string = null;
+    lastSelectValue: string = null;
+    firstItemFound = false;
+    private kbSelection = null;
     private skipFocus = false;
 
     constructor(renderer: Renderer2, protected cdRef: ChangeDetectorRef, private formatService: FormattingService, @Inject(DOCUMENT) doc: Document) {
@@ -29,12 +34,59 @@ export class ServoyDefaultCombobox extends ServoyDefaultBaseField<HTMLInputEleme
             // stop propagation when using list form component (to not break the selection)
             event.stopPropagation();
         }
+        this.lastSelectValue = null;
+        this.firstItemFound = false;
+        if (this.isPrintableChar(event.key)) {
+            clearTimeout(this.kbSelection);
+            if(event.key !== 'Backspace') this.keyboardSelectValue = (this.keyboardSelectValue ? this.keyboardSelectValue : '') + event.key;
+            else this.keyboardSelectValue = this.keyboardSelectValue ? this.keyboardSelectValue.slice(0, -1) : '';
+            this.lastSelectValue = this.keyboardSelectValue.slice();
+            this.refreshTooltip();
+            this.cdRef.detectChanges();
+            this.kbSelection = setTimeout(() => {
+                this.scrollToFirstMatchingItem();
+                this.keyboardSelectValue = null;
+                this.lastSelectValue = null;
+                this.tooltip.close();
+            }, 1000);
+        }
+    }
+    
+    svyOnInit() {
+        super.svyOnInit();
+        this.tooltip.autoClose = false;
     }
 
+    refreshTooltip() {
+        if (!this.tooltip.isOpen()) {
+            this.tooltip.open();
+        }
+    }
+    
+    handleTooltip(event: KeyboardEvent) {
+        this.tooltip.autoClose = false;
+        this.tooltip.ngbTooltip = 'This is the CHANGED text';
+        if (this.tooltip.isOpen()) {
+            this.tooltip.close();
+        } else {
+            this.tooltip.open();
+        }
+    }
+
+    isPrintableChar(key: string): boolean {
+        const nonPrintableValue = [
+            'Alt', 'AltGraph', 'CapsLock', 'Fn', 'Meta', 'NumLock', 'ScrollLock', 'Shift',
+            'Enter', 'Tab', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowUp', 'End', 'Home',
+            'PageUp', 'PageDown', 'Delete', 'Control', 'Insert', 'Del', 'Escape'
+        ];
+        if (nonPrintableValue.includes(key))
+            return false;
+        return true;
+    }
+    
     getDropDownWidth() {
         return this.input?.nativeElement?.clientWidth;
     }
-
 
     getFocusElement() {
         return this.input.nativeElement;
@@ -52,7 +104,7 @@ export class ServoyDefaultCombobox extends ServoyDefaultBaseField<HTMLInputEleme
                 if (!this.openState) this.onFocusLostMethodID(e);
             });
     }
-
+    
     openChange(state: boolean) {
         this.openState = state;
         if (state) {
@@ -64,13 +116,14 @@ export class ServoyDefaultCombobox extends ServoyDefaultBaseField<HTMLInputEleme
                 }
             });
         } else {
+            this.lastSelectValue = null;
             this.requestFocus(this.mustExecuteOnFocus);
         }
     }
 
     svyOnChanges(changes: SimpleChanges) {
-        this.valueComparator = this.valuelistID.isRealValueDate()? this.dateValueCompare: this.valueCompare;
-        if (changes['dataProviderID']) {
+        this.valueComparator =  this.valuelistID && this.valuelistID.isRealValueDate()? this.dateValueCompare: this.valueCompare;
+        if (changes['dataProviderID'] && this.valuelistID) {
             // eslint-disable-next-line eqeqeq
             const valueListElem = this.valuelistID.find(this.valueComparator);
             if (valueListElem) this.formattedValue = this.formatService.format(valueListElem.displayValue, this.format, false);
@@ -88,6 +141,33 @@ export class ServoyDefaultCombobox extends ServoyDefaultBaseField<HTMLInputEleme
     updateValue(realValue: any) {
         this.dataProviderID = realValue;
         this.dataProviderIDChange.emit(this.dataProviderID);
+    }
+
+    getStrongValue(value: any): any {
+        let retValue = '';
+        if (this.openState && this.lastSelectValue && value && value.startsWith(this.lastSelectValue)) {
+            retValue = this.lastSelectValue;
+        }
+        return retValue;
+    }
+
+    getRemainingValue(value: any): any {
+        let retValue = value;
+        if (this.openState && this.lastSelectValue && value && value.startsWith(this.lastSelectValue)) {
+            retValue = value.substring(this.lastSelectValue.length);
+        }
+        return retValue;
+    }
+
+    scrollToFirstMatchingItem() {
+        if (this.openState && this.lastSelectValue) {
+            for (const item of this.menuItems) {
+                if (item.elementRef.nativeElement.innerText.startsWith(this.lastSelectValue) && !this.firstItemFound) {
+                    this.firstItemFound = true;
+                    item.elementRef.nativeElement.focus();
+                }
+            }
+       }
     }
 
     // eslint-disable-next-line eqeqeq
