6d2f4f14b234b4ba4a9ddf968b5ea53bfa25c719
Andrei Costescu <costescuandrei@gmail.com>
2022-03-28 15:39:41 +0300

    Revert "Merge branch 'hackathon2022_epsilon'"
    This reverts commit 1373e2a153324967fa2917e3ebea1556ea2bc070, reversing
    changes made to 3e5e07ad818ce80034e9c64ef04ee38b546cce97.

--- com.servoy.eclipse.core/src/com/servoy/eclipse/core/JSDeveloperSolutionModel.java
+++ com.servoy.eclipse.core/src/com/servoy/eclipse/core/JSDeveloperSolutionModel.java
@@ -36,7 +36,6 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
-import org.json.JSONArray;
 
 import com.servoy.eclipse.core.resource.PersistEditorInput;
 import com.servoy.eclipse.core.util.RunInWorkspaceJob;
@@ -555,47 +554,5 @@ public class JSDeveloperSolutionModel implements IJSDeveloperSolutionModel
 			}
 		});
 		return foreignElementUUIDs;
-	}
-
-	public JSONArray js_getExistingVariants(String variantCategoryName)
-	{
-
-		JSONArray myArray = new JSONArray();
-
-		JSONObject myComp_1 = new JSONObject();
-		myComp_1.put("name", "button_1");
-
-		List<String> classes_1 = new ArrayList<String>();
-		classes_1.add("btn");
-		classes_1.add("btn-primary");
-		classes_1.add("margin-20");
-		classes_1.add("font-weight-bold");
-		myComp_1.put("classes", classes_1);
-
-		myArray.put(myComp_1);
-
-		JSONObject myComp_2 = new JSONObject();
-		myComp_2.put("name", "button_2");
-
-		List<String> classes_2 = new ArrayList<String>();
-		classes_2.add("btn");
-		classes_2.add("btn-default");
-		classes_2.add("margin-10");
-		classes_2.add("font-weight-bold");
-		myComp_2.put("classes", classes_2);
-
-		myArray.put(myComp_2);
-
-		return myArray;
-	}
-
-	public void js_setVariantsFor(String variantCategoryName, JSONArray components)
-	{
-		System.out.println(variantCategoryName + " : " + components.toString());
-	}
-
-	public void js_setVariants(JSONArray variants)
-	{
-		System.out.println(variants.toString());
 	}
 }
