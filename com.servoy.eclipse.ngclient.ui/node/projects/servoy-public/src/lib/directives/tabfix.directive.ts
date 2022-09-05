43ff826531a0ef3d14cd7e228b159e2f3976a846
lvostinar <lvostinar@servoy.com>
2022-09-05 16:24:04 +0300

    SVY-17316 Type Ahead doesn't allow value not in valuelist in NG2

---
+++ com.servoy.eclipse.ngclient.ui/node/projects/servoy-public/src/lib/directives/tabfix.directive.ts
@@ -0,0 +1,33 @@
+import { Directive , Input , HostListener} from '@angular/core';
+import {IPopupSupportComponent} from '../spectypes.service';
+
+@Directive({
+    selector: '[svyTabFix]'
+})
+export class TabFixDirective {
+    // this directive needs to be in a module because of listener priority, so i put it here
+    @Input('svyTabFix') typeahead: IPopupSupportComponent;
+    startedTyping : boolean = false;
+    
+    constructor() { }
+    
+    @HostListener('focus', ['$event'])
+    onFocus(target) {
+       this.startedTyping = false;
+    }
+    
+    @HostListener('keydown', ['$event'])
+    handleKeyDown(event: KeyboardEvent) {
+        if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
+            // stop propagation when using list form component (to not break the selection)
+            event.stopPropagation();
+        }
+        if (event.key != 'Tab')
+        {
+            this.startedTyping = true;
+        }
+        else if (!this.startedTyping){
+            this.typeahead.closePopup();
+        }
+    }
+}
