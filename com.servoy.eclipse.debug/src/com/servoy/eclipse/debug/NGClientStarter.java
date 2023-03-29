c0d4d751583311eb5818a6d4267427b023e5809e
Andrei Costescu <costescuandrei@gmail.com>
2023-03-29 17:24:45 +0300

    For SVY-17921: Adding missing String methods that are supported by Rhino
    In the added comments there is code that can be used to update dltk's
    native-references.xml with function descriptions based on JSDocs from
    our com.servoy.j2db.documentation.scripting.docs package (including
    sample/params/retval). I did that manually for a few new methods, but we
    should go through all of them at some point. See comments at the
    beginning of NGClientStarter.runProgressBarAndNGClient.
    
    Follow-up case: SVY-18066

--- com.servoy.eclipse.debug/src/com/servoy/eclipse/debug/NGClientStarter.java
+++ com.servoy.eclipse.debug/src/com/servoy/eclipse/debug/NGClientStarter.java
@@ -44,6 +44,50 @@ public interface NGClientStarter
 	 */
 	default void runProgressBarAndNGClient(IProgressMonitor monitor)
 	{
+		// *** the code in comments BELOW has nothing to do with the actual run client method; skip it
+
+		// this commented out code is useful for adjusting native-references.xml of org.eclipse.dltk.javascript.core/resources
+		// which is the documentation place for native javascript things (String, Number etc. for code completion; if you want
+		// to generate the content to put in a method's description in that xml based on the JSDoc of classes that we have
+		// in package com.servoy.j2db.documentation.scripting.docs (for example com.servoy.j2db.documentation.scripting.docs.String)
+		// (make sure you copy the generated servoydoc_jslib.xml from jenkins update_zip to /com.servoy.eclipse.core/src/com/servoy/eclipse/core/doc/servoydoc_jslib.xml)
+		// uncomment the lines below, and when running NGClient you will see these in the console:
+
+		// IN CASE OF OVERLOADED METHODS (same name, different/optional args) do use the one with the most args in generating the doc for native-references.xml
+		// AND ADD [] to the optional arguments (so '@param String [myOptionalArg]' instead of '@param String myOptionalArg') - StringEscapeUtils.escapeXml10 does not do that (as it's meant for having separate descriptions for each overload, but native-references.xml only supports one version of the method)
+//		System.out.println("\npadStart(int, String): " + StringEscapeUtils.escapeXml10(
+//			TypeCreator.getDoc("padStart", com.servoy.j2db.documentation.scripting.docs.String.class,
+//				new Class[] { int.class, com.servoy.j2db.documentation.scripting.docs.String.class }))
+//			.replace("\r", "&#13;").replace("\n", "&#10;"));
+//		System.out.println("\npadEnd(int, String): " + StringEscapeUtils.escapeXml10(
+//			TypeCreator.getDoc("padEnd", com.servoy.j2db.documentation.scripting.docs.String.class,
+//				new Class[] { int.class, com.servoy.j2db.documentation.scripting.docs.String.class }))
+//			.replace("\r", "&#13;").replace("\n", "&#10;"));
+//		System.out.println("\nfromCharCode(Number...): " + StringEscapeUtils.escapeXml10(
+//			TypeCreator.getDoc("fromCharCode", com.servoy.j2db.documentation.scripting.docs.String.class,
+//				new Class[] { com.servoy.j2db.documentation.scripting.docs.Number[].class }))
+//			.replace("\r", "&#13;").replace("\n", "&#10;"));
+//		System.out.println("\nfromCodePoint(Number...): " + StringEscapeUtils.escapeXml10(
+//			TypeCreator.getDoc("fromCodePoint", com.servoy.j2db.documentation.scripting.docs.String.class,
+//				new Class[] { com.servoy.j2db.documentation.scripting.docs.Number[].class }))
+//			.replace("\r", "&#13;").replace("\n", "&#10;"));
+//		System.out.println("\ncodePointAt(Number): " + StringEscapeUtils.escapeXml10(
+//			TypeCreator.getDoc("codePointAt", com.servoy.j2db.documentation.scripting.docs.String.class,
+//				new Class[] { com.servoy.j2db.documentation.scripting.docs.Number.class }))
+//			.replace("\r", "&#13;").replace("\n", "&#10;"));
+//		System.out.println("\ntrimStart(): " + StringEscapeUtils.escapeXml10(
+//			TypeCreator.getDoc("trimStart", com.servoy.j2db.documentation.scripting.docs.String.class,
+//				new Class[0]))
+//			.replace("\r", "&#13;").replace("\n", "&#10;"));
+//		System.out.println("\ntrimEnd(): " + StringEscapeUtils.escapeXml10(
+//			TypeCreator.getDoc("trimStart", com.servoy.j2db.documentation.scripting.docs.String.class,
+//				new Class[0]))
+//			.replace("\r", "&#13;").replace("\n", "&#10;"));
+
+
+		// *** the code ABOVE has nothing to do with the actual run client method
+
+
 //		Job[] buildJobs = Job.getJobManager().find(NGClientConstants.NPM_BUILD_JOB);
 		if ( /* buildJobs.length == 0 || */ RunNPMCommand.isNGBuildRunning())
 		{
