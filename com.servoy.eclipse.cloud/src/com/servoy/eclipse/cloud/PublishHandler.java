c0b53ed2717963b77818d19526f5e05688edd407
vidmarian <mvid@servoy.com>
2023-05-31 13:55:34 +0300

    SVY-17910 Edit/Publish Application to ServoyCloud

--- com.servoy.eclipse.cloud/src/com/servoy/eclipse/cloud/PublishHandler.java
+++ com.servoy.eclipse.cloud/src/com/servoy/eclipse/cloud/PublishHandler.java
@@ -1,6 +1,7 @@
 package com.servoy.eclipse.cloud;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
@@ -21,49 +22,54 @@ import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.RepositoryBuilder;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+
 import com.servoy.eclipse.model.util.ServoyLog;
 
 public class PublishHandler extends AbstractHandler implements IResourceChangeListener{
 
-    private Git git = null;
+	private static final AtomicBoolean isCleanGit = new AtomicBoolean(true);
+    private boolean isGitWs = false;
     private IWorkspace workspace = null;
+    Repository repository = null;
     
     public PublishHandler() {
     	try {
     		workspace = ResourcesPlugin.getWorkspace();
     		workspace.addResourceChangeListener(this);
     		IPath workspaceRoot = workspace.getRoot().getLocation();
-    		Repository repository = new  RepositoryBuilder().setWorkTree(workspaceRoot.toFile()).build();
-    		if (repository.getDirectory().exists())
-    			git = new Git(repository);
+    		repository = new  RepositoryBuilder().setWorkTree(workspaceRoot.toFile()).build(); //this will trigger an exception if workspace root is not a valid git repository;
+    		isGitWs = true;
+    		isCleanGit.set(isCleanGitRepo()); //detect changes from eventually outside apps
     		fireHandlerChanged(new HandlerEvent(this, true, false));//force eclipse to call isEnabled() method
     	} catch (IOException | NoWorkTreeException e) {
     		ServoyLog.logError(e);
     	}
     }
-
+    
     @Override
-    public Object execute(ExecutionEvent event) throws ExecutionException {
-    	String commitMessage = askForCommitMessage();
-    	if (commitMessage != null) {
-    		Job.create("Publish to ServoyCloud", (monitor) -> {
-        		try {
-        			monitor.beginTask("Pull from repository", 100);
-                    git.pull().call(); //pull from remote
-       		        commitChanges(commitMessage);
-       		        monitor.subTask(commitMessage);
-       		        git.push().call(); //push to remote    
-        			monitor.done(); 
-        			fireHandlerChanged(new HandlerEvent(this, true, false));
-                } catch (GitAPIException e) {
-                    ServoyLog.logError(e);
-                }
-         	}).schedule();
-    		
-    		
-    	}
-        return null;
-    }
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		if (isGitWs) {
+			String commitMessage = askForCommitMessage();
+        	if (commitMessage != null) {
+        		Job.create("Publish to ServoyCloud", (monitor) -> {
+            		try (Git git = new Git(repository);) {
+            			monitor.beginTask("Pull from repository", 100);
+                        git.pull().call(); //pull from remote
+           		        commitChanges(git, commitMessage);
+           		        monitor.subTask(commitMessage);
+           		        git.push().call(); //push to remote   
+           		        isCleanGit.set(isCleanGitRepo());
+            			fireHandlerChanged(new HandlerEvent(this, true, false));
+            			monitor.done(); 
+                    } catch (GitAPIException e) {
+                        ServoyLog.logError(e);
+                    }
+             	}).schedule();
+        	}
+		}
+		return null;
+	}
+   
 
     private String askForCommitMessage() {
     	Shell shell = Display.getCurrent().getActiveShell();
@@ -76,7 +82,7 @@ public class PublishHandler extends AbstractHandler implements IResourceChangeLi
         }
     }
 
-    private void commitChanges(String commitMessage) throws GitAPIException {
+    private void commitChanges(Git git, String commitMessage) throws GitAPIException {
         git.add().addFilepattern(".").call();
         CommitCommand commitCmd = git.commit();
         commitCmd.setMessage(commitMessage).call();
@@ -84,24 +90,26 @@ public class PublishHandler extends AbstractHandler implements IResourceChangeLi
     
     @Override
     public boolean isEnabled() {
-    	return (git != null) && !isCleanGitRepo(git);
+    	return !isCleanGit.get(); //git repo is dirty so we may publish the changes
     }
     
     @Override
     public void resourceChanged(IResourceChangeEvent event) {
         if (event.getType() == IResourceChangeEvent.POST_CHANGE) {//save or save all buttons were pressed
+        	if (isGitWs) isCleanGit.set(isCleanGitRepo()); //update git status
     		fireHandlerChanged(new HandlerEvent(this, true, false));
         }
     }
     
-    private boolean isCleanGitRepo(Git gitRepo) {
-    	if (gitRepo != null) {
-    		try {
-				return gitRepo.status().call().isClean();
+    
+    private boolean isCleanGitRepo() {
+    	if (isGitWs) {
+    		try (Git git = new Git(repository)) {
+				return git.status().call().isClean();
 			} catch (NoWorkTreeException | GitAPIException e) {
 				ServoyLog.logError(e);
 			}
-    	} 
-    	return true;
+    	}
+    	return true; //this will disable Publish
     }
 }
