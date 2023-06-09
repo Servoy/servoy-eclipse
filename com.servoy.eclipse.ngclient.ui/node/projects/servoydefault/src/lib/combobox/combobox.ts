31cb631831706a48910a2e88b0b0d8fbacf8cf9f
lvostinar <lvostinar@servoy.com>
2023-06-09 16:06:38 +0300

    SVY-18077 TiNG combobox opens on controller.focusField()

--- com.servoy.eclipse.ngclient.ui/node/projects/servoydefault/src/lib/combobox/combobox.ts
+++ com.servoy.eclipse.ngclient.ui/node/projects/servoydefault/src/lib/combobox/combobox.ts
@@ -1,7 +1,7 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, Renderer2, SimpleChanges, ChangeDetectorRef, ViewChild, HostListener, QueryList, ElementRef, ViewChildren, ChangeDetectionStrategy, Inject } from '@angular/core';
 import { NgbDropdownItem, NgbTooltip, NgbDropdown } from '@ng-bootstrap/ng-bootstrap';
-import { FormattingService } from '@servoy/public';
+import { FormattingService, ServoyPublicService } from '@servoy/public';
 import { ServoyDefaultBaseField } from '../basefield';
 
 @Component({
@@ -13,7 +13,7 @@ export class ServoyDefaultCombobox extends ServoyDefaultBaseField<HTMLInputEleme
 
     @ViewChildren(NgbDropdownItem) menuItems: QueryList<NgbDropdownItem>;
     @ViewChild('input') input: ElementRef<HTMLButtonElement>;
-    @ViewChild( NgbDropdown ) comboboxDropdown: NgbDropdown;
+    @ViewChild(NgbDropdown) comboboxDropdown: NgbDropdown;
     @ViewChild('tooltip') tooltip: NgbTooltip;
 
     formattedValue: any;
@@ -23,8 +23,9 @@ export class ServoyDefaultCombobox extends ServoyDefaultBaseField<HTMLInputEleme
     lastSelectValue: string = null;
     firstItemFound = false;
     private skipFocus = false;
+    private showPopupOnFocusGain = false;
 
-    constructor(renderer: Renderer2, protected cdRef: ChangeDetectorRef, private formatService: FormattingService, @Inject(DOCUMENT) doc: Document) {
+    constructor(renderer: Renderer2, protected cdRef: ChangeDetectorRef, private formatService: FormattingService, @Inject(DOCUMENT) doc: Document, protected servoyService: ServoyPublicService) {
         super(renderer, cdRef, formatService, doc);
     }
 
@@ -37,10 +38,10 @@ export class ServoyDefaultCombobox extends ServoyDefaultBaseField<HTMLInputEleme
         this.lastSelectValue = null;
         this.firstItemFound = false;
         if (this.isPrintableChar(event.key)) {
-            if(document.activeElement === this.getFocusElement() && !this.comboboxDropdown.isOpen()){
+            if (document.activeElement === this.getFocusElement() && !this.comboboxDropdown.isOpen()) {
                 this.comboboxDropdown.open();
             }
-            if(event.key !== 'Backspace') this.keyboardSelectValue = (this.keyboardSelectValue ? this.keyboardSelectValue : '') + event.key;
+            if (event.key !== 'Backspace') this.keyboardSelectValue = (this.keyboardSelectValue ? this.keyboardSelectValue : '') + event.key;
             else this.keyboardSelectValue = this.keyboardSelectValue ? this.keyboardSelectValue.slice(0, -1) : '';
             this.lastSelectValue = this.keyboardSelectValue.slice();
             if (!this.lastSelectValue) this.closeTooltip();
@@ -48,17 +49,24 @@ export class ServoyDefaultCombobox extends ServoyDefaultBaseField<HTMLInputEleme
 
             this.cdRef.detectChanges();
             this.scrollToFirstMatchingItem();
-       } else {
-           if(this.keyboardSelectValue) this.lastSelectValue = this.keyboardSelectValue.slice();
-           if (!this.lastSelectValue) this.closeTooltip();
-           else this.refreshTooltip();
-           
-           this.cdRef.detectChanges();
-           this.scrollToFirstMatchingItem();
-       }
+        } else {
+            if (this.keyboardSelectValue) this.lastSelectValue = this.keyboardSelectValue.slice();
+            if (!this.lastSelectValue) this.closeTooltip();
+            else this.refreshTooltip();
+
+            this.cdRef.detectChanges();
+            this.scrollToFirstMatchingItem();
+        }
     }
-    
+
     svyOnInit() {
+        let showPopup = this.servoyApi.getClientProperty('Combobox.showPopupOnFocusGain');
+        if (showPopup === null || showPopup === undefined) {
+            showPopup = this.servoyService.getUIProperty('Combobox.showPopupOnFocusGain');
+        }
+        if (showPopup !== null && showPopup !== undefined) {
+            this.showPopupOnFocusGain = showPopup;
+        }
         super.svyOnInit();
         this.tooltip.autoClose = false;
     }
@@ -68,7 +76,7 @@ export class ServoyDefaultCombobox extends ServoyDefaultBaseField<HTMLInputEleme
             this.tooltip.open();
         }
     }
-    
+
     handleTooltip(event: KeyboardEvent) {
         this.tooltip.autoClose = false;
         this.tooltip.ngbTooltip = 'This is the CHANGED text';
@@ -79,24 +87,24 @@ export class ServoyDefaultCombobox extends ServoyDefaultBaseField<HTMLInputEleme
         }
     }
 
-    isPrintableChar( key: string ): boolean {
+    isPrintableChar(key: string): boolean {
         const nonPrintableValue = [
             'Alt', 'AltGraph', 'CapsLock', 'Fn', 'Meta', 'NumLock', 'ScrollLock', 'Command', 'Shift',
             'Enter', 'Tab', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowUp', 'End', 'Home',
             'PageUp', 'PageDown', 'Delete', 'Control', 'Insert', 'Del', 'Escape',
-            'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12' 
+            'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12'
         ];
-        if ( nonPrintableValue.includes( key ) ) {
+        if (nonPrintableValue.includes(key)) {
             const keysThatCloseTooltip = [
                 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowUp', 'End', 'Home',
                 'PageUp', 'PageDown', 'Delete', 'Del', 'Tab'
             ];
-            if ( keysThatCloseTooltip.includes( key ) ) this.closeTooltip();
+            if (keysThatCloseTooltip.includes(key)) this.closeTooltip();
             return false;
         }
         return true;
     }
-    
+
     getDropDownWidth() {
         return this.input?.nativeElement?.clientWidth;
     }
@@ -106,9 +114,12 @@ export class ServoyDefaultCombobox extends ServoyDefaultBaseField<HTMLInputEleme
     }
 
     attachFocusListeners(nativeElement: HTMLElement) {
-        if (this.onFocusGainedMethodID)
+        if (this.onFocusGainedMethodID || this.showPopupOnFocusGain)
             this.renderer.listen(nativeElement, 'focus', (e) => {
-                if (!this.skipFocus && this.mustExecuteOnFocus !== false) this.onFocusGainedMethodID(e);
+                if (this.onFocusGainedMethodID && !this.skipFocus && this.mustExecuteOnFocus) this.onFocusGainedMethodID(e);
+                if (!this.skipFocus && this.showPopupOnFocusGain && !this.comboboxDropdown.isOpen()) {
+                    this.comboboxDropdown.open();
+                }
                 this.skipFocus = false;
                 this.mustExecuteOnFocus = true;
             });
@@ -117,16 +128,18 @@ export class ServoyDefaultCombobox extends ServoyDefaultBaseField<HTMLInputEleme
                 if (!this.openState) this.onFocusLostMethodID(e);
             });
     }
-    
+
     requestFocus(mustExecuteOnFocusGainedMethod: boolean) {
-		super.requestFocus(mustExecuteOnFocusGainedMethod);
-        this.comboboxDropdown.open();
-	}
-    
+        super.requestFocus(mustExecuteOnFocusGainedMethod);
+        if (this.showPopupOnFocusGain) {
+            this.comboboxDropdown.open();
+        }
+    }
+
     openChange(state: boolean) {
         this.openState = state;
+        this.skipFocus = true;
         if (state) {
-            this.skipFocus = true;
             setTimeout(() => {
                 const item = this.menuItems.find((element) => element.elementRef.nativeElement.classList.contains('active'));
                 if (item) {
@@ -140,7 +153,7 @@ export class ServoyDefaultCombobox extends ServoyDefaultBaseField<HTMLInputEleme
     }
 
     svyOnChanges(changes: SimpleChanges) {
-        this.valueComparator =  this.valuelistID && this.valuelistID.isRealValueDate()? this.dateValueCompare: this.valueCompare;
+        this.valueComparator = this.valuelistID && this.valuelistID.isRealValueDate() ? this.dateValueCompare : this.valueCompare;
         if (changes['dataProviderID'] && this.valuelistID) {
             // eslint-disable-next-line eqeqeq
             const valueListElem = this.valuelistID.find(this.valueComparator);
@@ -156,7 +169,7 @@ export class ServoyDefaultCombobox extends ServoyDefaultBaseField<HTMLInputEleme
             }
         }
         else if (changes['dataProviderID'] && !this.valuelistID) {
-          this.formattedValue = this.dataProviderID;
+            this.formattedValue = this.dataProviderID;
         }
         delete changes['editable']; // ignore the editable property
         if (this.formattedValue === "" || this.formattedValue === null || this.formattedValue === undefined) {
@@ -196,7 +209,7 @@ export class ServoyDefaultCombobox extends ServoyDefaultBaseField<HTMLInputEleme
                     item.elementRef.nativeElement.focus();
                 }
             }
-       }
+        }
     }
 
     private closeTooltip() {
@@ -206,10 +219,10 @@ export class ServoyDefaultCombobox extends ServoyDefaultBaseField<HTMLInputEleme
     }
 
     // eslint-disable-next-line eqeqeq
-    private valueCompare = (valueListValue: { displayValue: any; realValue: any }): boolean  => valueListValue.realValue == this.dataProviderID;
+    private valueCompare = (valueListValue: { displayValue: any; realValue: any }): boolean => valueListValue.realValue == this.dataProviderID;
 
     private dateValueCompare = (valueListValue: { displayValue: any; realValue: Date }): boolean => {
-        if (this.dataProviderID){
+        if (this.dataProviderID) {
             return valueListValue.realValue.getTime() === this.dataProviderID.getTime();
         }
         return false;
