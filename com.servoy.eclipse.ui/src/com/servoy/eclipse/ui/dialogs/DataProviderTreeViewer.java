831084837792238fa73a4c4994a61c75fa1c3b8c
cPecican <cpecican@servoy.com>
2023-11-29 17:01:03 +0200

    SVY-18741 Selected dp in dark theme is not visible - update

--- com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/dialogs/DataProviderTreeViewer.java
+++ com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/dialogs/DataProviderTreeViewer.java
@@ -40,14 +40,19 @@ import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.TreeItem;
 
 import com.servoy.eclipse.core.Activator;
 import com.servoy.eclipse.model.ServoyModelFinder;
 import com.servoy.eclipse.model.util.ModelUtils;
 import com.servoy.eclipse.model.util.ServoyLog;
+import com.servoy.eclipse.ui.EclipseCSSThemeListener;
 import com.servoy.eclipse.ui.dialogs.DataProviderTreeViewer.DataProviderOptions.INCLUDE_RELATIONS;
 import com.servoy.eclipse.ui.labelproviders.DelegateLabelProvider;
 import com.servoy.eclipse.ui.labelproviders.RelationLabelProvider;
@@ -1044,6 +1049,29 @@ public class DataProviderTreeViewer extends FilteredTreeViewer
 				else if (previousElems != null)
 				{
 					((TreeViewer)event.getSource()).update(previousElems.toArray(), null);
+				}
+
+				if (EclipseCSSThemeListener.isDarkThemeSelected())
+				{
+					((TreeViewer)event.getSource()).getTree().addListener(SWT.EraseItem, evt -> {
+						evt.detail &= ~SWT.HOT;
+						if ((evt.detail & SWT.SELECTED) == 0) return; /// item not selected
+
+						TreeItem item = (TreeItem)evt.item;
+						int clientWidth = item.getBounds().width;
+
+						GC gc = evt.gc;
+						Color oldForeground = gc.getForeground();
+						Color oldBackground = gc.getBackground();
+
+						gc.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_GRAY));
+						gc.setForeground(Display.getCurrent().getSystemColor(SWT.COLOR_WHITE));
+						gc.fillRectangle(item.getBounds().x, evt.y, clientWidth, evt.height);
+
+						gc.setForeground(oldForeground);
+						gc.setBackground(oldBackground);
+						evt.detail &= ~SWT.SELECTED;
+					});
 				}
 			}
 		}
