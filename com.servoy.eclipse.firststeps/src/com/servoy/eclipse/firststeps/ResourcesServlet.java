0f0c4f0f625802d41a502ae373f6581aeb79951e
Johan Compagner <jcompagner@servoy.com>
2023-12-18 15:12:28 +0100

    SVY-18757 Fix Server-Side Request Forgery (SSRF) reported in Snyk.io
    try to have a whitelist (startsWith)
    removed __MACOSX stuff

--- com.servoy.eclipse.firststeps/src/com/servoy/eclipse/firststeps/ResourcesServlet.java
+++ com.servoy.eclipse.firststeps/src/com/servoy/eclipse/firststeps/ResourcesServlet.java
@@ -79,17 +79,23 @@ public class ResourcesServlet extends HttpServlet
 		{
 			
 			path = Paths.get(req.getServletPath()).normalize().toString().replace('\\', '/') + path;
-			URL res = getClass().getResource(path);
-			if (res != null)
-			{
-				URLConnection uc = res.openConnection();
-				resp.setContentLength(uc.getContentLength());
-				resp.setContentType(MimeTypes.guessContentTypeFromName(path));
-				InputStream in = uc.getInputStream();
-				ServletOutputStream outputStream = resp.getOutputStream();
-				Utils.streamCopy(in, outputStream);
-				outputStream.flush();
-				Utils.close(in);
+			if (path.startsWith("/firststeps/")) {
+				URL res = getClass().getResource(path);
+				if (res != null)
+				{
+					URLConnection uc = res.openConnection();
+					resp.setContentLength(uc.getContentLength());
+					resp.setContentType(MimeTypes.guessContentTypeFromName(path));
+					InputStream in = uc.getInputStream();
+					ServletOutputStream outputStream = resp.getOutputStream();
+					Utils.streamCopy(in, outputStream);
+					outputStream.flush();
+					Utils.close(in);
+				}
+				else
+				{
+					resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+				}
 			}
 			else
 			{
