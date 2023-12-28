66c78d550281d3fe4fc646b410a49d044b6e7f14
Johan Compagner <jcompagner@servoy.com>
2023-12-28 15:22:17 +0100

    SVY-18757 Fix Server-Side Request Forgery (SSRF) reported in Snyk.io
    normalize the path and check if it starts with "rfb" then everything
    after that is allowed.

--- com.servoy.eclipse.designer.rfb/src/com/servoy/eclipse/designer/rfb/startup/ResourcesServlet.java
+++ com.servoy.eclipse.designer.rfb/src/com/servoy/eclipse/designer/rfb/startup/ResourcesServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.file.Paths;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -26,24 +27,27 @@ public class ResourcesServlet extends HttpServlet
 	{
 		String path = req.getPathInfo();
 		if ("/".equals(path)) path = "/index.html";
-		path = req.getServletPath() + path;
-		URL res = getClass().getResource(path);
-		if (res != null)
+		path = Paths.get(req.getServletPath() + path).normalize().toString().replace('\\', '/');
+		if (path.startsWith("/rfb/"))
 		{
-			URLConnection uc = res.openConnection();
-			uc.setUseCaches(false);
-			if (HTTPUtils.checkAndSetUnmodified(req, resp, uc.getLastModified())) return;
-			resp.setContentLength(uc.getContentLength());
-			resp.setContentType(MimeTypes.guessContentTypeFromName(path));
-			InputStream in = uc.getInputStream();
-			ServletOutputStream outputStream = resp.getOutputStream();
-			Utils.streamCopy(in, outputStream);
-			outputStream.flush();
-			Utils.close(in);
-		}
-		else
-		{
-			resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+			URL res = getClass().getResource(path);
+			if (res != null)
+			{
+				URLConnection uc = res.openConnection();
+				uc.setUseCaches(false);
+				if (HTTPUtils.checkAndSetUnmodified(req, resp, uc.getLastModified())) return;
+				resp.setContentLength(uc.getContentLength());
+				resp.setContentType(MimeTypes.guessContentTypeFromName(path));
+				InputStream in = uc.getInputStream();
+				ServletOutputStream outputStream = resp.getOutputStream();
+				Utils.streamCopy(in, outputStream);
+				outputStream.flush();
+				Utils.close(in);
+			}
+			else
+			{
+				resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+			}
 		}
 	}
 }
