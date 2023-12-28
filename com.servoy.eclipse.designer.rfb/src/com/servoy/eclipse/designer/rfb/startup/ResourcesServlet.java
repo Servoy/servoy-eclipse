75d401b35092a3dcd16fb4942337f34c8b3a3b7a
Johan Compagner <jcompagner@servoy.com>
2023-12-28 16:03:02 +0100

    SVY-18757 Fix Server-Side Request Forgery (SSRF) reported in Snyk.io
    validate the path against a path pattern.

--- com.servoy.eclipse.designer.rfb/src/com/servoy/eclipse/designer/rfb/startup/ResourcesServlet.java
+++ com.servoy.eclipse.designer.rfb/src/com/servoy/eclipse/designer/rfb/startup/ResourcesServlet.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.file.Paths;
+import java.util.regex.Pattern;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -20,6 +21,8 @@ import com.servoy.j2db.util.Utils;
 @WebServlet("/rfb/*")
 public class ResourcesServlet extends HttpServlet
 {
+	private static final Pattern PATH_PATTERN = Pattern.compile("^/([a-zA-Z0-9_-]+/)*[a-zA-Z0-9_-]+$");
+
 	private static final long serialVersionUID = 1L;
 
 	@Override
@@ -28,7 +31,7 @@ public class ResourcesServlet extends HttpServlet
 		String path = req.getPathInfo();
 		if ("/".equals(path)) path = "/index.html";
 		path = Paths.get(req.getServletPath() + path).normalize().toString().replace('\\', '/');
-		if (path.startsWith("/rfb/"))
+		if (isValidPath(path) && path.startsWith("/rfb/"))
 		{
 			URL res = getClass().getResource(path);
 			if (res != null)
@@ -49,5 +52,14 @@ public class ResourcesServlet extends HttpServlet
 				resp.sendError(HttpServletResponse.SC_NOT_FOUND);
 			}
 		}
+		else
+		{
+			resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+		}
+	}
+
+	private boolean isValidPath(String path)
+	{
+		return PATH_PATTERN.matcher(path).matches();
 	}
 }
