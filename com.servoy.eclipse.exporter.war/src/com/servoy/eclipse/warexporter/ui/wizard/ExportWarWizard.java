8e59deebd4bc97cc6706b9cbc8ad3e1b20c90755
Johan Compagner <jcompagner@servoy.com>
2025-01-14 09:28:22 +0100

    remove the ng2 property from the command line generator
    default is now true

--- com.servoy.eclipse.exporter.war/src/com/servoy/eclipse/warexporter/ui/wizard/ExportWarWizard.java
+++ com.servoy.eclipse.exporter.war/src/com/servoy/eclipse/warexporter/ui/wizard/ExportWarWizard.java
@@ -785,7 +785,6 @@ public class ExportWarWizard extends DirtySaveExportWizard implements IExportWiz
 		appendToBuilder(sb, " -overwriteAllProperties", exportModel.isOverwriteDeployedServoyProperties());
 		appendToBuilder(sb, " -log4jConfigurationFile ", exportModel.getLog4jConfigurationFile());
 		appendToBuilder(sb, " -webXmlFileName ", exportModel.getWebXMLFileName());
-		appendToBuilder(sb, " -ng2", exportModel.exportNG2Mode());
 		sb.append("\"");
 
 		StringSelection selection = new StringSelection(sb.toString());
