751b51219236f7666aeea3a3a294006065a79456
Andrei Costescu <costescuandrei@gmail.com>
2025-02-06 17:44:13 +0200

    Just some convenience things (anchor menu click + non watch debug target)

--- com.servoy.eclipse.designer.rfb/node/src/designer/contextmenu/contextmenu.component.ts
+++ com.servoy.eclipse.designer.rfb/node/src/designer/contextmenu/contextmenu.component.ts
@@ -477,7 +477,11 @@ export class ContextMenuComponent implements OnInit {
 
             entry = new ContextmenuItem(
                 'Anchoring',
-                null
+                () => {
+                    if (this.selection && this.selection.length > 0) {
+                        this.editorSession.setCssAnchoring(this.selection, null);
+                    }
+                }
             );
             entry.getItemClass = () => {
                 if (this.isInResponsiveContainer()) return 'disabled';
