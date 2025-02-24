bcba1ee62c5a160fe51803b9c5cd9cac43fabb6f
Johan Compagner <jcompagner@servoy.com>
2025-02-24 17:53:37 +0100

    SVY-19909 Update servoy-admin property testingMode

--- com.servoy.eclipse.ngclient.ui/src/com/servoy/eclipse/ngclient/ui/WebPackagesListener.java
+++ com.servoy.eclipse.ngclient.ui/src/com/servoy/eclipse/ngclient/ui/WebPackagesListener.java
@@ -88,6 +88,7 @@ import com.servoy.eclipse.model.war.exporter.ITiNGExportModel;
 import com.servoy.eclipse.ngclient.ui.utils.ZipUtils;
 import com.servoy.j2db.util.DeletePathVisitor;
 import com.servoy.j2db.util.Pair;
+import com.servoy.j2db.util.Settings;
 import com.servoy.j2db.util.SortedList;
 import com.servoy.j2db.util.Utils;
 
@@ -137,6 +138,13 @@ public class WebPackagesListener implements ILoadedNGPackagesListener
 				else
 				{
 					toRun = "build";
+				}
+			}
+			else
+			{
+				if (Boolean.valueOf(Settings.getInstance().getProperty("servoy.ngclient.testingMode", "false")).booleanValue())
+				{
+					toRun = "build_sourcemap";
 				}
 			}
 			whatToRun = toRun;
