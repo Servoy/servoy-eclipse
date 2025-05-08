cfe897541b117ed0e6d17c0419d80583f659564e
Diana Bunaciu <dtimut@servoy.com>
2025-05-08 16:47:00 +0300

    SVY-20198 TypeError: Cannot read properties of null (reading 'script') throw error when callback is not defined

--- com.servoy.eclipse.ngclient.ui/node/projects/window/src/lib/window_service/window.service.ts
+++ com.servoy.eclipse.ngclient.ui/node/projects/window/src/lib/window_service/window.service.ts
@@ -83,7 +83,8 @@ export class WindowPluginService {
                                 //$sabloTestability.block(true);
                                 setTimeout((clb: { script: string; formname?: string }, clbArgs: Array<any>) => {
                                     let formName = clbArgs[0].formName;
-                                    if (!formName) formName = clb.formname;
+                                    if (!formName && clb) formName = clb.formname;
+									if (!clb) throw new Error('No callback script found for shortcut: ' + newvalue.shortcut);
                                     this.servoyService.executeInlineScript(formName, clb.script, clbArgs);
                                     //$sabloTestability.block(false);
                                 }, 10, callback, argsWithEvent);
