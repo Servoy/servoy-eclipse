2374db3f110b05adf2eaeb590cfaf6fc2f28e2d8
Rob Gansevles <rgansevles@gmail.com>
2025-05-16 14:13:05 +0200

    SVY-20207 Support array columns for some databases
    Fix support for uuid array columns

--- com.servoy.eclipse.model/src/com/servoy/eclipse/model/repository/DataModelManager.java
+++ com.servoy.eclipse.model/src/com/servoy/eclipse/model/repository/DataModelManager.java
@@ -22,6 +22,7 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
+import java.sql.Types;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -80,7 +81,6 @@ import com.servoy.j2db.query.ColumnType;
 import com.servoy.j2db.server.shared.ApplicationServerRegistry;
 import com.servoy.j2db.util.DataSourceUtils;
 import com.servoy.j2db.util.DatabaseUtils;
-import com.servoy.j2db.util.Debug;
 import com.servoy.j2db.util.Pair;
 import com.servoy.j2db.util.ServoyJSONArray;
 import com.servoy.j2db.util.ServoyJSONObject;
@@ -542,7 +542,7 @@ public class DataModelManager implements IServerInfoManager
 				}
 				catch (CoreException e)
 				{
-					Debug.error(e);
+					ServoyLog.logError(e);
 				}
 			}
 		}
@@ -555,7 +555,7 @@ public class DataModelManager implements IServerInfoManager
 			}
 			catch (CoreException e)
 			{
-				Debug.error(e);
+				ServoyLog.logError(e);
 			}
 		}
 	}
@@ -1667,7 +1667,7 @@ public class DataModelManager implements IServerInfoManager
 					{
 						severity = computeCustomSeverity(ServoyBuilder.DBI_COLUMN_CONFLICT);
 					}
-					else
+					else if (t1 != Types.ARRAY || t2 != Types.ARRAY) // ignore difference of array length, db reports as MAXINT
 					{
 						boolean compatibleLengths = (t1 == t2) && (t1 == IColumnTypes.MEDIA || t1 == IColumnTypes.TEXT) &&
 							(Math.abs(colColumnType.getLength() - (float)dbiColumnType.getLength()) > (Integer.MAX_VALUE / 2));
