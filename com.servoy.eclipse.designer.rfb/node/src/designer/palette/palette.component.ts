8e4b39f27957786025281e8c827827e6af728c06
cPecican <cpecican@servoy.com>
2025-10-09 09:21:39 +0300

    SVY-20543 Row drop location is not the same as the hover location check for absolute form layout before setting size

--- com.servoy.eclipse.designer.rfb/node/src/designer/palette/palette.component.ts
+++ com.servoy.eclipse.designer.rfb/node/src/designer/palette/palette.component.ts
@@ -341,8 +341,7 @@ export class PaletteComponent implements ISupportAutoscroll, ISupportRefreshPale
                 this.renderer.setStyle(this.dragItem.contentItemBeingDragged, 'top', event.pageY - this.editorContentService.getTopPositionIframe() + 'px');
 
                 this.canDrop = this.designerUtilsService.getDropNode(this.urlParser.isAbsoluteFormLayout(), this.dragItem.componentType, this.dragItem.topContainer, this.dragItem.layoutName ? this.dragItem.packageName + '.' + this.dragItem.layoutName : this.dragItem.layoutName, event, this.dragItem.elementName);
-                
-                console.log(this.canDrop);
+
                 if (!this.canDrop.dropAllowed) {
                     this.editorContentService.getGlassPane().style.cursor = 'not-allowed';
                 }
@@ -353,7 +352,6 @@ export class PaletteComponent implements ISupportAutoscroll, ISupportRefreshPale
                 if (this.dragItem.contentItemBeingDragged) {
                     if (this.canDrop.dropAllowed) {
                         //TODO do we need to optimize the calls to insert the dragged component?
-                        console.log("call insert");
                         this.editorContentService.sendMessageToIframe({
                             id: 'insertDraggedComponent',
                             dropTarget: this.canDrop.dropTarget && !this.canDrop.dropTarget.classList.contains('svy-csspositioncontainer') ? this.canDrop.dropTarget.getAttribute('svy-id') : null,
