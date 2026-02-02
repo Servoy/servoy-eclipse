edb6dd7c0528ca06aa810c54f1b4d8f01d5ce56b
Johan Compagner <jcompagner@servoy.com>
2026-02-02 16:22:41 +0100

    SVY-20851 create a code completion implementation based on our own ai plugin
    Created an extension point so that the servoy core plugins can get an AI
    Model to chat with. A system message should be given to configure the
    model correctly in what it should do

--- com.servoy.eclipse.core/src/com/servoy/eclipse/core/Activator.java
+++ com.servoy.eclipse.core/src/com/servoy/eclipse/core/Activator.java
@@ -97,6 +97,7 @@ import org.osgi.service.url.URLStreamHandlerService;
 import org.sablo.websocket.GetHttpSessionConfigurator;
 
 import com.servoy.base.persistence.constants.IFormConstants;
+import com.servoy.eclipse.core.ai.AIModelProvider;
 import com.servoy.eclipse.core.doc.IDocumentationManagerProvider;
 import com.servoy.eclipse.core.repository.SwitchableEclipseUserManager;
 import com.servoy.eclipse.core.resource.PersistEditorInput;
@@ -185,6 +186,8 @@ public class Activator extends Plugin
 	private final List<Runnable> postgressCheckedNotify = new ArrayList<Runnable>(3);
 
 	private boolean designerCallbackNotSetTodch = true;
+
+	private AIModelProvider aiModelProvider;
 
 	@Override
 	public void start(BundleContext context) throws Exception
@@ -1075,6 +1078,15 @@ public class Activator extends Plugin
 			debuggerStarter = (IDebuggerStarter)createExtensionPoint(IDebuggerStarter.EXTENSION_ID);
 		}
 		return debuggerStarter;
+	}
+
+	public synchronized AIModelProvider getAIModelProvider()
+	{
+		if (aiModelProvider == null)
+		{
+			aiModelProvider = (AIModelProvider)createExtensionPoint(AIModelProvider.EXTENSION_ID);
+		}
+		return aiModelProvider;
 	}
 
 	/**
