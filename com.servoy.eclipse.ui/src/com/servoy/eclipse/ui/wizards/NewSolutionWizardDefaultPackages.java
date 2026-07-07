ec570925a90832ed60f2eddf34cff81910cbb369
Johan Compagner <jcompagner@servoy.com>
2026-07-07 04:21:18 -0500

    fix for: (security fix was to strict)
    org.xml.sax.SAXParseException; lineNumber: 2; columnNumber: 10; DOCTYPE
    is disallowed when the feature
    "http://apache.org/xml/features/disallow-doctype-decl" set to true.  at
    java.xml/com.sun.org.apache.xerces.internal.parsers.DOMParser.parse(Unknown
    Source)  at
    java.xml/com.sun.org.apache.xerces.internal.jaxp.DocumentBuilderImpl.parse(Unknown
    Source)  at java.xml/javax.xml.parsers.DocumentBuilder.parse(Unknown
    Source)  at com.servoy.eclipse.ui.wizards.NewSolutionWizardDefaultPackages.getDatabaseInfo(NewSolutionWizardDefaultPackages.java:259)  at
    com.servoy.eclipse.ui.wizards.NewSolutionWizard.searchMissingServers(NewSolutionWizard.java:572)  at
    com.servoy.eclipse.ui.wizards.NewSolutionWizard.canFinish(NewSolutionWizard.java:863)  at
    org.eclipse.jface.wizard.WizardDialog.updateButtons(WizardDialog.java:1332)  at
    com.servoy.eclipse.ui.views.solutionexplorer.actions.OpenWizardAction$1.updateButtons(OpenWizardAction.java:249)

--- com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/wizards/NewSolutionWizardDefaultPackages.java
+++ com.servoy.eclipse.ui/src/com/servoy/eclipse/ui/wizards/NewSolutionWizardDefaultPackages.java
@@ -236,7 +236,7 @@ public class NewSolutionWizardDefaultPackages
 		{
 			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 			factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-			factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+			factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", false);
 			factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
 			factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
 			factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
