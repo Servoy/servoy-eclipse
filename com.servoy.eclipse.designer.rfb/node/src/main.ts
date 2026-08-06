4957d8f5a31518d1cbc6229db025aec14c17d100
Johan Compagner <jcompagner@servoy.com>
2026-08-06 16:31:55 +0200

    cleanup RFB: remove unused deps, stale config, environments, .browserslistrc [ai]
    - Remove @angular-devkit/core + @angular-devkit/schematics from devDeps
    - Remove .browserslistrc (Angular 22 has sensible defaults)
    - Remove src/environments/ and fileReplacements from angular.json
    - Remove enableProdMode/environment import from main.ts (builder handles it)
    - Remove stale NgModule-era schematics from angular.json
    - Set analytics: false in angular.json
    - Clean tsconfig.json: remove compileOnSave, declaration; fix indentation
    - Build: passing, 307 tests: passing, lint: 131 pre-existing warnings (unchanged)

--- com.servoy.eclipse.designer.rfb/node/src/main.ts
+++ com.servoy.eclipse.designer.rfb/node/src/main.ts
@@ -1,13 +1,7 @@
-import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
+import { provideZoneChangeDetection } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { DesignerModule } from './designer/designer.module';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
 
 platformBrowserDynamic().bootstrapModule(DesignerModule, { applicationProviders: [provideZoneChangeDetection()], })
   .catch(err => console.error(err));
-
