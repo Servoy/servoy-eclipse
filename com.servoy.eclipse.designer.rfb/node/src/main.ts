62b5a51f7cef1ffe45bc16873ffb6cb40f0a6d63
Johan Compagner <jcompagner@servoy.com>
2026-08-11 10:53:28 +0200

    remove importProvidersFrom from main.ts, use direct providers [ai]
    bootstrapApplication() already provides BrowserModule internally.
    All standalone components import their own directives (NgStyle, NgClass,
    FormsModule, NgbModule directives) so the global importProvidersFrom
    wrapper was redundant. DragDropModule was unused entirely.

--- com.servoy.eclipse.designer.rfb/node/src/main.ts
+++ com.servoy.eclipse.designer.rfb/node/src/main.ts
@@ -1,24 +1,18 @@
-import { importProvidersFrom, provideZonelessChangeDetection } from '@angular/core';
-
+import { provideZonelessChangeDetection } from '@angular/core';
 
 import { EditorSessionService } from './designer/services/editorsession.service';
 import { URLParserService } from './designer/services/urlparser.service';
-import { WindowRefService, ServoyPublicModule } from '@servoy/public';
+import { WindowRefService } from '@servoy/public';
 import { DesignSizeService } from './designer/services/designsize.service';
 import { DesignerUtilsService } from './designer/services/designerutils.service';
 import { EditorContentService } from './designer/services/editorcontent.service';
 import { DynamicGuidesService } from './designer/services/dynamicguides.service';
 import { provideHttpClient, withXhr, withInterceptorsFromDi } from '@angular/common/http';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { DragDropModule } from '@angular/cdk/drag-drop';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { DesignerComponent } from './designer/designer.component';
 
 bootstrapApplication(DesignerComponent, {
     providers: [
-        importProvidersFrom(BrowserModule, ServoyPublicModule, FormsModule, CommonModule, NgbModule, DragDropModule),
         EditorSessionService, URLParserService, WindowRefService, DesignSizeService, DesignerUtilsService,
         EditorContentService, DynamicGuidesService, provideHttpClient(withXhr(), withInterceptorsFromDi()),
         provideZonelessChangeDetection()
