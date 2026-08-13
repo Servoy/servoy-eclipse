c080bc72f70712336f11336d9f18a52e755c62e3
Johan Compagner <jcompagner@servoy.com>
2026-08-13 10:25:00 +0200

    SVY-19023 add serveronly tag support in ComponentTemplateGenerator [ai]

--- com.servoy.eclipse.ngclient.ui/src/com/servoy/eclipse/ngclient/ui/ComponentTemplateGenerator.java
+++ com.servoy.eclipse.ngclient.ui/src/com/servoy/eclipse/ngclient/ui/ComponentTemplateGenerator.java
@@ -153,6 +153,7 @@ public class ComponentTemplateGenerator
 		{
 			String name = pd.getName();
 			if (pd.isInternal()) continue;
+			if (Boolean.TRUE.equals(pd.getTag("serveronly"))) continue;
 			if (name.equals(IContentSpecConstants.PROPERTY_ATTRIBUTES))
 			{
 				name = "servoyAttributes";
