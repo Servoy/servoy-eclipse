15790ffd91040f84eef3667e13ec128d608b7cdd
Johan Compagner <jcompagner@servoy.com>
2026-08-27 13:52:03 +0200

    modernize packagetemplate: Angular 22, Vitest, standalone, no peerDeps [ai]

--- com.servoy.eclipse.ngclient.ui/packagetemplate/project/src/public-api.ts
+++ com.servoy.eclipse.ngclient.ui/packagetemplate/project/src/public-api.ts
@@ -1,6 +1,5 @@
 /*
  * Public API Surface
  */
-export * from './##packagename##.module';
 //export all components here
-//export * from './svygmaps/svygmaps';
+//export * from './mycomponent/mycomponent';
